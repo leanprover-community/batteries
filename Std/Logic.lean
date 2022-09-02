c3057d9bf359f4ade1c125db2e79647e1f10a915
Mario Carneiro <di.gama@gmail.com>
2022-09-02 08:39:20 -0400

    feat: move Mathlib.Init.Data.Nat.Lemmas to Std

--- Std/Logic.lean
+++ Std/Logic.lean
@@ -10,23 +10,28 @@ Authors: Mario Carneiro
 the arguments flipped, but it is in the `not` namespace so that projection notation can be used. -/
 def Not.elim {α : Sort _} (H1 : ¬a) (H2 : a) : α := absurd H2 H1
 
+theorem not_congr (h : a ↔ b) : ¬a ↔ ¬b := ⟨mt h.2, mt h.1⟩
+
 /-! Declarations about `iff` -/
 
-theorem iff_of_true (ha : a) (hb : b) : a ↔ b :=
-⟨λ_ => hb, λ _ => ha⟩
+theorem iff_of_true (ha : a) (hb : b) : a ↔ b := ⟨λ_ => hb, λ _ => ha⟩
 
-theorem iff_of_false (ha : ¬a) (hb : ¬b) : a ↔ b :=
-⟨ha.elim, hb.elim⟩
+theorem iff_of_false (ha : ¬a) (hb : ¬b) : a ↔ b := ⟨ha.elim, hb.elim⟩
+
+/- or simp rules -/
+
+-- Port note: in mathlib3, this is not_or
+theorem not_or_intro {a b : Prop} : ¬ a → ¬ b → ¬ (a ∨ b)
+  | hna, _, .inl ha => absurd ha hna
+  | _, hnb, .inr hb => absurd hb hnb
 
 /- or resolution rules -/
 
-theorem Or.resolve_left {a b : Prop} (h: a ∨ b) (na : ¬ a) : b :=
-Or.elim h (λ ha => absurd ha na) id
+theorem Or.resolve_left {a b : Prop} (h: a ∨ b) (na : ¬ a) : b := h.elim (absurd · na) id
 
 theorem Or.neg_resolve_left (h : ¬a ∨ b) (ha : a) : b := h.elim (absurd ha) id
 
-theorem Or.resolve_right {a b : Prop} (h: a ∨ b) (nb : ¬ b) : a :=
-Or.elim h id (λ hb => absurd hb nb)
+theorem Or.resolve_right {a b : Prop} (h: a ∨ b) (nb : ¬ b) : a := h.elim id (absurd · nb)
 
 theorem Or.neg_resolve_right (h : a ∨ ¬b) (nb : b) : a := h.elim id (absurd nb)
 
