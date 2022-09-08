25fe33ea832ccc3f6bfa4699cc09223461ef3351
Mario Carneiro <di.gama@gmail.com>
2022-09-08 01:59:08 -0400

    feat: missingDocs handler for binder_predicate

--- Std/Util/ExtendedBinder.lean
+++ Std/Util/ExtendedBinder.lean
@@ -3,14 +3,15 @@ Copyright (c) 2021 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gabriel Ebner
 -/
-import Lean
-open Lean
+import Lean.Elab.MacroArgUtil
+import Lean.Linter.MissingDocs
 
 /-!
 Defines an extended binder syntax supporting `∀ ε > 0, ...` etc.
 -/
 
 namespace Std.ExtendedBinder
+open Lean
 
 /--
 The syntax category of binder predicates contains predicates like `> 0`, `∈ s`, etc.
@@ -94,13 +95,13 @@ Declares a binder predicate.  For example:
 binder_predicate x " > " y:term => `($x > $y)
 ```
 -/
-syntax (name := binderPredicate) (docComment)? (attrKind)?
+syntax (name := binderPredicate) (docComment)? (Parser.Term.attributes)? (attrKind)?
   "binder_predicate " optNamedName optNamedPrio ident macroArg* " => " term : command
 
 -- adapted from the macro macro
 open Elab Command in
 elab_rules : command
-  | `($[$doc?:docComment]? $attrKind:attrKind binder_predicate%$tk
+  | `($[$doc?:docComment]? $[@[$attrs?,*]]? $attrKind:attrKind binder_predicate%$tk
       $[(name := $name?)]? $[(priority := $prio?)]? $x $args:macroArg* => $rhs) => do
     let prio ← liftMacroM do evalOptPrio prio?
     let (stxParts, patArgs) := (← args.mapM expandMacroArg).unzip
@@ -112,10 +113,18 @@ elab_rules : command
     `macro_rules` commands. -/
     let pat : TSyntax `binderPred := ⟨(mkNode ((← getCurrNamespace) ++ name) patArgs).1⟩
     elabCommand <|<-
-    `($[$doc?:docComment]? $attrKind:attrKind syntax%$tk
+    `($[$doc?:docComment]? $[@[$attrs?,*]]? $attrKind:attrKind syntax%$tk
         (name := $(← mkIdentFromRef name)) (priority := $(quote prio)) $[$stxParts]* : binderPred
       $[$doc?:docComment]? macro_rules%$tk
         | `(satisfiesBinderPred% $$($x):term $pat:binderPred) => $rhs)
+
+open Linter.MissingDocs Parser Term in
+/-- Missing docs handler for `binder_predicate` -/
+@[missingDocsHandler binderPredicate]
+def checkBinderPredicate : SimpleHandler := fun stx => do
+  if stx[0].isNone && stx[2][0][0].getKind != ``«local» then
+    if stx[4].isNone then lint stx[3] "binder predicate"
+    else lintNamed stx[4][0][3] "binder predicate"
 
 /-- Declare `∃ x > y, ...` as syntax for `∃ x, x > y ∧ ...` -/
 binder_predicate x " > " y:term => `($x > $y)
