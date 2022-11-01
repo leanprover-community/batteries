c93fd4cc25838a5f6e8c6d750e606bc487798db8
James Gallicchio <JamesGallicchio@users.noreply.github.com>
2022-11-01 13:27:59 -0400

    feat: Nat.sqrt

--- Std/Data/Nat/Basic.lean
+++ Std/Data/Nat/Basic.lean
@@ -15,3 +15,19 @@ instance : Dvd Nat := ⟨fun a b => ∃ c, b = a * c⟩
 
 /-- Sum of a list of natural numbers. -/
 protected def sum (l : List Nat) : Nat := l.foldr (·+·) 0
+
+/--
+Integer square root function. Implemented via Newton's method.
+-/
+def sqrt (n : Nat) : Nat :=
+  let guess := n / 2
+  if guess = 0 then n else
+  let rec iter (guess : Nat) : Nat :=
+    let next := (guess + n / guess) / 2
+    if h : guess ≤ next then
+      guess
+    else
+      have : next < guess := Nat.gt_of_not_le h
+      iter next
+  iter guess
+termination_by iter guess => guess
