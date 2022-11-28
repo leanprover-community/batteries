53adfd7b2c3177b38422cc4d646c5edc9ed9cc3e
Scott Morrison <scott.morrison@gmail.com>
2022-11-27 19:56:19 -0600

    import all

--- Std.lean
+++ Std.lean
@@ -40,6 +40,7 @@ import Std.Data.Rat.Lemmas
 import Std.Lean.AttributeExtra
 import Std.Lean.Command
 import Std.Lean.Delaborator
+import Std.Lean.HashMap
 import Std.Lean.Meta
 import Std.Lean.NameMapAttribute
 import Std.Lean.Parser
