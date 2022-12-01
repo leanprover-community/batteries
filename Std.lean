d2633bd66d75e0ae609c591d2ab70897da14f14d
Jannis Limperg <jannis@limperg.de>
2022-12-01 20:03:43 +0100

    feat: add Lean.Meta.getUnusedNames

--- Std.lean
+++ Std.lean
@@ -37,12 +37,14 @@ import Std.Data.RBMap.WF
 import Std.Data.Rat
 import Std.Data.Rat.Basic
 import Std.Data.Rat.Lemmas
+import Std.Data.String
 import Std.Lean.AttributeExtra
 import Std.Lean.Command
 import Std.Lean.Delaborator
 import Std.Lean.Meta.Basic
 import Std.Lean.Meta.InstantiateMVars
 import Std.Lean.Meta.SavedState
+import Std.Lean.Meta.UnusedNames
 import Std.Lean.MonadBacktrack
 import Std.Lean.NameMapAttribute
 import Std.Lean.Parser
---
+++ Std/Data/String.lean
@@ -0,0 +1,19 @@
+/-
+Copyright (c) 2022 Jannis Limperg. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jannis Limperg
+-/
+
+namespace String
+
+/--
+If `pre` is a prefix of `s`, i.e. `s = pre ++ t`, return the remainder `t`.
+-/
+def dropPrefix? (s : String) (pre : String) : Option Substring :=
+  let s := s.toSubstring
+  if s.take pre.length == pre.toSubstring then
+    s.drop pre.length
+  else
+    none
+
+end String
