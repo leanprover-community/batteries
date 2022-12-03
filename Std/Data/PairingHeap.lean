a2742a93164fcef0a4fc79e4daa6203b0102a0d3
negiizhao <egresf@gmail.com>
2022-12-03 10:09:57 +0800

    forgot to edit copyright info

--- Std/Data/PairingHeap.lean
+++ Std/Data/PairingHeap.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2022 Microsoft Corporation. All rights reserved.
+Copyright (c) 2022 Yuyang Zhao. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yuyang Zhao
 -/
