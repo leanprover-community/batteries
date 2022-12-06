707090e0f795c0ac9106e02f2f99f7daebbe7c32
Gabriel Ebner <gebner@gebner.org>
2022-12-06 01:32:05 +0100

    feat: add dangerous instance linter (#54)
    * feat: add dangerous instance linter
    
    * pacify linter
    
    * fix

---
+++ test/lintTC.lean
@@ -0,0 +1,31 @@
+import Std.Tactic.Lint.TypeClass
+open Std.Tactic.Lint
+
+namespace A
+local instance impossible {α β : Type} [Inhabited α] : Nonempty α := ⟨default⟩
+#eval do guard (← impossibleInstance.test ``impossible).isSome
+end A
+
+namespace B
+class VectorSpace (K : outParam Type) (V : Type) extends Add V
+#eval do guard (← dangerousInstance.test ``VectorSpace.toAdd).isNone
+end B
+
+namespace C
+class VectorSpace (K : Type) (V : Type) extends Add V
+#eval do guard (← dangerousInstance.test ``VectorSpace.toAdd).isSome
+end C
+
+namespace D
+class OrderHomClass (F : Type) (α β : outParam Type) [LE α] [LE β]
+instance dangerous [LE α] [LE β] [OrderHomClass F α β] : CoeFun F fun _ => α → β := sorry
+#eval do guard (← dangerousInstance.test ``dangerous).isSome
+instance fixed {_ : LE α} {_ : LE β} [OrderHomClass F α β] : CoeFun F fun _ => α → β := sorry
+#eval do guard (← dangerousInstance.test ``fixed).isNone
+end D
+
+namespace E
+class Foo (α : Type)
+class Bar (α : Type) [LE α] extends Foo α
+#eval do guard (← dangerousInstance.test ``Bar.toFoo).isNone
+end E
