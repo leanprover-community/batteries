35cbe1a67e88bdbb6cecba7ea966d0b029bd7787
Jannis Limperg <jannis@limperg.de>
2022-12-07 17:41:37 +0100

    feat: Prod.Lex is a DecidableRel

--- Std.lean
+++ Std.lean
@@ -31,6 +31,7 @@ import Std.Data.Option.Basic
 import Std.Data.Option.Init.Lemmas
 import Std.Data.Option.Lemmas
 import Std.Data.PairingHeap
+import Std.Data.Prod.Lex
 import Std.Data.RBMap
 import Std.Data.RBMap.Alter
 import Std.Data.RBMap.Basic
