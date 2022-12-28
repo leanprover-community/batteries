844d57b4645cd4ed4e5d91882a03d960329a5fac
François G. Dorais <fgdorais@gmail.com>
2022-12-28 01:59:48 -0500

    feat: add `LinearCmp` class
    * add `OrientedOrd`, `TransOrd`, `LinearOrd` class abbrevs
    * add some more lemmas

--- Std/Classes/Order.lean
+++ Std/Classes/Order.lean
@@ -54,6 +54,9 @@ class OrientedCmp (cmp : α → α → Ordering) : Prop where
   /-- The comparator operation is swap-symmetric. -/
   cmp_swap_symm (x y) : (cmp x y).swap = cmp y x
 
+/-- `OrientedOrd α` asserts that `compare` is determined by the relation `compare x y = .lt`. -/
+class abbrev OrientedOrd (α) [self : Ord α] := OrientedCmp self.compare
+
 namespace OrientedCmp
 variable {cmp : α → α → Ordering} [OrientedCmp cmp]
 
@@ -69,6 +72,22 @@ theorem cmp_eq_refl : cmp x x = .eq :=
   | .eq => rfl
   | .gt => nomatch (cmp_gt_lt_symm.1 e).symm.trans e
 
+theorem cmp_lt_irrefl : cmp x x ≠ .lt := fun h => Ordering.noConfusion (cmp_eq_refl (cmp:=cmp) ▸ h)
+
+theorem cmp_gt_irrefl : cmp x x ≠ .gt := fun h => Ordering.noConfusion (cmp_eq_refl (cmp:=cmp) ▸ h)
+
+theorem cmp_le_total : cmp x y ≠ .gt ∨ cmp y x ≠ .gt :=
+  match h : cmp x y with
+  | .lt => .inl Ordering.noConfusion
+  | .gt => .inr fun h' => Ordering.noConfusion (cmp_gt_lt_symm.1 h ▸ h')
+  | .eq => .inl Ordering.noConfusion
+
+theorem cmp_ge_total : cmp x y ≠ .lt ∨ cmp y x ≠ .lt :=
+  match h : cmp x y with
+  | .lt => .inr fun h' => Ordering.noConfusion (cmp_gt_lt_symm.2 h ▸ h')
+  | .gt => .inl Ordering.noConfusion
+  | .eq => .inl Ordering.noConfusion
+
 end OrientedCmp
 
 /-- `TransCmp cmp` asserts that `cmp` induces a transitive relation. -/
@@ -76,11 +95,14 @@ class TransCmp (cmp : α → α → Ordering) extends OrientedCmp cmp : Prop whe
   /-- The comparator operation is transitive. -/
   cmp_le_trans : cmp x y ≠ .gt → cmp y z ≠ .gt → cmp x z ≠ .gt
 
+/-- `TransOrd α` asserts that `compare` induces a transitive relation. -/
+class abbrev TransOrd (α) [self : Ord α] := TransCmp self.compare
+
 namespace TransCmp
 variable {cmp : α → α → Ordering} [TransCmp cmp]
 open OrientedCmp Decidable
 
-theorem cmp_ge_trans (h₁ : cmp x y ≠ .lt) (h₂ : cmp y z ≠ .lt) : cmp x z ≠ .lt := by
+theorem cmp_ge_trans {x y z} (h₁ : cmp x y ≠ .lt) (h₂ : cmp y z ≠ .lt) : cmp x z ≠ .lt := by
   have := @cmp_le_trans _ cmp _ z y x
   simp [cmp_gt_lt_symm] at *; exact this h₂ h₁
 
@@ -99,6 +121,12 @@ theorem cmp_lt_le_trans (h₁ : cmp x y = .lt) (h₂ : cmp y z ≠ .gt) : cmp x 
 theorem cmp_lt_trans (h₁ : cmp x y = .lt) (h₂ : cmp y z = .lt) : cmp x z = .lt :=
   cmp_le_lt_trans (cmp_gt_asymm <| cmp_gt_lt_symm.2 h₁) h₂
 
+theorem cmp_ge_gt_trans (h₁ : cmp x y ≠ .lt) (h₂ : cmp y z = .gt) : cmp x z = .gt :=
+  byContradiction fun h₃ => cmp_le_trans (mt cmp_gt_lt_symm.1 h₁) h₃ h₂
+
+theorem cmp_gt_ge_trans (h₁ : cmp x y = .gt) (h₂ : cmp y z ≠ .lt) : cmp x z = .gt :=
+  byContradiction fun h₃ => cmp_le_trans h₃ (mt cmp_gt_lt_symm.1 h₂) h₁
+
 theorem cmp_gt_trans (h₁ : cmp x y = .gt) (h₂ : cmp y z = .gt) : cmp x z = .gt := by
   rw [cmp_gt_lt_symm] at h₁ h₂ ⊢; exact cmp_lt_trans h₂ h₁
 
@@ -115,6 +143,47 @@ theorem cmp_congr_right (yz : cmp y z = .eq) : cmp x y = cmp x z := by
   rw [← Ordering.swap_inj, cmp_swap_symm, cmp_swap_symm, cmp_congr_left yz]
 
 end TransCmp
+
+/-- `LinearCmp cmp` asserts that `cmp` induces a linear ordering. -/
+class LinearCmp (cmp : α → α → Ordering) extends TransCmp cmp : Prop where
+  /-- The comparator equality is strict. -/
+  cmp_eq_strict : cmp x y = .eq → x = y
+
+/-- `LinearOrd α` asserts that `compare` induces a linear ordering. -/
+class abbrev LinearOrd (α) [self : Ord α] := LinearCmp self.compare
+
+namespace LinearCmp
+variable {cmp : α → α → Ordering} [LinearCmp cmp]
+open OrientedCmp TransCmp
+
+theorem cmp_eq_iff_eq : cmp x y = .eq ↔ x = y :=
+  ⟨cmp_eq_strict, fun | rfl => cmp_eq_refl⟩
+
+theorem cmp_antisymm (h₁ : cmp x y ≠ .lt) (h₂ : cmp x y ≠ .gt) : x = y :=
+  match h : cmp x y with
+  | .lt => absurd h h₁
+  | .gt => absurd h h₂
+  | .eq => cmp_eq_strict h
+
+theorem cmp_le_antisymm (h₁ : cmp x y ≠ .lt) (h₂ : cmp y x ≠ .lt) : x = y :=
+  cmp_antisymm h₁ (mt cmp_gt_lt_symm.1 h₂)
+
+theorem cmp_ge_antisymm (h₁ : cmp x y ≠ .gt) (h₂ : cmp y x ≠ .gt) : x = y :=
+  cmp_antisymm (mt cmp_gt_lt_symm.2 h₂) h₁
+
+theorem cmp_lt_connex (hne : x ≠ y) : cmp x y = .lt ∨ cmp y x = .lt :=
+  match h : cmp x y with
+  | .lt => .inl rfl
+  | .gt => .inr (cmp_gt_lt_symm.1 h)
+  | .eq => absurd (cmp_eq_strict h) hne
+
+theorem cmp_gt_connex (hne : x ≠ y) : cmp x y = .gt ∨ cmp y x = .gt :=
+  match h : cmp x y with
+  | .lt => .inr (cmp_gt_lt_symm.2 h)
+  | .gt => .inl rfl
+  | .eq => absurd (cmp_eq_strict h) hne
+
+end LinearCmp
 
 /-- Pull back a comparator by a function `f`, by applying the comparator to both arguments. -/
 @[inline] def byKey (f : α → β) (cmp : β → β → Ordering) (a b : α) : Ordering := cmp (f a) (f b)
