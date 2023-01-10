bf2059da3cea9e0d1c1a29abd90cf6b397810b3e
Mario Carneiro <di.gama@gmail.com>
2023-01-09 23:40:08 -0500

    chore: style, add operation complexities

--- Std/Lean/HashSet.lean
+++ Std/Lean/HashSet.lean
@@ -11,7 +11,7 @@ namespace Lean.HashSet
 variable [BEq α] [Hashable α]
 
 /--
-Returns `true` if `f` returns `true` for any element of the set.
+`O(n)`. Returns `true` if `f` returns `true` for any element of the set.
 -/
 @[specialize]
 def anyM [Monad m] (s : HashSet α) (f : α → m Bool) : m Bool := do
@@ -21,34 +21,34 @@ def anyM [Monad m] (s : HashSet α) (f : α → m Bool) : m Bool := do
   return false
 
 /--
-Returns `true` if `f` returns `true` for any element of the set.
+`O(n)`. Returns `true` if `f` returns `true` for any element of the set.
 -/
 @[inline]
 def any (s : HashSet α) (f : α → Bool) : Bool :=
-  Id.run $ s.anyM f
+  Id.run <| s.anyM f
 
 /--
-Returns `true` if `f` returns `true` for all elements of the set.
+`O(n)`. Returns `true` if `f` returns `true` for all elements of the set.
 -/
 @[specialize]
 def allM [Monad m] (s : HashSet α) (f : α → m Bool) : m Bool := do
   for a in s do
-    if ! (← f a) then
+    if !(← f a) then
       return false
   return true
 
 /--
-Returns `true` if `f` returns `true` for all elements of the set.
+`O(n)`. Returns `true` if `f` returns `true` for all elements of the set.
 -/
 @[inline]
 def all (s : HashSet α) (f : α → Bool) : Bool :=
-  Id.run $ s.allM f
+  Id.run <| s.allM f
 
 instance : BEq (HashSet α) where
   beq s t := s.all (t.contains ·) && t.all (s.contains ·)
 
 /--
-Similar to `insert`, but also returns a Boolean flad indicating whether an
+`O(1)` amortized. Similar to `insert`, but also returns a Boolean flag indicating whether an
 existing entry has been replaced with `a ↦ b`.
 -/
 @[inline]
@@ -58,23 +58,23 @@ def insert' (s : HashSet α) (a : α) : HashSet α × Bool :=
   (s, s.size == oldSize)
 
 /--
-Obtain a `HashSet` from an array.
+`O(n)`. Obtain a `HashSet` from an array.
 -/
 @[inline]
 protected def ofArray [BEq α] [Hashable α] (as : Array α) : HashSet α :=
   HashSet.empty.insertMany as
 
 /--
-Obtain a `HashSet` from a list.
+`O(n)`. Obtain a `HashSet` from a list.
 -/
 @[inline]
 protected def ofList [BEq α] [Hashable α] (as : Array α) : HashSet α :=
   HashSet.empty.insertMany as
 
 /--
-Merge two `HashSet`s.
+`O(|t|)` amortized. Merge two `HashSet`s.
 -/
 @[inline]
 def merge {α : Type u} [BEq α] [Hashable α] (s t : HashSet α) : HashSet α :=
-  t.fold (init := s) λ s a => s.insert a
+  t.fold (init := s) fun s a => s.insert a
   -- We don't use `insertMany` here because it gives weird universes.
