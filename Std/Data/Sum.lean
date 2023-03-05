6e2d1914ab97bb6f2af4adc891130f542ceb0ed2
James <jamesgallicchio@gmail.com>
2023-03-05 03:10:24 -0500

    feat: Sum.inl, Sum.inr

---
+++ Std/Data/Sum.lean
@@ -0,0 +1,15 @@
+/-
+Copyright (c) 2023 James Gallicchio. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: James Gallicchio
+-/
+
+/-- Alias for `Sum.inl`. -/
+@[match_pattern] def Sum.left (x : α) : α ⊕ β := .inl x
+
+/-- Alias for `Sum.inr`. -/
+@[match_pattern] def Sum.right (x : β) : α ⊕ β := .inr x
+
+example : Nat ⊕ String := .left 0
+example (x : Nat ⊕ String) :=
+  match x with | .left n => n | .right _ => 0
