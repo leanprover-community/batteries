15eb6cce6eb177bc6aca110bf39a6991bca04372
thorimur <68410468+thorimur@users.noreply.github.com>
2023-03-21 18:07:56 -0400

    feat: make `Rat.ofScientific` `@[irreducible]`

--- Std/Data/Rat/Basic.lean
+++ Std/Data/Rat/Basic.lean
@@ -101,8 +101,9 @@ def divInt : Int → Int → Rat
 
 @[inherit_doc] scoped infixl:70 " /. " => Rat.divInt
 
-/-- Implements "scientific notation" `123.4e-5` for rational numbers. -/
-protected def ofScientific (m : Nat) (s : Bool) (e : Nat) : Rat :=
+/-- Implements "scientific notation" `123.4e-5` for rational numbers. (This definition is
+`@[irreducible]` because you don't want to unfold it over e.g. `RatCast.ratCast`.) -/
+@[irreducible] protected def ofScientific (m : Nat) (s : Bool) (e : Nat) : Rat :=
   if s then
     Rat.normalize m (10 ^ e) <| Nat.ne_of_gt <| Nat.pos_pow_of_pos _ (by decide)
   else
