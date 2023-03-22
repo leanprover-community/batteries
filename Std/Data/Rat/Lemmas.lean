3450bac883a51139afcca216a69a5ab146ddfb6e
thorimur <68410468+thorimur@users.noreply.github.com>
2023-03-22 03:13:28 -0400

    chore: save a couple lines

--- Std/Data/Rat/Lemmas.lean
+++ Std/Data/Rat/Lemmas.lean
@@ -307,6 +307,4 @@ theorem ofScientific_false_def : Rat.ofScientific m false e = (m * 10 ^ e : Nat)
 
 theorem ofScientific_def : Rat.ofScientific m s e =
     if s then mkRat m (10 ^ e) else (m * 10 ^ e : Nat) := by
-  cases s
-  · exact ofScientific_false_def
-  · exact ofScientific_true_def
+  cases s; exact ofScientific_false_def; exact ofScientific_true_def
