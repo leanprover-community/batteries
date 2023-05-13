476e814dec280d79144f1b7f3c56c4aad98d1c7d
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-05-12 20:59:58 -0400

    import to main file

--- Std.lean
+++ Std.lean
@@ -78,6 +78,7 @@ import Std.Tactic.Basic
 import Std.Tactic.ByCases
 import Std.Tactic.CoeExt
 import Std.Tactic.Congr
+import Std.Tactic.Exact
 import Std.Tactic.Ext
 import Std.Tactic.Ext.Attr
 import Std.Tactic.GuardExpr
