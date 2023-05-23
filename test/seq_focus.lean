a064d0f4336122bfbb834b266975146e335ab7ce
Mario Carneiro <di.gama@gmail.com>
2023-05-22 23:31:13 -0400

    fix: tests

--- test/seq_focus.lean
+++ test/seq_focus.lean
@@ -4,15 +4,15 @@ example : (True ∧ (∃ x : Nat, x = x)) ∧ True := by
   constructor
   constructor
   -- error: too many tactics
-  fail_if_success map_tacs [trivial, exact ⟨0, rfl⟩, trivial, trivial]
+  fail_if_success map_tacs [trivial; exact ⟨0, rfl⟩; trivial; trivial]
   -- error: not enough tactics
-  fail_if_success map_tacs [trivial, exact ⟨0, rfl⟩]
-  map_tacs [trivial, exact ⟨0, rfl⟩, trivial]
+  fail_if_success map_tacs [trivial; exact ⟨0, rfl⟩]
+  map_tacs [trivial; exact ⟨0, rfl⟩; trivial]
 
 example : ((True ∧ True) ∧ (∃ x : Nat, x = x)) ∧ (True ∧ (∃ x : Nat, x = x)) := by
   constructor
   constructor
-  map_tacs [(constructor; trivial), exact ⟨0, rfl⟩, constructor]
+  map_tacs [(constructor; trivial); exact ⟨0, rfl⟩; constructor]
   trivial
   trivial
   exact ⟨0, rfl⟩
@@ -21,4 +21,4 @@ example : (True ∧ (∃ x : Nat, x = x)) ∧ True := by
   constructor
   -- error: not enough tactics
   fail_if_success constructor <;> [trivial]
-  map_tacs [constructor <;> [trivial, exact ⟨0, rfl⟩], constructor]
+  map_tacs [constructor <;> [trivial; exact ⟨0, rfl⟩]; constructor]
