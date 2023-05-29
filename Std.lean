9b4e56227b3c82bd46d333155aa7322e712a4db3
Kyle Miller <kmill31415@gmail.com>
2023-05-29 15:51:27 -0600

    feat: `#instances` command to find applicable instances (#148)
    * feat: `#instances` command to find applicable instances
    
    * feat: add more priority info
    
    ---------
    
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- Std.lean
+++ Std.lean
@@ -92,6 +92,7 @@ import Std.Tactic.Ext.Attr
 import Std.Tactic.GuardExpr
 import Std.Tactic.GuardMsgs
 import Std.Tactic.HaveI
+import Std.Tactic.Instances
 import Std.Tactic.Lint
 import Std.Tactic.Lint.Basic
 import Std.Tactic.Lint.Frontend
