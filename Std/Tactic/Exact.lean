a706b3495c9661fb54267f47631f8e2acbace319
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-06-04 02:52:55 -0400

    Update Std/Tactic/Exact.lean

--- Std/Tactic/Exact.lean
+++ Std/Tactic/Exact.lean
@@ -13,7 +13,7 @@ open Lean Meta
 `MetaM` version of `Lean.Elab.Tactic.evalExact`: add `mvarId := x` to the metavariable assignment.
 This method wraps `Lean.MVarId.assign`, checking whether `mvarId` is already assigned, and whether
 the expression has the right type. -/
-def Lean.MVarId.myExact (g : MVarId) (e : Expr) : MetaM Unit := do
+def Lean.MVarId.assignIfDefeq (g : MVarId) (e : Expr) : MetaM Unit := do
   guard <| ← isDefEq (← g.getType) (← inferType e)
   g.checkNotAssigned `myExact
   g.assign e
