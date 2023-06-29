d5d98e05465fc546d519706e7fd0961b3f37d648
Patrick Massot <patrickmassot@free.fr>
2023-06-29 15:33:04 +0200

    Restore accidentally mangled name

--- Std/Data/Nat/Gcd.lean
+++ Std/Data/Nat/Gcd.lean
@@ -379,7 +379,7 @@ theorem coprime.eq_one_of_dvd {k m : Nat} (H : coprime k m) (d : k ∣ m) : k = 
   rw [← H.gcd_eq_one, gcd_eq_left d]
 
 /-- Represent a divisor of `m * n` as a product of a divisor of `m` and a divisor of `n`. -/
-def prod_Nat.dvd_and_dvd_of_dvd_prod {k m n : Nat} (H : k ∣ m * n) :
+def prod_dvd_and_dvd_of_dvd_prod {k m n : Nat} (H : k ∣ m * n) :
     {d : {m' // m' ∣ m} × {n' // n' ∣ n} // k = d.1.val * d.2.val} :=
   if h0 : gcd k m = 0 then
     ⟨⟨⟨0, eq_zero_of_gcd_eq_zero_right h0 ▸ Nat.dvd_refl 0⟩,
@@ -394,7 +394,7 @@ def prod_Nat.dvd_and_dvd_of_dvd_prod {k m n : Nat} (H : k ∣ m * n) :
 
 theorem gcd_mul_dvd_mul_gcd (k m n : Nat) : gcd k (m * n) ∣ gcd k m * gcd k n := by
   let ⟨⟨⟨m', hm'⟩, ⟨n', hn'⟩⟩, (h : gcd k (m * n) = m' * n')⟩ :=
-    prod_Nat.dvd_and_dvd_of_dvd_prod <| gcd_dvd_right k (m * n)
+    prod_dvd_and_dvd_of_dvd_prod <| gcd_dvd_right k (m * n)
   rw [h]
   have h' : m' * n' ∣ k := h ▸ gcd_dvd_left ..
   exact Nat.mul_dvd_mul
