1ac8198731f37288c834e70654324f30f65c5f4a
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-06-29 04:34:57 -0400

    feat: MetaM exact tactic (#137)

--- Std.lean
+++ Std.lean
@@ -91,6 +91,7 @@ import Std.Tactic.Basic
 import Std.Tactic.ByCases
 import Std.Tactic.CoeExt
 import Std.Tactic.Congr
+import Std.Tactic.Exact
 import Std.Tactic.Ext
 import Std.Tactic.Ext.Attr
 import Std.Tactic.GuardExpr
