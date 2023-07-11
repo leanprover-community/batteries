aaae4d0d64de333733e5105454542f540582b6b8
F. G. Dorais <fgdorais@gmail.com>
2023-07-10 23:16:54 -0400

    fix: copyright header

--- Std/Data/Bool.lean
+++ Std/Data/Bool.lean
@@ -1,6 +1,6 @@
 /-
-Copyright (c) 2023 F. G. Dorais. All rights reserved.
-eleased under Apache 2.0 license as described in the file LICENSE.
+Copyright (c) 2023 F. G. Dorais. No rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
 Authors: F. G. Dorais
 -/
 
