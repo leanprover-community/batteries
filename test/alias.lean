937355c6efb5c6039333105972eec49642abd5c7
F. G. Dorais <fgdorais@gmail.com>
2023-07-26 02:32:44 -0400

    fix: update test for alias

--- test/alias.lean
+++ test/alias.lean
@@ -17,9 +17,10 @@ example : 1 + 1 = 2 := foo1
 example : 1 + 1 = 2 := foo2
 example : 1 + 1 = 2 := B.foo3
 
+/-- doc string for bar -/
 def bar : Nat := 5
-alias bar1 := bas
-alias _root_.B.bar2 := 5
+alias bar1 := bar
+alias _root_.B.bar2 := bar
 example : bar1 = 5 := rfl
 example : B.bar2 = 5 := rfl
 
@@ -27,7 +28,14 @@ theorem baz (x : Nat) : x = x := rfl
 alias baz1 := baz
 example : 3 = 3 := baz1 3
 
+/-- doc string for foobar -/
 def foobar : ℕ → ℕ := id
 alias foobar1 := foobar
-def foobar2 (n : ℕ) := foobar2 n
+/-- doc string for foobar2 -/
+def foobar2 (n : ℕ) := foobar1 n
 
+/-- doc string for foobaz -/
+noncomputable def foobaz : ℕ → ℕ := id
+alias foobaz1 := foobaz
+/-- doc string for foobar2 -/
+noncomputable def foobaz2 (n : ℕ) := foobaz1 n
