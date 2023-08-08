7c5a41b45e98f856e632a1d9851f29a86dc223a3
Scott Morrison <scott.morrison@gmail.com>
2023-08-08 13:38:12 +1000

    import all

--- Std.lean
+++ Std.lean
@@ -35,6 +35,7 @@ import Std.Data.List.Basic
 import Std.Data.List.Init.Lemmas
 import Std.Data.List.Lemmas
 import Std.Data.MLList.Basic
+import Std.Data.MLList.Heartbeats
 import Std.Data.Nat.Basic
 import Std.Data.Nat.Gcd
 import Std.Data.Nat.Init.Lemmas
@@ -59,6 +60,7 @@ import Std.Data.String.Basic
 import Std.Data.String.Lemmas
 import Std.Lean.AttributeExtra
 import Std.Lean.Command
+import Std.Lean.CoreM
 import Std.Lean.Delaborator
 import Std.Lean.Expr
 import Std.Lean.Format
