c94147583c36c80f77cae53603a4c8804d016480
F. G. Dorais <fgdorais@gmail.com>
2023-08-15 05:10:53 -0400

    chore: remove explicit toSubstring

--- Std/Data/String/Basic.lean
+++ Std/Data/String/Basic.lean
@@ -81,21 +81,21 @@ namespace String
 /--
 If `pre` is a prefix of `s`, i.e. `s = pre ++ t`, returns the remainder `t`.
 -/
-def dropPrefix? (s : String) (pre : String) : Option Substring :=
-  s.toSubstring.dropPrefix? pre.toSubstring
+def dropPrefix? (s : String) (pre : Substring) : Option Substring :=
+  Substring.dropPrefix? s pre
 
 /--
 If `suff` is a suffix of `s`, i.e. `s = t ++ suff`, returns the remainder `t`.
 -/
-def dropSuffix? (s : String) (suff : String) : Option Substring :=
-  s.toSubstring.dropSuffix? suff.toSubstring
+def dropSuffix? (s : String) (suff : Substring) : Option Substring :=
+  Substring.dropSuffix? s suff
 
 /-- `s.stripPrefix pre` will remove `pre` from the beginning of `s` if it occurs there,
 or otherwise return `s`. -/
-def stripPrefix (s pre : String) : String :=
-  s.dropPrefix? pre |>.map Substring.toString |>.getD s
+def stripPrefix (s : String) (pre : Substring) : Substring :=
+  s.dropPrefix? pre |>.getD s
 
 /-- `s.stripSuffix suff` will remove `suff` from the end of `s` if it occurs there,
 or otherwise return `s`. -/
-def stripSuffix (s suff : String) : String :=
-  s.dropSuffix? suff |>.map Substring.toString |>.getD s
+def stripSuffix (s : String) (suff : Substring) : Substring :=
+  s.dropSuffix? suff |>.getD s
