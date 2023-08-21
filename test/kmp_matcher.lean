f69dc1608c115b711d8cd6901e631cfc36b86171
F. G. Dorais <fgdorais@gmail.com>
2023-08-20 23:42:01 -0400

    feat: more tests

--- test/kmp_matcher.lean
+++ test/kmp_matcher.lean
@@ -6,18 +6,22 @@ import Std.Data.String.Basic
 /-- Matcher for pattern "abba" -/
 def m := String.Matcher.ofString "abba"
 
-/-- info: false -/
-#guard_msgs in
-#eval Option.isSome <| m.find? "AbbabbA" -- false
+#guard Option.isSome (m.find? "AbbabbA") = false
 
-/-- info: true -/
-#guard_msgs in
-#eval Option.isSome <| m.find? "aabbaa" -- true
+#guard Option.isSome (m.find? "aabbaa") = true
 
-/-- info: 2 -/
-#guard_msgs in
-#eval Array.size <| m.findAll "abbabba" -- 2
+#guard Array.size (m.findAll "abbabba") = 2
 
-/-- info: 3 -/
-#guard_msgs in
-#eval Array.size <| m.findAll "abbabbabba" -- 3
+#guard Array.size (m.findAll "abbabbabba") = 3
+
+#guard Option.isSome ("xyyxx".findSubstr? "xy") = true
+
+#guard Option.isSome ("xyyxx".findSubstr? "xyx") = false
+
+#guard Array.size ("xyyxx".findAllSubstr "xyx") = 0
+
+#guard Array.size ("xyyxxyx".findAllSubstr "xyx") = 1
+
+#guard Array.size ("xyxyyxxyx".findAllSubstr "xyx") = 2
+
+#guard Array.size ("xyxyxyyxxyxyx".findAllSubstr "xyx") = 4
