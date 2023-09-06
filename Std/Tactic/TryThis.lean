16588a257d3021cf59b81c2818c0c179d196a1d6
thorimur <68410468+thorimur@users.noreply.github.com>
2023-09-06 00:10:15 -0400

    docs: authorship

--- Std/Tactic/TryThis.lean
+++ Std/Tactic/TryThis.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Gabriel Ebner. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Gabriel Ebner, Mario Carneiro
+Authors: Gabriel Ebner, Mario Carneiro, Thomas Murrills
 -/
 import Lean.Server.CodeActions
 import Lean.Widget.UserWidget
