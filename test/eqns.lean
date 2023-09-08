5e69910703413dfc2fbe41cc9aef69359615d49a
Scott Morrison <scott.morrison@gmail.com>
2023-09-08 13:37:53 +1000

    include changes from #7019

--- test/eqns.lean
+++ test/eqns.lean
@@ -1,4 +1,5 @@
 import Std.Tactic.Eqns
+import Std.Tactic.GuardMsgs
 
 set_option linter.missingDocs false
 
@@ -16,3 +17,22 @@ theorem transpose_const {m n} (c : Nat) :
   fail_if_success simp [transpose]
   funext i j
   rw [transpose]
+
+def t : Nat := 0 + 1
+
+theorem t_def : t = 1 := rfl
+-- this rw causes lean to generate equations itself for t before the user can register them
+theorem t_def' : t = 1 := by rw [t]
+
+/--
+error: There already exist stored eqns for 't' registering new equations
+will not have the desired effect.
+-/
+#guard_msgs(error) in
+attribute [eqns t_def] t
+
+-- the above should error as the above equation would not have changed the output of the below
+example (n : Nat) : t = n ∨ True := by
+  rw [t]
+  apply Or.inr
+  trivial
