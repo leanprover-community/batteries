531580e7a0633a7104badf03490c5aa12495d0df
Scott Morrison <scott.morrison@gmail.com>
2023-09-13 09:55:05 +1000

    comments

--- Std/Tactic/DummyLabelAttr.lean
+++ Std/Tactic/DummyLabelAttr.lean
@@ -1,4 +1,7 @@
 import Std.Tactic.LabelAttr
 
--- We register a dummy here, so it can be used in testing.
+/-- A dummy label attribute, which can be used for testing. -/
+-- This can't live in `Std.Tactic.LabelAttr`
+-- (because we can't use the extension in the same file it is initialized)
+-- and it can't live in `test/`, because files there can not import other files in `test/`.
 register_label_attr dummy_label_attr
