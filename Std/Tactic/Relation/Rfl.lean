f2eedebbecdb9a9554ea7c4c19762a1abae167c2
Scott Morrison <scott.morrison@gmail.com>
2023-09-13 10:33:53 +1000

    fix imports

--- Std/Tactic/Relation/Rfl.lean
+++ Std/Tactic/Relation/Rfl.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Newell Jensen, Thomas Murrills
 -/
 import Std.Lean.Elab.Tactic
+import Lean.Meta.Tactic.Apply
 
 /-!
 # `rfl` tactic extension for reflexive relations
