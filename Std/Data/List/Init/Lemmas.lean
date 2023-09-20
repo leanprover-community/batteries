d47786f646871a1612fda338f39839a3f2b4e43f
Scott Morrison <scott.morrison@gmail.com>
2023-09-20 13:26:21 +1000

    move lemma to new file Std.Data.Fin.Init

--- Std/Data/List/Init/Lemmas.lean
+++ Std/Data/List/Init/Lemmas.lean
@@ -3,10 +3,7 @@ Copyright (c) 2014 Parikshit Khanna. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Parikshit Khanna, Jeremy Avigad, Leonardo de Moura, Floris van Doorn, Mario Carneiro
 -/
-
--- TODO: I'm not sure where this should live.
--- It is needed below so that `List.get_cons_zero` fires.
-@[simp] theorem Fin.zero_eta : (⟨0, Nat.zero_lt_succ _⟩ : Fin (n + 1)) = 0 := rfl
+import Std.Data.Fin.Init
 
 namespace List
 
