6cff6ef8689e269805aff975fb9ca6fb0ec00a39
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2023-10-05 00:06:31 -0400

    chore: restore default capacity to 8

--- Std/Data/HashMap/Basic.lean
+++ Std/Data/HashMap/Basic.lean
@@ -253,7 +253,7 @@ def _root_.Std.HashMap (α : Type u) (β : Type v) [BEq α] [Hashable α] := {m 
 open HashMap.Imp
 
 /-- Make a new hash map with the specified capacity. -/
-@[inline] def _root_.Std.mkHashMap [BEq α] [Hashable α] (capacity := 0) : HashMap α β :=
+@[inline] def _root_.Std.mkHashMap [BEq α] [Hashable α] (capacity := 8) : HashMap α β :=
   ⟨.empty capacity, .empty⟩
 
 instance [BEq α] [Hashable α] : Inhabited (HashMap α β) where
