ca821164d213c50229b6ae1cbce2e400cc780591
Mario Carneiro <di.gama@gmail.com>
2023-10-09 20:51:22 -0400

    chore: use functions from #282

--- Std/Lean/Json.lean
+++ Std/Lean/Json.lean
@@ -1,35 +1,30 @@
 /-
  Copyright (c) 2022 E.W.Ayers. All rights reserved.
  Released under Apache 2.0 license as described in the file LICENSE.
- Authors: E.W.Ayers
+ Authors: E.W.Ayers, Mario Carneiro
 -/
 
 import Lean.Data.Json
+import Std.Lean.Float
 
 open Lean
 
 instance : OfScientific JsonNumber where
   ofScientific mantissa exponentSign decimalExponent :=
     if exponentSign then
-      {mantissa := mantissa, exponent := decimalExponent}
+      { mantissa := mantissa, exponent := decimalExponent }
     else
-      {mantissa := (mantissa * 10 ^ decimalExponent : Nat), exponent := 0}
+      { mantissa := (mantissa * 10 ^ decimalExponent : Nat), exponent := 0 }
 
 instance : Neg JsonNumber where
-  neg jn := ⟨- jn.mantissa, jn.exponent⟩
+  neg jn := ⟨-jn.mantissa, jn.exponent⟩
 
 instance : ToJson Float where
   toJson x :=
-    if x == 0.0 then 0 else
-    let s := toString (if x < 0.0 then - x else x)
-    match Lean.Syntax.decodeScientificLitVal? <| s with
-    | none =>
-      match s with
-      | "inf" => "inf" -- [todo] emit a warning
-      | "-inf" => "-inf"
-      | "nan" => "nan"
-      | _ => panic! s!"unhandled float string {s}"
-    | some (m, e, de) =>
-      let j : JsonNumber := OfScientific.ofScientific m e de
-      let j := if x < 0.0 then -j else j
-      Json.num j
+    match x.toRatParts' with
+    | none => Json.null
+    | some (n, d) =>
+      if d < 0 then
+        Json.num { mantissa := n * (5^d.natAbs : Nat), exponent := d.natAbs }
+      else
+        Json.num { mantissa := n * (2^d.natAbs : Nat), exponent := 0 }
