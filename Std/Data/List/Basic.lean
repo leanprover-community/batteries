334044a0adafb9189493f53a8606ba6f85356cca
Scott Morrison <scott.morrison@gmail.com>
2023-10-15 14:06:26 +1100

    feat: List.zipWithAll, that continues to the end of both lists

--- Std/Data/List/Basic.lean
+++ Std/Data/List/Basic.lean
@@ -1460,6 +1460,26 @@ zipRight = zipWithRight prod.mk
 @[inline] def zipRight : List α → List β → List (Option α × β) := zipWithRight Prod.mk
 
 /--
+Version of `List.zipWith` that continues to the end of both lists, passing `none` to one argument
+once the shorter list has run out.
+-/
+def zipWithAll (f : Option α → Option β → γ) : List α → List β → List γ
+  | [], bs => bs.map fun b => f none (some b)
+  | a :: as, [] => (a :: as).map fun a => f (some a) none
+  | a :: as, b :: bs => f a b :: zipWithAll f as bs
+
+@[simp] theorem zipWithAll_nil_right :
+    zipWithAll f as [] = as.map fun a => f (some a) none := by
+  cases as <;> rfl
+
+@[simp] theorem zipWithAll_nil_left :
+    zipWithAll f [] bs = bs.map fun b => f none (some b) := by
+  rw [zipWithAll]
+
+@[simp] theorem zipWithAll_cons_cons :
+    zipWithAll f (a :: as) (b :: bs) = f (some a) (some b) :: zipWithAll f as bs := rfl
+
+/--
 If all elements of `xs` are `some xᵢ`, `allSome xs` returns the `xᵢ`. Otherwise
 it returns `none`.
 ```
