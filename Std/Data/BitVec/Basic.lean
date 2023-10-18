894b0d0607dfd6d2d41607258dbc948199156281
Joe Hendrix <joe@lean-fro.org>
2023-10-18 11:10:46 -0700

    chore: Add SMTLIB compatible shifts; fix documentation mistake.

--- Std/Data/BitVec/Basic.lean
+++ Std/Data/BitVec/Basic.lean
@@ -53,7 +53,7 @@ protected def toNat (a : BitVec n) : Nat := a.toFin.val
 /-- Return the `i`-th least significant bit. -/
 @[inline] def getLsb (x : BitVec w) (i : Nat) : Bool := x.toNat &&& (1 <<< i) != 0
 
-/-- Return the `i`-th least significant bit. -/
+/-- Return the `i`-th most significant bit. -/
 @[inline] def getMsb (x : BitVec w) (i : Nat) : Bool := getLsb x (w-1-i)
 
 /-- Return most-significant bit in bitvector. -/
@@ -310,7 +310,7 @@ instance : Complement (BitVec w) := ⟨.not⟩
 Left shift for bit vectors. The low bits are filled with zeros. As a numeric operation, this is
 equivalent to `a * 2^s`, modulo `2^n`.
 
-SMT-Lib name: `bvshl`.
+See `smtShl` for an SMT-Lib compatible version.
 -/
 protected def shiftLeft (a : BitVec n) (s : Nat) : BitVec n := .ofNat n (a.toNat <<< s)
 instance : HShiftLeft (BitVec w) Nat (BitVec w) := ⟨.shiftLeft⟩
@@ -319,19 +319,43 @@ instance : HShiftLeft (BitVec w) Nat (BitVec w) := ⟨.shiftLeft⟩
 (Logical) right shift for bit vectors. The high bits are filled with zeros.
 As a numeric operation, this is equivalent to `a / 2^s`, rounding down.
 
-SMT-Lib name: `bvlshr`.
+SMT-Lib name: `bvshl`.
+-/
+def smtShl (a : BitVec n) (s : BitVec n) : BitVec n := .shiftLeft a s.toNat
+
+/--
+(Logical) right shift for bit vectors. The high bits are filled with zeros.
+As a numeric operation, this is equivalent to `a / 2^s`, rounding down.
+
+See `smtLShr` for an SMT-Lib compatible version.
 -/
 def ushiftRight (a : BitVec n) (s : Nat) : BitVec n := .ofNat n (a.toNat >>> s)
 instance : HShiftRight (BitVec w) Nat (BitVec w) := ⟨.ushiftRight⟩
+
+/--
+(Logical) right shift for bit vectors. The high bits are filled with zeros.
+As a numeric operation, this is equivalent to `a / 2^s`, rounding down.
+
+SMT-Lib name: `bvlshr`.
+-/
+def smtLShr (a : BitVec n) (s : BitVec n) : BitVec n := ushiftRight a s.toNat
 
 /--
 Arithmetic right shift for bit vectors. The high bits are filled with the
 most-significant bit.
 As a numeric operation, this is equivalent to `a.toInt >>> s`.
 
-SMT-Lib name: `bvashr`.
+See `smtAShr` for an SMT-Lib compatible version.
 -/
 def sshiftRight (a : BitVec n) (s : Nat) : BitVec n := .ofInt n (a.toInt >>> s)
+
+/--
+(Logical) right shift for bit vectors. The high bits are filled with zeros.
+As a numeric operation, this is equivalent to `a / 2^s`, rounding down.
+
+SMT-Lib name: `bvashr`.
+-/
+def smtAShr (a : BitVec n) (s : BitVec n) : BitVec n := sshiftRight a s.toNat
 
 /--
 Rotate left for bit vectors. All the bits of `x` are shifted to higher positions, with the top `n`
