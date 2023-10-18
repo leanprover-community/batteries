5cf432260fc405dac7fa0737df2164831687927d
Joe Hendrix <jhendrix@whoisjoe.info>
2023-10-17 18:56:40 -0700

    Remove Std/Data/BitVec.lean copyright header.
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- Std/Data/BitVec.lean
+++ Std/Data/BitVec.lean
@@ -1,7 +1,1 @@
-/-
-Copyright (c) 2022 by the authors listed in the file AUTHORS and their
-institutional affiliations. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joe Hendrix, Wojciech Nawrocki, Leonardo de Moura
--/
 import Std.Data.BitVec.Basic
