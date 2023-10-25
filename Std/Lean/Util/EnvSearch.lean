488f4547c25d5415c99b869c1807be924ac3232c
Joe Hendrix <joe@lean-fro.org>
2023-10-25 11:36:29 -0700

    feat: Migrate #print prefix from Mathlib

---
+++ Std/Lean/Util/EnvSearch.lean
@@ -0,0 +1,41 @@
+/-
+Copyright (c) 2021 Shing Tak Lam. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Shing Tak Lam, Daniel Selsam, Mario Carneiro
+-/
+
+import Lean.Modifiers
+
+open Lean
+
+namespace Lean
+
+/--
+Options to control getMatchingConstants options below.
+-/
+structure EnvironmentSearchOptions where
+  /-- Include declarations in imported environment. -/
+  stage1       : Bool := true
+  /-- Include private declarations. -/
+  checkPrivate : Bool := false
+
+/--
+Find constants in current environment that match find options and predicate.
+-/
+def getMatchingConstants {m} [Monad m] [MonadEnv m] (p : ConstantInfo → m Bool) (opts : EnvironmentSearchOptions := {}) : m (Array ConstantInfo) := do
+  let matches_ ← if opts.stage1 then
+                    (← getEnv).constants.map₁.foldM (init := #[]) check
+                   else
+                    pure #[]
+  (← getEnv).constants.map₂.foldlM (init := matches_) check
+where
+  check matches_ name cinfo := do
+    if opts.checkPrivate || !isPrivateName name then
+      if ← p cinfo then
+        pure $ matches_.push cinfo
+      else
+        pure matches_
+    else
+      pure matches_
+
+end Lean
---
+++ Std/Tactic/PrintPrefix.lean
@@ -0,0 +1,48 @@
+/-
+Copyright (c) 2021 Shing Tak Lam. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Shing Tak Lam, Daniel Selsam, Mario Carneiro
+-/
+import Std.Lean.Util.EnvSearch
+import Lean.Elab.Command
+
+open Lean Meta Elab Command
+
+namespace Lean
+
+
+namespace Elab.Command
+
+
+private def appendMatchingConstants (msg : String)
+    (ϕ : ConstantInfo → MetaM Bool) (opts : EnvironmentSearchOptions := {}) : MetaM String := do
+  let cinfos ← getMatchingConstants ϕ opts
+  let cinfos := cinfos.qsort fun p q ↦ p.name.lt q.name
+  let mut msg := msg
+  for cinfo in cinfos do
+    msg := msg ++ s!"{cinfo.name} : {← Meta.ppExpr cinfo.type}\n"
+  pure msg
+
+/--
+Command for #print prefix
+-/
+syntax (name := printPrefix) "#print prefix " ident : command
+
+/--
+The command `#print prefix foo` will print all definitions that start with
+the namespace `foo`.
+-/
+@[command_elab printPrefix] def elabPrintPrefix : CommandElab
+| `(#print prefix%$tk $name:ident) => do
+  let nameId := name.getId
+  liftTermElabM do
+    let mut msg ← appendMatchingConstants "" fun cinfo ↦ pure $ nameId.isPrefixOf cinfo.name
+    if msg.isEmpty then
+      if let [name] ← resolveGlobalConst name then
+        msg ← appendMatchingConstants msg fun cinfo ↦ pure $ name.isPrefixOf cinfo.name
+    if !msg.isEmpty then
+      logInfoAt tk msg
+| _ => throwUnsupportedSyntax
+
+end Elab.Command
+end Lean
