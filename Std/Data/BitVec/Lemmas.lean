e9dd554a2a00cc8af06257e134274f6a3461a356
Alex Keizer <alex@keizer.dev>
2023-11-09 20:50:31 +0100

    Fix whitespace
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Std/Data/BitVec/Lemmas.lean
+++ Std/Data/BitVec/Lemmas.lean
@@ -12,8 +12,8 @@ variable {w v : Nat}
 
 /-! We add simp-lemmas that rewrite bitvector operations into the equivalent notation -/
 @[simp] theorem append_eq (x : BitVec w) (y : BitVec v)   : BitVec.append x y = x ++ y        := rfl
-@[simp] theorem shiftLeft_eq (x : BitVec w) (n: Nat)      : BitVec.shiftLeft x n = x <<< n    := rfl
-@[simp] theorem ushiftRight_eq (x : BitVec w) (n: Nat)    : BitVec.ushiftRight x n = x >>> n  := rfl
+@[simp] theorem shiftLeft_eq (x : BitVec w) (n : Nat)     : BitVec.shiftLeft x n = x <<< n    := rfl
+@[simp] theorem ushiftRight_eq (x : BitVec w) (n : Nat)   : BitVec.ushiftRight x n = x >>> n  := rfl
 @[simp] theorem not_eq (x : BitVec w)                     : BitVec.not x = ~~~x               := rfl
 @[simp] theorem and_eq (x y : BitVec w)                   : BitVec.and x y = x &&& y          := rfl
 @[simp] theorem or_eq (x y : BitVec w)                    : BitVec.or x y = x ||| y           := rfl
