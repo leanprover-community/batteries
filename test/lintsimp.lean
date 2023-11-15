bd49c40aa168d1bfca97715ba898b5a5a1a977be
Mario Carneiro <di.gama@gmail.com>
2023-11-15 13:47:55 -0500

    revert test

--- test/lintsimp.lean
+++ test/lintsimp.lean
@@ -9,8 +9,8 @@ def h : Nat := 0
 @[simp] theorem fh : f = h := rfl
 #eval do guard (← [``fg, ``fh].anyM fun n => return (← simpNF.test n).isSome)
 
-@[simp] theorem and_comm' : a ∧ b ↔ b ∧ a := And.comm
-#eval do guard (← simpComm.test ``and_comm').isSome
+@[simp] theorem and_comm : a ∧ b ↔ b ∧ a := And.comm
+#eval do guard (← simpComm.test ``and_comm).isSome
 
 @[simp] theorem Prod.mk_fst : (a, b).1 = id a := rfl
 #eval do guard (← simpVarHead.test ``Prod.mk_fst).isSome
