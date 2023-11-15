6fefc0d48e63df57876cdc7dc4a2369923b28208
Mario Carneiro <di.gama@gmail.com>
2023-11-15 13:49:11 -0500

    move to Array namespace

--- Std/Data/Array/Basic.lean
+++ Std/Data/Array/Basic.lean
@@ -106,6 +106,17 @@ protected def maxI [ord : Ord α] [Inhabited α]
     (xs : Array α) (start := 0) (stop := xs.size) : α :=
   xs.minI (ord := ord.opposite) start stop
 
+/--
+Unsafe implementation of `attach`, taking advantage of the fact that the representation of
+`Array {x // x ∈ xs}` is the same as the input `Array α`.
+-/
+@[inline] private unsafe def attachImpl (xs : Array α) : Array {x // x ∈ xs} := unsafeCast xs
+
+/-- "Attach" the proof that the elements of `xs` are in `xs` to produce a new list
+  with the same elements but in the type `{x // x ∈ xs}`. -/
+@[implemented_by attachImpl] def attach (xs : Array α) : Array {x // x ∈ xs} :=
+  ⟨xs.data.pmap Subtype.mk fun _ => Array.Mem.mk⟩
+
 end Array
 
 
@@ -158,14 +169,3 @@ def popHead? (as : Subarray α) : Option (α × Subarray α) :=
       none
 
 end Subarray
-
-/--
-Unsafe implementation of `attach`, taking advantage of the fact that the representation of
-`Array {x // x ∈ xs}` is the same as the input `Array α`.
--/
-@[inline] private unsafe def attachImpl (xs : Array α) : Array {x // x ∈ xs} := unsafeCast xs
-
-/-- "Attach" the proof that the elements of `xs` are in `xs` to produce a new list
-  with the same elements but in the type `{x // x ∈ xs}`. -/
-@[implemented_by attachImpl] def attach (xs : Array α) : Array {x // x ∈ xs} :=
-  ⟨xs.data.pmap Subtype.mk fun _ => Array.Mem.mk⟩
