56f78e780fec35fa49e55e6c8368f1e0d7a181a9
F. G. Dorais <fgdorais@gmail.com>
2023-11-19 06:51:24 -0500

    chore: remove test file

--- test/fin_fold.lean
+++
@@ -1,5 +0,0 @@
-import Std.Data.Fin.Basic
-import Std.Tactic.GuardExpr
-
-#guard Fin.foldl 4 (fun x i => List.cons i x) [] == [3,2,1,0]
-#guard Fin.foldr 4 (fun i x => List.cons i x) [] == [0,1,2,3]
