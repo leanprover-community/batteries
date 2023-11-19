13efdf96f83adfb94be5708f80667c6a521d6ff2
François G. Dorais <fgdorais@gmail.com>
2023-11-19 06:38:22 -0500

    fix: import
    Co-authored-by: Scott Morrison <scott@tqft.net>

--- test/fin_fold.lean
+++ test/fin_fold.lean
@@ -1,5 +1,5 @@
 import Std.Data.Fin.Basic
-import Std.Tactic.GuardMsgs
+import Std.Tactic.GuardExpr
 
 #guard Fin.foldl 4 (fun x i => List.cons i x) [] == [3,2,1,0]
 #guard Fin.foldr 4 (fun i x => List.cons i x) [] == [0,1,2,3]
