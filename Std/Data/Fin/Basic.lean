1e73dcce9373110ea07484cf89b052b301697520
François G. Dorais <fgdorais@gmail.com>
2023-11-19 06:38:40 -0500

    fix: import
    Co-authored-by: Scott Morrison <scott@tqft.net>

--- Std/Data/Fin/Basic.lean
+++ Std/Data/Fin/Basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Y. Lewis, Keeley Hoek, Mario Carneiro
 -/
 import Std.Data.Nat.Init.Lemmas
-import Std.Tactic.Lint
 
 namespace Fin
 
