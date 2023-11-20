d00dfb71add9eaf255662b28e78d91ea9288cc0b
Mario Carneiro <di.gama@gmail.com>
2023-11-20 01:33:13 -0500

    style

--- Std/Control/Lemmas.lean
+++ Std/Control/Lemmas.lean
@@ -10,10 +10,10 @@ namespace ReaderT
 attribute [ext] ReaderT.ext
 
 @[simp] theorem run_failure [Monad m] [Alternative m] (ctx : ρ) :
-  (failure : ReaderT ρ m α).run ctx = failure := rfl
+    (failure : ReaderT ρ m α).run ctx = failure := rfl
 
 @[simp] theorem run_orElse [Monad m] [Alternative m] (x y : ReaderT ρ m α) (ctx : ρ) :
-  (x <|> y).run ctx = (x.run ctx <|> y.run ctx) := rfl
+    (x <|> y).run ctx = (x.run ctx <|> y.run ctx) := rfl
 
 end ReaderT
 
@@ -22,9 +22,9 @@ namespace StateT
 attribute [ext] StateT.ext
 
 @[simp] theorem run_failure {α σ} [Monad m] [Alternative m] (s : σ) :
-  (failure : StateT σ m α).run s = failure := rfl
+    (failure : StateT σ m α).run s = failure := rfl
 
 @[simp] theorem run_orElse {α σ} [Monad m] [Alternative m] (x y : StateT σ m α) (s : σ) :
-  (x <|> y).run s = (x.run s <|> y.run s) := rfl
+    (x <|> y).run s = (x.run s <|> y.run s) := rfl
 
 end StateT
