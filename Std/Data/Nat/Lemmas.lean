42b918902e74bdb981a35be810aa691cfdeca11d
F. G. Dorais <fgdorais@gmail.com>
2023-11-21 07:02:00 -0500

    fix: revert accidental edit

--- Std/Data/Nat/Lemmas.lean
+++ Std/Data/Nat/Lemmas.lean
@@ -1054,12 +1054,11 @@ theorem le_log2 (h : n ≠ 0) : k ≤ n.log2 ↔ 2 ^ k ≤ n := by
   match k with
   | 0 => simp [show 1 ≤ n from Nat.pos_of_ne_zero h]
   | k+1 =>
-    rw [log2]
-    split
+    rw [log2]; split
     · have n0 : 0 < n / 2 := (Nat.le_div_iff_mul_le (by decide)).2 ‹_›
       simp only [Nat.add_le_add_iff_right, le_log2 (Nat.ne_of_gt n0), le_div_iff_mul_le,
         Nat.pow_succ]
-      exact le_div_iff_mul_le (by decide)
+      exact Nat.le_div_iff_mul_le (by decide)
     · simp only [le_zero_eq, succ_ne_zero, false_iff]
       refine mt (Nat.le_trans ?_) ‹_›
       exact Nat.pow_le_pow_of_le_right (Nat.succ_pos 1) (Nat.le_add_left 1 k)
