fc41cb6abd25a4f4c1eebb1eb71dea52a490db86
Eric Wieser <wieser.eric@gmail.com>
2023-12-04 15:53:02 +0000

    define not in terms of a xor + bit shift

--- Std/Data/BitVec/Basic.lean
+++ Std/Data/BitVec/Basic.lean
@@ -310,7 +310,10 @@ Bitwise NOT for bit vectors.
 ```
 SMT-Lib name: `bvnot`.
 -/
-protected def not (x : BitVec n) : BitVec n := -(x + .ofNat n 1)
+protected def not (x : BitVec n) : BitVec n :=
+  .ofFin ⟨(1 <<< n).pred,
+    n.one_shiftLeft.symm ▸
+      (Nat.pred_lt <| Nat.ne_of_gt <| Nat.pos_pow_of_pos _ <| Nat.zero_lt_succ _)⟩ ^^^ x
 instance : Complement (BitVec w) := ⟨.not⟩
 
 /-- The `BitVec` with value `(2^n + (i mod 2^n)) mod 2^n`.  -/
