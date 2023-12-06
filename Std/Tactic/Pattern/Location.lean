6e2ba3c41e46896ecaf54a4637a9f3a210dccb75
ART0 <18333981+0Art0@users.noreply.github.com>
2023-12-06 16:49:35 +0000

    Docs for `Pattern/Location`

--- Std/Tactic/Pattern/Location.lean
+++ Std/Tactic/Pattern/Location.lean
@@ -19,25 +19,40 @@ This file is modelled on `Lean/Elab/Tactic/Location.lean`.
 
 -/
 
-
 open Lean Meta Elab Tactic
 
 namespace Pattern.Location
 
-syntax hypTarget := ("at" <|> "in") ppSpace term
-syntax goalTarget := "at" ppSpace patternIgnore( atomic("|" noWs "-") <|> "⊢")
-syntax wildcardTarget := "at" ppSpace "*"
+/-- A hypothesis location.
+    Locations of the form
+    `at h` refer to hypothesis types,
+    while those of the form
+    `in h` refer to `let` hypothesis values. -/
+syntax hypLoc:= ("at" <|> "in") ppSpace term
 
-syntax loc := (Parser.Tactic.Conv.occs)? ppSpace (hypTarget <|> goalTarget)
-syntax locs := withPosition(wildcardTarget <|> (ppSpace colGt loc)+)
+/-- The goal or target location. -/
+syntax goalLoc := "at" ppSpace patternIgnore( atomic("|" noWs "-") <|> "⊢")
 
+/-- The wildcard location which refers to all valid locations. -/
+syntax wildcardLoc := "at" ppSpace "*"
+
+/-- A location in the goal state with a list of occurrences. -/
+syntax loc := (Parser.Tactic.Conv.occs)? ppSpace (hypLoc <|> goalLoc)
+
+/-- A collection of locations (see `loc`). -/
+syntax locs := withPosition(wildcardLoc <|> (ppSpace colGt loc)+)
+
+/-- Occurrences at a goal location. This is similar to `SubExpr.GoalLocation`. -/
 inductive GoalOccurrences
+  /-- Occurrences in the type of a hypothesis. -/
   | hypType (fvarId : FVarId) (occs : Occurrences)
+  /-- Occurrences in the value of a `let` hypothesis. -/
   | hypValue (fvarId : FVarId) (occs : Occurrences)
+  /-- Occurrences in the target. -/
   | target (occs : Occurrences)
 
 open Parser Tactic Conv in
-/-- Expand syntax of type `occs` into `Occurrences`. -/
+/-- Interpret `occs` syntax as `Occurrences`. -/
 def expandOccs : Option (TSyntax ``occs) → Occurrences
   | none => .all
   | some occs =>
@@ -47,25 +62,31 @@ def expandOccs : Option (TSyntax ``occs) → Occurrences
       | _ => panic! s!"Invalid syntax {occs} for occurrences."
 
 open Parser Tactic Conv in
+/-- Interpret `loc` syntax as `GoalOccurrences`. -/
 def expandLoc : TSyntax ``loc → TacticM GoalOccurrences
   | `(loc| $[$occs?]? at $hyp:term) => withMainContext do
     return .hypType (← getFVarId hyp) (expandOccs occs?)
   | `(loc| $[$occs?]? in $hyp:term) => withMainContext do
     return .hypValue (← getFVarId hyp) (expandOccs occs?)
-  | `(loc| $[$occs?]? $_:goalTarget) => do
+  | `(loc| $[$occs?]? $_:goalLoc) => do
     return .target (expandOccs occs?)
   | _ => throwUnsupportedSyntax
 
+/-- Interpret `locs` syntax as an array of `GoalOccurrences`. -/
 def expandLocs : TSyntax ``locs → TacticM (Array GoalOccurrences)
-  | `(locs| $_:wildcardTarget) => withMainContext do
+  | `(locs| $_:wildcardLoc) => withMainContext do
+    -- TODO: Maybe reverse the list of declarations, following `Lean/Elab/Tactic/Location.lean`
     (← getLCtx).decls.foldlM (init := #[.target .all]) fun goalOccs decl? => do
       match decl? with
       | some decl =>
-        let goalOccs :=
-          if decl.isLet then
-            goalOccs.push <| .hypValue decl.fvarId .all
-          else goalOccs
-        return goalOccs.push <| .hypType decl.fvarId .all
+        if decl.isImplementationDetail then
+          return goalOccs
+        else
+          let goalOccs :=
+            if decl.isLet then
+              goalOccs.push <| .hypValue decl.fvarId .all
+            else goalOccs
+          return goalOccs.push <| .hypType decl.fvarId .all
       | none => return goalOccs
   | `(locs| $[$locs]*) => locs.mapM expandLoc
   | _ => throwUnsupportedSyntax
@@ -74,35 +95,48 @@ end Pattern.Location
 
 open Pattern.Location
 
-def substitute (e : Expr) (pattern : AbstractMVarsResult) (occs : Occurrences) (replacement : Expr → MetaM Expr) : MetaM Expr := do
+/-- Substitute occurrences of a pattern in an expression with the result of `replacement`. -/
+def substitute (e : Expr) (pattern : AbstractMVarsResult) (occs : Occurrences)
+    (replacement : Expr → MetaM Expr) : MetaM Expr := do
   let (_, _, p) ← openAbstractMVarsResult pattern
   let eAbst ← kabstract e p occs
-  return Expr.instantiate1 eAbst (← replacement p)
+  unless eAbst.hasLooseBVars do
+    throwError m!"Failed to find instance of pattern {indentExpr p} in {indentExpr e}."
+  instantiateMVars <| Expr.instantiate1 eAbst (← replacement p)
 
--- modified from `Lean/Meta/Tactic/Replace/_root_.Lean.MVarId.replaceLocalDeclDefEq`
-def _root_.Lean.MVarId.replaceLocalLetDefEq (mvarId : MVarId) (fvarId : FVarId) (valNew : Expr) : MetaM MVarId := do
+/-- Replace the value of a local `let` hypothesis with `valNew`,
+    which is assumed to be definitionally equal.
+
+    This function is modified from `_root_.Lean.MVarId.replaceLocalDeclDefEq` in
+    `Lean/Meta/Tactic/Replace.lean`. -/
+def _root_.Lean.MVarId.replaceLocalLetDefEq (mvarId : MVarId) (fvarId : FVarId)
+    (valNew : Expr) : MetaM MVarId := do
   mvarId.withContext do
     if valNew == (← fvarId.getValue?) then
       return mvarId
     else
       let mvarDecl ← mvarId.getDecl
       let lctxNew := (← getLCtx).modifyLocalDecl fvarId (·.setValue valNew)
-      let mvarNew ← mkFreshExprMVarAt lctxNew (← getLocalInstances) mvarDecl.type mvarDecl.kind mvarDecl.userName
+      let mvarNew ← mkFreshExprMVarAt lctxNew (← getLocalInstances)
+                      mvarDecl.type mvarDecl.kind mvarDecl.userName
       mvarId.assign mvarNew
       return mvarNew.mvarId!
 
-def replaceOccurrencesDefEq (locs : Array GoalOccurrences)
-    (pattern : AbstractMVarsResult) (replacement : Expr → MetaM Expr) : TacticM Unit := withMainContext do
+/-- Replace a pattern at the specified locations with the value of `replacement`,
+    which is assumed to be definitionally equal to the original pattern. -/
+def replaceOccurrencesDefEq (locs : Array GoalOccurrences) (pattern : AbstractMVarsResult)
+    (replacement : Expr → MetaM Expr) : TacticM Unit := withMainContext do
   let mut mvarId ← getMainGoal
-  for loc in locs do -- TODO: Get the `locs` in the right order (see `withLocation`) and filter out implementation details
+  for loc in locs do
     match loc with
     | .hypType fvarId occs =>
       let hypType ← fvarId.getType
       mvarId ← mvarId.replaceLocalDeclDefEq fvarId <| ← substitute hypType pattern occs replacement
     | .hypValue fvarId occs =>
-      let .some hypValue ← fvarId.getValue? | throwError m!"Hypothesis {fvarId.name} is not a let-declaration."
+      let .some hypValue ← fvarId.getValue? |
+        throwError m!"Hypothesis {fvarId.name} is not a `let`-declaration."
       mvarId ← mvarId.replaceLocalLetDefEq fvarId <| ← substitute hypValue pattern occs replacement
     | .target occs => do
-      let targetType ← mvarId.getType'
+      let targetType ← mvarId.getType
       mvarId ← mvarId.replaceTargetDefEq <| ← substitute targetType pattern occs replacement
   replaceMainGoal [mvarId]
