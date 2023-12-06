16204270df4262c30e0a0a9a902cfad8ccfd048a
Joe Hendrix <joe@lean-fro.org>
2023-12-06 10:41:07 -0800

    feat: Control warningAsError (default off)

--- lakefile.lean
+++ lakefile.lean
@@ -2,13 +2,26 @@ import Lake
 
 open Lake DSL
 
+/-- Turn a config option into a Boolean -/
+def enabled (val : Option String) (default : Bool := false) : Bool :=
+  match val with
+  | .some "off" => false
+  | .some "false" => false
+  | .some "on" => true
+  | .some "true" => true
+  | .some _ => not default -- This allows -K=opt to flip the default.
+  | _ => default
+
+-- Selectively choose an option if feature is enabled.
+macro "optArg?" x:ident ws d:ident v:term : term =>
+  `(if enabled (get_config? $x) $d then $v else default)
+
 package std where
-  moreLeanArgs := #["-DwarningAsError=true"]
-  leanOptions := Id.run do
-    let mut opts := #[⟨`linter.missingDocs, true⟩]
-    if get_config? disable_new_compiler |>.isSome then
-      opts := opts.push ⟨`compiler.enableNew, false⟩
-    opts
+  moreLeanArgs :=
+    optArg? werror false #["-DwarningAsError=true"]
+  leanOptions :=
+    #[⟨`linter.missingDocs, true⟩] ++
+    optArg? disable_new_compiler true #[⟨`compiler.enableNew, false⟩]
 
 @[default_target]
 lean_lib Std
@@ -18,5 +31,5 @@ lean_exe runLinter where
   srcDir := "scripts"
   supportInterpreter := true
 
-meta if get_config? doc = some "on" then
+meta if enabled (get_config? doc) then
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
