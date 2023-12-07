4feb2d3e06c2548535d090cab2afe6ddb5bc9fea
Joe Hendrix <joe@lean-fro.org>
2023-12-06 18:43:40 -0800

    Simplify

--- lakefile.lean
+++ lakefile.lean
@@ -2,26 +2,14 @@ import Lake
 
 open Lake DSL
 
-/-- Turn a config option into a Boolean -/
-def enabled (val : Option String) (default : Bool := false) : Bool :=
-  match val with
-  | .some "off" => false
-  | .some "false" => false
-  | .some "on" => true
-  | .some "true" => true
-  | .some _ => not default -- This allows -K=opt to flip the default.
-  | _ => default
-
--- Selectively choose an option if feature is enabled.
-macro "optArg?" x:ident ws d:ident v:term : term =>
-  `(if enabled (get_config? $x) $d then $v else default)
+macro "optArg?" x:ident v:term : term =>
+  `(if get_config? $x |>.isSome then $v else default)
 
 package std where
-  moreLeanArgs :=
-    optArg? werror false #["-DwarningAsError=true"]
+  moreLeanArgs := optArg? werror #["-DwarningAsError=true"]
   leanOptions :=
     #[⟨`linter.missingDocs, true⟩] ++
-    optArg? disable_new_compiler true #[⟨`compiler.enableNew, false⟩]
+    optArg? disable_new_compiler #[⟨`compiler.enableNew, false⟩]
 
 @[default_target]
 lean_lib Std
@@ -31,5 +19,5 @@ lean_exe runLinter where
   srcDir := "scripts"
   supportInterpreter := true
 
-meta if enabled (get_config? doc) then
+meta if get_config? doc |>.isSome then
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
