21fb26fec4c5175d974cd73bff31447e6b01b939
ART0 <18333981+0Art0@users.noreply.github.com>
2023-12-07 17:35:01 +0000

    Clean up `unfold` tactic

--- Std/Tactic/Pattern/Unfold.lean
+++ Std/Tactic/Pattern/Unfold.lean
@@ -15,18 +15,17 @@ An implementation of a tactic to unfold patterns at specified locations.
 
 -/
 
-section
-
 /-- If the expression is a projection under binders, calculate the projection. -/
-partial def reduceProjection (e : Expr) : ExceptT MessageData MetaM Expr :=
-  e.withAppRev fun f revArgs => match f with
+partial def reduceProjection (e : Expr) : MetaM Expr :=
+  e.withAppRev fun f revArgs =>
+    match f with
     | .proj _ i b => do
-      let some value ← project? b i | throw m!"Could not project expression {b}."
+      let some value ← project? b i | throwError m!"Could not project expression {b}."
       reduceProjection (value.betaRev revArgs)
     | _ => return e
 
 /-- Replace a constant under binders by its definition (also taking care of let reductions). -/
-def replaceByDefAux (e : Expr) : ExceptT MessageData MetaM Expr := do
+def replaceByDef (e : Expr) : MetaM Expr := do
   if let .letE _ _ v b _ := e then return b.instantiate1 v
   e.withAppRev fun f revArgs => match f with
     | .const name us => do
@@ -35,42 +34,29 @@ def replaceByDefAux (e : Expr) : ExceptT MessageData MetaM Expr := do
       if ← isDefEq result f then
         reduceProjection (result.betaRev revArgs)
       else
-        throw m!"Could not replace {f} by its definition."
+        throwError m!"Could not replace {f} by its definition."
     | _ => do
       let result ← reduceProjection (f.betaRev revArgs)
-      if result == e then throw m!"Could not find a definition for {e}."
+      if result == e then throwError m!"Could not find a definition for {e}."
       else return result
 
-/-- Replace the pattern by its definition at the specified occurrences in the expression. -/
-def replaceByDef (e : Expr) (pattern : AbstractMVarsResult) (occs : Occurrences) : MetaM Expr := do
-  let (_, _, p) ← openAbstractMVarsResult pattern
-  let eAbst ← kabstract e p occs
-  unless eAbst.hasLooseBVars do
-    throwError m!"Failed to find instance of pattern {indentExpr p} in {indentExpr e}."
-  match ← replaceByDefAux p with
-  | .ok q =>
-    let e' := eAbst.instantiate1 q
-    instantiateMVars e'
-  | .error err => throwError err
-
-end
-
-open Parser Tactic Conv in
+open Parser Tactic Conv Pattern Location in
 /-- A tactic to perform targeted unfolding of patterns. -/
-elab "unfold'" occs:(occs)? p:term loc:(location)? : tactic => withMainContext do
+elab "unfold'" p:term locs:locs : tactic => withMainContext do
   let pattern ← expandPattern p
-  let location := (expandLocation <$> loc).getD (.targets #[] true)
-  let occurrences := expandOccs occs
-  let goal ← getMainGoal
-  goal.withContext do
-    withLocation location
-      (atLocal := fun fvarId => do
-        let hypType ← fvarId.getType
-        let newGoal ← goal.replaceLocalDeclDefEq fvarId <| ←
-          replaceByDef hypType pattern occurrences
-        replaceMainGoal [newGoal])
-      (atTarget := do
-        let newGoal ← goal.replaceTargetDefEq <| ←
-          replaceByDef (← goal.getType) pattern occurrences
-        replaceMainGoal [newGoal])
-      (failed := (throwTacticEx `unfold' · m!"Failed to unfold pattern {p}."))
+  let occurrences ← expandLocs locs
+  replaceOccurrencesDefEq occurrences pattern replaceByDef
+
+
+section Test
+
+def f := Nat.add
+
+def g := fun x ↦ x + 5
+
+example (h : f 0 0 = 0) (h' : g 1 = f 1 2 + f 3 3) : f 0 0 = g 0 := by
+  let h'' := f 5 5
+  unfold' f ?n ?n at h ‹h' (occs := 1)› ‹h'' (occs := 1) in body› <⊢ (occs := 1)>
+  sorry
+
+end Test
