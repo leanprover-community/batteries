bd2a4908abe32a55beb085a50936551a51526efe
Scott Morrison <scott.morrison@gmail.com>
2023-12-11 08:59:13 +1100

    fix build (bad merge)

--- Std/Data/Bool.lean
+++ Std/Data/Bool.lean
@@ -196,6 +196,18 @@ theorem and_or_inj_left_iff :
     ∀ {m x y : Bool}, (m && x) = (m && y) ∧ (m || x) = (m || y) ↔ x = y := by decide
 @[deprecated] alias and_or_inj_left' := and_or_inj_left_iff
 
+/-! ## toNat -/
+
+/-- convert a `Bool` to a `Nat`, `false -> 0`, `true -> 1` -/
+def toNat (b:Bool) : Nat := cond b 1 0
+
+@[simp] theorem toNat_false : false.toNat = 0 := rfl
+
+@[simp] theorem toNat_true : true.toNat = 1 := rfl
+
+theorem toNat_le_one (c:Bool) : c.toNat ≤ 1 := by
+  cases c <;> trivial
+
 end Bool
 
 /-! ### cond -/
@@ -210,15 +222,3 @@ theorem cond_eq_if : (bif b then x else y) = (if b then x else y) := by
 
 @[simp] theorem true_eq_decide_iff {p : Prop} [h : Decidable p] : true = decide p ↔ p := by
   cases h with | _ q => simp [q]
-
-/-! ## toNat -/
-
-/-- convert a `Bool` to a `Nat`, `false -> 0`, `true -> 1` -/
-def toNat (b:Bool) : Nat := cond b 1 0
-
-@[simp] theorem toNat_false : false.toNat = 0 := rfl
-
-@[simp] theorem toNat_true : true.toNat = 1 := rfl
-
-theorem toNat_le_one (c:Bool) : c.toNat ≤ 1 := by
-  cases c <;> trivial
