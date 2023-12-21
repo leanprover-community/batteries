cfa4e41989d46c6e579ba222d3268f17efcdd413
James Gallicchio <JamesGallicchio@users.noreply.github.com>
2023-12-21 08:39:28 -0500

    feat: lift between StateT

--- Std/Control/State.lean
+++ Std/Control/State.lean
@@ -6,3 +6,6 @@ Authors: James Gallicchio
 
 instance [Monad m] : MonadLift (StateM σ) (StateT σ m) where
   monadLift s := pure ∘ s
+
+instance [MonadLift m n] : MonadLift (StateT σ m) (StateT σ n) where
+  monadLift s := fun state => liftM (s state)
