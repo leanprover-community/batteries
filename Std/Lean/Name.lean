88f502d1d479830fb934a33a398bea8a4ee0a6ff
Jon Eugster <eugster.jon@gmail.com>
2023-12-21 14:20:29 +0100

    add stuff

--- Std/Lean/Name.lean
+++ Std/Lean/Name.lean
@@ -4,6 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
 import Lean.Data.Name
+import Lean
+import Std.Lean.SMap
 
 namespace Lean.Name
 
@@ -34,3 +36,44 @@ def isInternalDetail : Name → Bool
       || p.isInternalOrNum
   | .num _ _     => true
   | p            => p.isInternalOrNum
+
+end Lean.Name
+
+open Lean Meta Elab
+
+/-!
+# Additional functions on `Lean.Name`.
+-/
+
+private def isBlackListed (declName : Name) : CoreM Bool := do
+  if declName.toString.startsWith "Lean" then return true
+  let env ← getEnv
+  pure $ declName.isInternalDetail
+   || isAuxRecursor env declName
+   || isNoConfusion env declName
+  <||> isRec declName <||> isMatcher declName
+
+/--
+Retrieve all names in the environment satisfying a predicate.
+-/
+def allNames (p : Name → Bool) : CoreM (Array Name) := do
+  (← getEnv).constants.foldM (init := #[]) fun names n _ => do
+    if p n && !(← isBlackListed n) then
+      return names.push n
+    else
+      return names
+
+/--
+Retrieve all names in the environment satisfying a predicate,
+gathered together into a `HashMap` according to the module they are defined in.
+-/
+def allNamesByModule (p : Name → Bool) : CoreM (HashMap Name (Array Name)) := do
+  (← getEnv).constants.foldM (init := HashMap.empty) fun names n _ => do
+    if p n && !(← isBlackListed n) then
+      let some m ← findModuleOf? n | return names
+      -- TODO use `Std.HashMap.modify` when we bump Std4 (or `alter` if that is written).
+      match names.find? m with
+      | some others => return names.insert m (others.push n)
+      | none => return names.insert m #[n]
+    else
+      return names
