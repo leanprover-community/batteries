55bb39e89640ec9a5ba07b879f9f8e0f22006486
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2023-12-25 02:32:03 +0100

    remove ↦

--- Std/Tactic/Pattern/Unfold.lean
+++ Std/Tactic/Pattern/Unfold.lean
@@ -14,16 +14,25 @@ open Lean Meta
 A tactic for definitionally unfolding expressions.
 The targeted sub-expression is selected using a pattern.
 
+example use case:
+```
+@[irreducible] def f (n : Nat) := n + 1
+example : ∀ n : Nat, n + 1 = f n := by
+  unfold' f _ at ⊢ --∀ (n : Nat), n + 1 = n + 1
+  intro n
+  rfl
+```
 -/
 
 /-- If the head of the expression is a projection, reduce the projection. -/
-def reduceProjection (e : Expr) : MetaM (Option Expr) := do
+def reduceHeadProjection (e : Expr) : MetaM (Option Expr) := do
   let .proj _ i b := e.getAppFn | return none
   let some f ← project? b i | return none
   return f.betaRev e.getAppRevArgs
 
 /-- Reduction function for the `unfold'` tactic. -/
-def replaceByDef (e : Expr) : MetaM Expr := do
+def replaceByDef (e : Expr) : MetaM Expr :=
+  withTransparency .all do
   /- β-reduction -/
   if e.isHeadBetaTarget then
     return e.withAppRev Expr.betaRev
@@ -34,15 +43,15 @@ def replaceByDef (e : Expr) : MetaM Expr := do
   if let .letE _ _ v b _ := e then
     return b.instantiate1 v
   /- projection reduction -/
-  if let some e ← reduceProjection e then
+  if let some e ← reduceHeadProjection e then
     return e
   /- unfolding a let-bound free variable -/
   if let .fvar fvarId := e.getAppFn then
     if let some value ← fvarId.getValue? then
       return value.betaRev e.getAppRevArgs
   /- unfolding a constant -/
-  if let some e ← withTransparency .all <| unfoldDefinition? e then
-    match ← reduceProjection e with
+  if let some e ← unfoldDefinition? e then
+    match ← reduceHeadProjection e with
       | some e => return e
       | none => return e
 
@@ -52,11 +61,11 @@ open Elab.Tactic Pattern.Location
 
 /-- Unfold the selected expression in one of the following ways:
 
-- β-reduction: `(fun x₁ .. xₙ => t[x₁, .., xₙ]) a₁ .. aₙ` ↦ `t[a₁, .., aₙ]`
-- η-reduction: `fun x₁ .. xₙ => f x₁ .. xₙ` ↦ `f`
-- ζ-reduction: `let a := v; t[a]` ↦ `t[v]`
-- projection reduction: `instAddNat.1 a b` ↦ `Nat.add a b`
-- unfolding a constant or a let-bound free variable: `Surjective f` ↦ `∀ b, ∃ a, f a = b`
+- β-reduction: `(fun x₁ .. xₙ => t[x₁, .., xₙ]) a₁ .. aₙ` → `t[a₁, .., aₙ]`
+- η-reduction: `fun x₁ .. xₙ => f x₁ .. xₙ` → `f`
+- ζ-reduction: `let a := v; t[a]` → `t[v]`
+- projection reduction: `instAddNat.1 a b` → `Nat.add a b`
+- unfolding a constant or a let-bound free variable: `Surjective f` → `∀ b, ∃ a, f a = b`
 
 Note that we always reduce a projection after unfolding a constant,
 so that `@Add.add ℕ instAddNat a b` gives `Nat.add a b` instead of `instAddNat.1 a b`.
