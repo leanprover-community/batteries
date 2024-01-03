933bfcc9add277c06245a6d147990d8522435552
Mario Carneiro <di.gama@gmail.com>
2024-01-03 13:55:32 -0500

    move to scripts/

--- scripts/updateStd.sh
+++ scripts/updateStd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This command updates the `Std.lean` file to include a list of all files
 # in the `Std` directory.
-__LEAN_STD_AUTOFIX_IMPORTS=true lake env lean test/check_imports_std_data.lean
+__LEAN_STD_AUTOFIX_IMPORTS=true lake env lean scripts/checkStdDataImports.lean
 lake build
