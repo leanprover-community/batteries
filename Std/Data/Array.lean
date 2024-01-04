a6a3c4ac0ca9ae9fe127ea6df9ca5aa75945358f
Joe Hendrix <joe@lean-fro.org>
2024-01-03 23:31:38 -0800

    chore: Move files around again; Intentional missing import.

--- Std/Data/Array.lean
+++ Std/Data/Array.lean
@@ -2,5 +2,4 @@ import Std.Data.Array.Basic
 import Std.Data.Array.Init.Basic
 import Std.Data.Array.Init.Lemmas
 import Std.Data.Array.Lemmas
-import Std.Data.Array.Match
 import Std.Data.Array.Merge
