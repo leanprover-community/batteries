42de8e0e124d4e5802aef330fbd0dbfde2c09c72
François G. Dorais <fgdorais@gmail.com>
2024-01-14 01:45:14 -0500

    fix: single-use lemma
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- Std/Data/ByteArray.lean
+++ Std/Data/ByteArray.lean
@@ -134,10 +134,9 @@ private def ofFnAux (f : Fin n → UInt8) : ByteArray := go 0 (mkEmpty n) where
     if h : i < n then go (i+1) (acc.push (f ⟨i, h⟩)) else acc
 termination_by _ => n - i
 
-@[simp] theorem ofFnAux_data (f : Fin n → UInt8) :
-    (ofFnAux.go f i acc).data = Array.ofFn.go f i acc.data := by
-  rw [ofFnAux.go, Array.ofFn.go]; split; rw [ofFnAux_data, push_data]; rfl
-termination_by _ => n - i
-
 @[csimp] theorem ofFn_eq_ofFnAux : @ofFn = @ofFnAux := by
+  let rec ofFnAux_data (f : Fin n → UInt8) :
+      (ofFnAux.go f i acc).data = Array.ofFn.go f i acc.data := by
+    rw [ofFnAux.go, Array.ofFn.go]; split; rw [ofFnAux_data, push_data]; rfl
+  termination_by _ => n - i
   funext n f; ext; simp [ofFnAux, Array.ofFn, ofFnAux_data, mkEmpty]
