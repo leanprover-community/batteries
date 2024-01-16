d0bc25434c35ee6d0a719dcf2a746157dc3ff571
Bulhwi Cha <chabulhwi@semmalgil.com>
2024-01-16 14:35:31 +0900

    refactor: rename `List.splitOnceP`

--- Std/Data/List/SplitOnList.lean
+++ Std/Data/List/SplitOnList.lean
@@ -8,37 +8,37 @@ import Std.Data.List.Lemmas
 namespace List
 
 /-- Returns `(l₁, l₂)` for the first split `l = l₁ ++ l₂` such that `P l₂` returns true. -/
-def splitOnceP (P : List α → Bool) (l : List α) : Option (List α × List α) :=
+def splitOnceRightP (P : List α → Bool) (l : List α) : Option (List α × List α) :=
   if P l then
     some ([], l)
   else
     match l with
     | [] => none
-    | a :: l => (splitOnceP P l).map fun (l, r) => (a :: l, r)
+    | a :: l => (splitOnceRightP P l).map fun (l, r) => (a :: l, r)
 
-theorem eq_append_of_splitOnceP (P : List α → Bool) (l : List α) :
-    ∀ l₁ l₂, splitOnceP P l = some (l₁, l₂) → l = l₁ ++ l₂ := by
+theorem eq_append_of_splitOnceRightP (P : List α → Bool) (l : List α) :
+    ∀ l₁ l₂, splitOnceRightP P l = some (l₁, l₂) → l = l₁ ++ l₂ := by
   induction l with
-  | nil => simp [splitOnceP]
+  | nil => simp [splitOnceRightP]
   | cons a l ih =>
     if h : P (a :: l) then
-      simp [splitOnceP, h]
+      simp [splitOnceRightP, h]
     else
-      simp only [splitOnceP, h, ite_false]
-      match e : splitOnceP P l with
+      simp only [splitOnceRightP, h, ite_false]
+      match e : splitOnceRightP P l with
       | none => simp
       | some (lf, rt) => simp; apply ih; assumption
 
-theorem P_of_splitOnceP (P : List α → Bool) (l : List α) :
-    ∀ l₁ l₂, splitOnceP P l = some (l₁, l₂) → P l₂ := by
+theorem P_of_splitOnceRightP (P : List α → Bool) (l : List α) :
+    ∀ l₁ l₂, splitOnceRightP P l = some (l₁, l₂) → P l₂ := by
   induction l with
-  | nil => simp [splitOnceP]
+  | nil => simp [splitOnceRightP]
   | cons a l ih =>
     if h : P (a :: l) then
-      simp [splitOnceP, h]
+      simp [splitOnceRightP, h]
     else
-      simp only [splitOnceP, h, ite_false]
-      match e : splitOnceP P l with
+      simp only [splitOnceRightP, h, ite_false]
+      match e : splitOnceRightP P l with
       | none => simp
       | some (lf, rt) => simp; apply ih; assumption
 
@@ -52,16 +52,16 @@ def splitOnList [BEq α] (l sep : List α) : List (List α) :=
   if h : sep.isEmpty then
     [l]
   else
-    match e : splitOnceP sep.isPrefixOf l with
+    match e : splitOnceRightP sep.isPrefixOf l with
     | none => [l]
     | some (l₁, l₂) =>
       have : length l₂ - sep.length < length l := by
-        rw [eq_append_of_splitOnceP (sep.isPrefixOf) l l₁ l₂ e, length_append]
+        rw [eq_append_of_splitOnceRightP (sep.isPrefixOf) l l₁ l₂ e, length_append]
         calc
           length l₂ - length sep < length l₂ := by
             apply Nat.sub_lt_self
             · apply length_pos.mpr; simp [← isEmpty_iff_eq_nil, h]
-            · apply length_le_of_isPrefixOf (P_of_splitOnceP sep.isPrefixOf l l₁ l₂ e)
+            · apply length_le_of_isPrefixOf (P_of_splitOnceRightP sep.isPrefixOf l l₁ l₂ e)
           _ ≤ length l₁ + length l₂ := Nat.le_add_left ..
       splitOnList (l₂.drop sep.length) sep
 termination_by _ => l.length
