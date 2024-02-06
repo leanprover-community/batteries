a95f136eee3586a9940979bba787cf0ac0f39a65
Scott Morrison <scott.morrison@gmail.com>
2024-02-06 13:17:35 +1100

    fix import

--- Std/Tactic/Omega/Frontend.lean
+++ Std/Tactic/Omega/Frontend.lean
@@ -3,7 +3,6 @@ Copyright (c) 2023 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import Std.Data.List.Basic
 import Std.Tactic.Omega.Core
 import Std.Tactic.Omega.LinearCombo
 import Std.Tactic.Omega.Logic
