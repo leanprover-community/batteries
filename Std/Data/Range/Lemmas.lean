e449af0c5bb885aed33ce1086d3551f7b80e4c87
Scott Morrison <scott.morrison@gmail.com>
2024-02-08 08:46:41 +1100

    chore: add missing copyright headers (#595)
    * chore: add missing copyright headers
    
    * oops, didn't hit save on two

--- Std/Data/Range/Lemmas.lean
+++ Std/Data/Range/Lemmas.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2022 Mario Carneiro. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Mario Carneiro
+-/
 import Std.Tactic.ByCases
 import Std.Data.List.Lemmas
 import Std.Data.List.Init.Attach
