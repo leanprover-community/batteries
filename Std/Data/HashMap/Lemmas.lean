044c185e712754b127f4f4f51881b27f21f0887c
Daniel Windham <daniel@atlascomputing.org>
2024-02-14 20:20:42 -0500

    feat(Data/HashMap): add proof for HashMap.empty_find?
    Co-authored-by: Timo Carlin-Burns <timorcb@gmail.com>

--- Std/Data/HashMap/Lemmas.lean
+++ Std/Data/HashMap/Lemmas.lean
@@ -5,11 +5,21 @@ Authors: Scott Morrison
 -/
 import Std.Data.HashMap.Basic
 import Std.Util.ProofWanted
+import Std.Data.Array.Lemmas
 
 namespace Std.HashMap
 
-@[simp] proof_wanted empty_find? [BEq α] [Hashable α] {a : α} :
-    (∅ : HashMap α β).find? a = none
+theorem empty_def [BEq α] [Hashable α] : (∅ : HashMap α β) = HashMap.empty :=
+  rfl
+
+@[simp] theorem mkArray_getElem {α : Type u} {v: α} {n i : Nat}
+    (h : i < Array.size (mkArray n v)) :
+    (mkArray n v)[i]'h = v :=
+  List.eq_of_mem_replicate (Array.getElem_mem_data _ h)
+
+@[simp] theorem empty_find? [BEq α] [Hashable α] {a : α} :
+    (∅ : HashMap α β).find? a = none := by
+  simp [empty_def, empty, mkHashMap, find?, Imp.find?, Imp.empty', Imp.empty, Imp.Buckets.mk]
 
 proof_wanted insert_find? [BEq α] [Hashable α] (m : HashMap α β) (a a' : α) (b : β) :
     (m.insert a b).find? a' = if a' == a then some b else m.find? a'
