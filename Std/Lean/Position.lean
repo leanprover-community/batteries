65f594e65b613e5ed4f056aaeafbe7149c8c9a58
Joe Hendrix <joe@lean-fro.org>
2024-02-15 19:37:53 -0800

    chore: remove unnecessary imports

--- Std/Lean/Position.lean
+++ Std/Lean/Position.lean
@@ -3,9 +3,7 @@ Copyright (c) 2023 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import Lean.Syntax
 import Lean.Meta.Tactic.TryThis
-import Lean.Data.Lsp.Utf16
 
 /-- Gets the LSP range of syntax `stx`. -/
 def Lean.FileMap.rangeOfStx? (text : FileMap) (stx : Syntax) : Option Lsp.Range :=
