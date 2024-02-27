dc72e985fd565635b055483f9e2a808d68deaa06
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-02-27 16:10:32 +0100

    test

--- test/lint_dupNamespace.lean
+++ test/lint_dupNamespace.lean
@@ -1,6 +1,17 @@
+import Std.Data.Nat.Basic
 import Std.Tactic.Lint
 
 -- internal names should be ignored
 theorem Foo.Foo._bar : True := trivial
+
+def Foo := Nat
+  deriving Add
+
+namespace Foo
+
+instance : Mul Foo where
+  mul := Nat.mul
+
+end Foo
 
 #lint- only dupNamespace
