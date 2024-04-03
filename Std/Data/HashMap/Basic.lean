9968d2d3571508f147b9a464beaed9480c5b0099
austinletson <waustinletson@gmail.com>
2024-04-03 14:51:40 -0400

    doc: add examples to `Std.Data.HashMap.Init` docs

--- Std/Data/HashMap/Basic.lean
+++ Std/Data/HashMap/Basic.lean
@@ -262,26 +262,52 @@ instance [BEq α] [Hashable α] : Inhabited (HashMap α β) where
 
 instance [BEq α] [Hashable α] : EmptyCollection (HashMap α β) := ⟨mkHashMap⟩
 
-/-- Make a new empty hash map. -/
+/--
+Make a new empty hash map
+```
+(empty : Std.HashMap Int Int).toList = []
+```
+-/
 @[inline] def empty [BEq α] [Hashable α] : HashMap α β := mkHashMap
 
 variable {_ : BEq α} {_ : Hashable α}
 
-/-- The number of elements in the hash map. -/
+/--
+The number of elements in the hash map.
+```
+(ofList [("one", 1), ("two", 2)]).size = 2
+```
+-/
 @[inline] def size (self : HashMap α β) : Nat := self.1.size
 
-/-- Is the map empty? -/
+/--
+Is the map empty?
+```
+(empty : Std.HashMap Int Int).isEmpty = true
+(ofList [("one", 1), ("two", 2)]).isEmpty = false
+```
+-/
 @[inline] def isEmpty (self : HashMap α β) : Bool := self.size = 0
 
 /--
 Inserts key-value pair `a, b` into the map.
 If an element equal to `a` is already in the map, it is replaced by `b`.
+```
+def hashMap := ofList [("one", 1), ("two", 2)]
+hashMap.insert "three" 3 = ofList [("one", 1), ("two", 2), ("three", 3)]
+hashMap.insert "two" 0 = ofList [("one", 1), ("two", 0)]
+```
 -/
 def insert (self : HashMap α β) (a : α) (b : β) : HashMap α β := ⟨self.1.insert a b, self.2.insert⟩
 
 /--
 Similar to `insert`, but also returns a boolean flag indicating whether an existing entry has been
 replaced with `a => b`.
+```
+def hashMap := ofList [("one", 1), ("two", 2)]
+(hashMap.insert' "three" 3).snd = false
+(hashMap.insert' "two" 0).snd = true
+```
 -/
 @[inline] def insert' (m : HashMap α β) (a : α) (b : β) : HashMap α β × Bool :=
   let old := m.size
@@ -291,43 +317,114 @@ replaced with `a => b`.
 
 /--
 Removes key `a` from the map. If it does not exist in the map, the map is returned unchanged.
+```
+def hashMap := ofList [("one", 1), ("two", 2)]
+hashMap.erase "one" = ofList [("two", 2)]
+hashMap.erase "three" = ofList [("one", 1), ("two", 2)]
+```
 -/
 @[inline] def erase (self : HashMap α β) (a : α) : HashMap α β := ⟨self.1.erase a, self.2.erase⟩
 
 /--
 Performs an in-place edit of the value, ensuring that the value is used linearly.
 The function `f` is passed the original key of the entry, along with the value in the map.
+```
+(ofList [("one", 1), ("two", 2)]).modify "one" (fun _ v => v + 1) = ofList [("one", 2), ("two", 2)]
+```
 -/
 def modify (self : HashMap α β) (a : α) (f : α → β → β) : HashMap α β :=
   ⟨self.1.modify a f, self.2.modify⟩
 
-/-- Given a key `a`, returns a key-value pair in the map whose key compares equal to `a`. -/
+/--
+Given a key `a`, returns a key-value pair in the map whose key compares equal to `a`.
+```
+def hashMap := ofList [("one", 1), ("two", 2)]
+hashMap.findEntry? "one" = some ("one", 1)
+hashMap.findEntry? "three" = none
+```
+-/
 @[inline] def findEntry? (self : HashMap α β) (a : α) : Option (α × β) := self.1.findEntry? a
 
-/-- Looks up an element in the map with key `a`. -/
+/--
+Looks up an element in the map with key `a`.
+```
+def hashMap := ofList [("one", 1), ("two", 2)]
+hashMap.find? "one" = some 1
+hashMap.find? "three" = none
+```
+-/
 @[inline] def find? (self : HashMap α β) (a : α) : Option β := self.1.find? a
 
-/-- Looks up an element in the map with key `a`. Returns `b₀` if the element is not found. -/
+/--
+Looks up an element in the map with key `a`. Returns `b₀` if the element is not found.
+```
+def hashMap := ofList [("one", 1), ("two", 2)]
+hashMap.findD "one" 0 = 1
+hashMap.findD "three" 0 = 0
+```
+-/
 @[inline] def findD (self : HashMap α β) (a : α) (b₀ : β) : β := (self.find? a).getD b₀
 
-/-- Looks up an element in the map with key `a`. Panics if the element is not found. -/
+/--
+Looks up an element in the map with key `a`. Panics if the element is not found.
+```
+def hashMap := ofList [("one", 1), ("two", 2)]
+hashMap.find! "one" = 1
+TODO: Confirm this is the correct way to notate a panic
+hashMap.find! "three" => panic!
+```
+-/
 @[inline] def find! [Inhabited β] (self : HashMap α β) (a : α) : β :=
   (self.find? a).getD (panic! "key is not in the map")
 
 instance : GetElem (HashMap α β) α (Option β) fun _ _ => True where
   getElem m k _ := m.find? k
 
-/-- Returns true if the element `a` is in the map. -/
+/--
+Returns true if the element `a` is in the map.
+```
+def hashMap := ofList [("one", 1), ("two", 2)]
+hashMap.contains "one" = true
+hashMap.contains "three" = false
+```
+-/
 @[inline] def contains (self : HashMap α β) (a : α) : Bool := self.1.contains a
 
-/-- Folds a monadic function over the elements in the map (in arbitrary order). -/
+/--
+Folds a monadic function over the elements in the map (in arbitrary order).
+```
+def sumEven (sum: Nat) (k : String) (v : Nat) : Except String Nat :=
+  if v % 2 == 0 then pure (sum + v) else throw s!"value {v} at key {k} is not even"
+
+foldM sumEven 0 (ofList [("one", 1), ("three", 3)]) = Except.error "value 3 at key three is not even"
+foldM sumEven 0 (ofList [("two", 2), ("four", 4)]) = Except.ok 6
+```
+-/
 @[inline] def foldM [Monad m] (f : δ → α → β → m δ) (init : δ) (self : HashMap α β) : m δ :=
   self.1.foldM f init
 
-/-- Folds a function over the elements in the map (in arbitrary order). -/
+/--
+Folds a function over the elements in the map (in arbitrary order).
+```
+fold (fun sum _ v => sum + v) 0 (ofList [("one", 1), ("two", 2)]) = 3
+```
+-/
 @[inline] def fold (f : δ → α → β → δ) (init : δ) (self : HashMap α β) : δ := self.1.fold f init
 
-/-- Combines two hashmaps using a monadic function `f` to combine two values at a key. -/
+/--
+Combines two hashmaps using a monadic function `f` to combine two values at a key.
+```
+def map1 := ofList [("one", 1), ("two", 2)]
+def map2 := ofList [("two", 2), ("three", 3)]
+def map3 := ofList [("two", 3), ("three", 3)]
+def mergeIfNoConflict? (_ : String) (v₁ v₂ : Nat) : Option Nat :=
+  if v₁ != v₂ then none else some v₁
+
+
+mergeWithM mergeIfNoConflict? map1 map2 = some $ toList [("one", 1), ("two", 2), ("three", 3)]
+mergeWithM mergeIfNoConflict? map1 map3 = none
+```
+-/
 @[specialize] def mergeWithM [Monad m] (f : α → β → β → m β)
     (self other : HashMap α β) : m (HashMap α β) :=
   other.foldM (init := self) fun m k v₂ =>
@@ -335,7 +432,14 @@ instance : GetElem (HashMap α β) α (Option β) fun _ _ => True where
     | none => return m.insert k v₂
     | some v₁ => return m.insert k (← f k v₁ v₂)
 
-/-- Combines two hashmaps using function `f` to combine two values at a key. -/
+/--
+Combines two hashmaps using function `f` to combine two values at a key.
+```
+mergeWith (fun _ v₁ v₂ => v₁ + v₂ )
+  (ofList [("one", 1), ("two", 2)]) (ofList [("two", 2), ("three", 3)]) =
+    ofList [("one", 1), ("two", 4), ("three", 3)]
+```
+-/
 @[inline] def mergeWith (f : α → β → β → β) (self other : HashMap α β) : HashMap α β :=
   -- Implementing this function directly, rather than via `mergeWithM`, gives
   -- us less constrained universes.
@@ -344,13 +448,35 @@ instance : GetElem (HashMap α β) α (Option β) fun _ _ => True where
     | none => map.insert k v₂
     | some v₁ => map.insert k $ f k v₁ v₂
 
-/-- Runs a monadic function over the elements in the map (in arbitrary order). -/
+/--
+Runs a monadic function over the elements in the map (in arbitrary order).
+```
+def checkEven (k : String) (v : Nat) : Except String Unit :=
+  if v % 2 == 0 then pure () else throw s!"value {v} at key {k} is not even"
+
+forM checkEven (ofList [("one", 1), ("three", 3)]) = Except.error "value 3 at key three is not even"
+forM checkEven (ofList [("two", 2), ("four", 4)]) = Except.ok ()
+```
+-/
 @[inline] def forM [Monad m] (f : α → β → m PUnit) (self : HashMap α β) : m PUnit := self.1.forM f
 
-/-- Converts the map into a list of key-value pairs. -/
+/--
+Converts the map into a list of key-value pairs.
+```
+TODO: confirm this acceptable
+def hashMap := ofList [("one", 1), ("two", 2)]
+hashMap.toList = [("one", 1), ("two", 2)] ∨ hashMap.toList = [("two", 2), ("one", 1)]
+```
+-/
 def toList (self : HashMap α β) : List (α × β) := self.fold (init := []) fun r k v => (k, v)::r
 
-/-- Converts the map into an array of key-value pairs. -/
+/--
+Converts the map into an array of key-value pairs.
+```
+def hashMap := ofList [("one", 1), ("two", 2)]
+hashMap.toArray = #[("one", 1), ("two", 2)] ∨ hashMap.toArray = #[("two", 2), ("one", 1)]
+```
+-/
 def toArray (self : HashMap α β) : Array (α × β) :=
   self.fold (init := #[]) fun r k v => r.push (k, v)
 
@@ -360,11 +486,19 @@ def numBuckets (self : HashMap α β) : Nat := self.1.buckets.1.size
 /--
 Builds a `HashMap` from a list of key-value pairs.
 Values of duplicated keys are replaced by their respective last occurrences.
+```
+ofList [("one", 1), ("one", 2)] = ofList [("one", 2)]
+```
 -/
 def ofList [BEq α] [Hashable α] (l : List (α × β)) : HashMap α β :=
   l.foldl (init := HashMap.empty) fun m (k, v) => m.insert k v
 
-/-- Variant of `ofList` which accepts a function that combines values of duplicated keys. -/
+/--
+Variant of `ofList` which accepts a function that combines values of duplicated keys.
+```
+ofListWith [("one", 1), ("one", 2)] (fun v₁ v₂ => v₁ + v₂) = ofList [("one", 3)]
+```
+-/
 def ofListWith [BEq α] [Hashable α] (l : List (α × β)) (f : β → β → β) : HashMap α β :=
   l.foldl (init := HashMap.empty) fun m p =>
     match m.find? p.1 with
