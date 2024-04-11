154a659ad1fe8683283a498e3f39f3086fd5178b
Mario Carneiro <di.gama@gmail.com>
2024-04-11 01:14:16 -0400

    feat: cleanup PrintPrefix, use ppSignature

--- test/print_prefix.lean
+++ test/print_prefix.lean
@@ -3,11 +3,11 @@ import Std.Tactic.PrintPrefix
 inductive TEmpty : Type
 /--
 info: TEmpty : Type
-TEmpty.casesOn : (motive : TEmpty → Sort u) → (t : TEmpty) → motive t
-TEmpty.noConfusion : {P : Sort u} → {v1 v2 : TEmpty} → v1 = v2 → TEmpty.noConfusionType P v1 v2
-TEmpty.noConfusionType : Sort u → TEmpty → TEmpty → Sort u
-TEmpty.rec : (motive : TEmpty → Sort u) → (t : TEmpty) → motive t
-TEmpty.recOn : (motive : TEmpty → Sort u) → (t : TEmpty) → motive t
+TEmpty.casesOn.{u} (motive : TEmpty → Sort u) (t : TEmpty) : motive t
+TEmpty.noConfusion.{u} {P : Sort u} {v1 v2 : TEmpty} (h12 : v1 = v2) : TEmpty.noConfusionType P v1 v2
+TEmpty.noConfusionType.{u} (P : Sort u) (v1 v2 : TEmpty) : Sort u
+TEmpty.rec.{u} (motive : TEmpty → Sort u) (t : TEmpty) : motive t
+TEmpty.recOn.{u} (motive : TEmpty → Sort u) (t : TEmpty) : motive t
 -/
 #guard_msgs in
 #print prefix TEmpty -- Test type that probably won't change much.
@@ -15,7 +15,7 @@ TEmpty.recOn : (motive : TEmpty → Sort u) → (t : TEmpty) → motive t
 /--
 -/
 #guard_msgs in
-#print prefix (config:={imported:=false}) Empty
+#print prefix (config := {imported := false}) Empty
 
 namespace EmptyPrefixTest
 
@@ -35,9 +35,7 @@ def foo (_l:List String) : Int := 0
 
 end Prefix.Test
 
-/--
-info: Prefix.Test.foo : List String → Int
--/
+/-- info: Prefix.Test.foo (_l : List String) : Int -/
 #guard_msgs in
 #print prefix Prefix.Test
 
@@ -50,44 +48,52 @@ structure TestStruct where
 
 /--
 info: TestStruct : Type
-TestStruct.bar : TestStruct → Int
-TestStruct.casesOn : {motive : TestStruct → Sort u} → (t : TestStruct) → ((foo bar : Int) → motive { foo := foo, bar := bar }) → motive t
-TestStruct.foo : TestStruct → Int
-TestStruct.mk : Int → Int → TestStruct
-TestStruct.mk.inj : ∀ {foo bar foo_1 bar_1 : Int}, { foo := foo, bar := bar } = { foo := foo_1, bar := bar_1 } → foo = foo_1 ∧ bar = bar_1
-TestStruct.mk.injEq : ∀ (foo bar foo_1 bar_1 : Int),
-  ({ foo := foo, bar := bar } = { foo := foo_1, bar := bar_1 }) = (foo = foo_1 ∧ bar = bar_1)
-TestStruct.mk.sizeOf_spec : ∀ (foo bar : Int), sizeOf { foo := foo, bar := bar } = 1 + sizeOf foo + sizeOf bar
-TestStruct.noConfusion : {P : Sort u} → {v1 v2 : TestStruct} → v1 = v2 → TestStruct.noConfusionType P v1 v2
-TestStruct.noConfusionType : Sort u → TestStruct → TestStruct → Sort u
-TestStruct.rec : {motive : TestStruct → Sort u} → ((foo bar : Int) → motive { foo := foo, bar := bar }) → (t : TestStruct) → motive t
-TestStruct.recOn : {motive : TestStruct → Sort u} → (t : TestStruct) → ((foo bar : Int) → motive { foo := foo, bar := bar }) → motive t
+TestStruct.bar (self : TestStruct) : Int
+TestStruct.casesOn.{u} {motive : TestStruct → Sort u} (t : TestStruct)
+  (mk : (foo bar : Int) → motive { foo := foo, bar := bar }) : motive t
+TestStruct.foo (self : TestStruct) : Int
+TestStruct.mk (foo bar : Int) : TestStruct
+TestStruct.mk.inj {foo bar foo bar : Int} (x✝ : { foo := foo, bar := bar } = { foo := foo, bar := bar }) :
+  foo = foo ∧ bar = bar
+TestStruct.mk.injEq (foo bar foo bar : Int) :
+  ({ foo := foo, bar := bar } = { foo := foo, bar := bar }) = (foo = foo ∧ bar = bar)
+TestStruct.mk.sizeOf_spec (foo bar : Int) : sizeOf { foo := foo, bar := bar } = 1 + sizeOf foo + sizeOf bar
+TestStruct.noConfusion.{u} {P : Sort u} {v1 v2 : TestStruct} (h12 : v1 = v2) : TestStruct.noConfusionType P v1 v2
+TestStruct.noConfusionType.{u} (P : Sort u) (v1 v2 : TestStruct) : Sort u
+TestStruct.rec.{u} {motive : TestStruct → Sort u} (mk : (foo bar : Int) → motive { foo := foo, bar := bar })
+  (t : TestStruct) : motive t
+TestStruct.recOn.{u} {motive : TestStruct → Sort u} (t : TestStruct)
+  (mk : (foo bar : Int) → motive { foo := foo, bar := bar }) : motive t
 -/
 #guard_msgs in
 #print prefix TestStruct
 
 /--
 info: TestStruct : Type
-TestStruct.bar : TestStruct → Int
-TestStruct.casesOn : {motive : TestStruct → Sort u} → (t : TestStruct) → ((foo bar : Int) → motive { foo := foo, bar := bar }) → motive t
-TestStruct.foo : TestStruct → Int
-TestStruct.mk : Int → Int → TestStruct
-TestStruct.noConfusion : {P : Sort u} → {v1 v2 : TestStruct} → v1 = v2 → TestStruct.noConfusionType P v1 v2
-TestStruct.noConfusionType : Sort u → TestStruct → TestStruct → Sort u
-TestStruct.rec : {motive : TestStruct → Sort u} → ((foo bar : Int) → motive { foo := foo, bar := bar }) → (t : TestStruct) → motive t
-TestStruct.recOn : {motive : TestStruct → Sort u} → (t : TestStruct) → ((foo bar : Int) → motive { foo := foo, bar := bar }) → motive t
+TestStruct.bar (self : TestStruct) : Int
+TestStruct.casesOn.{u} {motive : TestStruct → Sort u} (t : TestStruct)
+  (mk : (foo bar : Int) → motive { foo := foo, bar := bar }) : motive t
+TestStruct.foo (self : TestStruct) : Int
+TestStruct.mk (foo bar : Int) : TestStruct
+TestStruct.noConfusion.{u} {P : Sort u} {v1 v2 : TestStruct} (h12 : v1 = v2) : TestStruct.noConfusionType P v1 v2
+TestStruct.noConfusionType.{u} (P : Sort u) (v1 v2 : TestStruct) : Sort u
+TestStruct.rec.{u} {motive : TestStruct → Sort u} (mk : (foo bar : Int) → motive { foo := foo, bar := bar })
+  (t : TestStruct) : motive t
+TestStruct.recOn.{u} {motive : TestStruct → Sort u} (t : TestStruct)
+  (mk : (foo bar : Int) → motive { foo := foo, bar := bar }) : motive t
 -/
 #guard_msgs in
-#print prefix (config:={propositions:=false}) TestStruct
+#print prefix (config := {propositions := false}) TestStruct
 
 /--
-info: TestStruct.mk.inj : ∀ {foo bar foo_1 bar_1 : Int}, { foo := foo, bar := bar } = { foo := foo_1, bar := bar_1 } → foo = foo_1 ∧ bar = bar_1
-TestStruct.mk.injEq : ∀ (foo bar foo_1 bar_1 : Int),
-  ({ foo := foo, bar := bar } = { foo := foo_1, bar := bar_1 }) = (foo = foo_1 ∧ bar = bar_1)
-TestStruct.mk.sizeOf_spec : ∀ (foo bar : Int), sizeOf { foo := foo, bar := bar } = 1 + sizeOf foo + sizeOf bar
+info: TestStruct.mk.inj {foo bar foo bar : Int} (x✝ : { foo := foo, bar := bar } = { foo := foo, bar := bar }) :
+  foo = foo ∧ bar = bar
+TestStruct.mk.injEq (foo bar foo bar : Int) :
+  ({ foo := foo, bar := bar } = { foo := foo, bar := bar }) = (foo = foo ∧ bar = bar)
+TestStruct.mk.sizeOf_spec (foo bar : Int) : sizeOf { foo := foo, bar := bar } = 1 + sizeOf foo + sizeOf bar
 -/
 #guard_msgs in
-#print prefix (config:={propositionsOnly:=true}) TestStruct
+#print prefix (config := {propositionsOnly := true}) TestStruct
 
 /--
 info: TestStruct
@@ -104,7 +110,7 @@ TestStruct.rec
 TestStruct.recOn
 -/
 #guard_msgs in
-#print prefix (config:={showTypes:=false}) TestStruct
+#print prefix (config := {showTypes := false}) TestStruct
 
 /--
 Artificial test function to show #print prefix filters out internals
@@ -118,52 +124,49 @@ def testMatchProof : (n : Nat) → Fin n → Unit
   | _,  ⟨0, _⟩ => ()
   | Nat.succ as, ⟨Nat.succ i, h⟩ => testMatchProof as ⟨i, Nat.le_of_succ_le_succ h⟩
 
-/--
-info: testMatchProof : (n : Nat) → Fin n → Unit
--/
+/-- info: testMatchProof (n : Nat) (a✝ : Fin n) : Unit -/
 #guard_msgs in
 #print prefix testMatchProof
 
 /--
-info: testMatchProof : (n : Nat) → Fin n → Unit
-testMatchProof._cstage1 : (n : Nat) → Fin n → Unit
-testMatchProof._cstage2 : _obj → _obj → _obj
-testMatchProof._sunfold : (n : Nat) → Fin n → Unit
-testMatchProof._unsafe_rec : (n : Nat) → Fin n → Unit
-testMatchProof.match_1 : (motive : (x : Nat) → Fin x → Sort u_1) →
-  (x : Nat) →
-    (x_1 : Fin x) →
-      ((n : Nat) → (isLt : 0 < n) → motive n { val := 0, isLt := isLt }) →
-        ((as i : Nat) → (h : Nat.succ i < Nat.succ as) → motive (Nat.succ as) { val := Nat.succ i, isLt := h }) →
-          motive x x_1
-testMatchProof.match_1._cstage1 : (motive : (x : Nat) → Fin x → Sort u_1) →
-  (x : Nat) →
-    (x_1 : Fin x) →
-      ((n : Nat) → (isLt : 0 < n) → motive n { val := 0, isLt := isLt }) →
-        ((as i : Nat) → (h : Nat.succ i < Nat.succ as) → motive (Nat.succ as) { val := Nat.succ i, isLt := h }) →
-          motive x x_1
-testMatchProof.proof_1 : ∀ (as i : Nat), Nat.succ i < Nat.succ as → Nat.succ i ≤ as
-testMatchProof.proof_2 : ∀ (as i : Nat), Nat.succ i < Nat.succ as → Nat.succ i ≤ as
+info: testMatchProof (n : Nat) (a✝ : Fin n) : Unit
+testMatchProof._cstage1 (n : Nat) (a✝ : Fin n) : Unit
+testMatchProof._cstage2 (x✝x✝¹ : _obj) : _obj
+testMatchProof._sunfold (n : Nat) (a✝ : Fin n) : Unit
+testMatchProof._unsafe_rec (n : Nat) (a✝ : Fin n) : Unit
+testMatchProof.match_1.{u_1} (motive : (x : Nat) → Fin x → Sort u_1) (x✝ : Nat) (x✝¹ : Fin x✝)
+  (h_1 : (n : Nat) → (isLt : 0 < n) → motive n { val := 0, isLt := isLt })
+  (h_2 : (as i : Nat) → (h : Nat.succ i < Nat.succ as) → motive (Nat.succ as) { val := Nat.succ i, isLt := h }) :
+  motive x✝ x✝¹
+testMatchProof.match_1._cstage1.{u_1} (motive : (x : Nat) → Fin x → Sort u_1) (x✝ : Nat) (x✝¹ : Fin x✝)
+  (h_1 : (n : Nat) → (isLt : 0 < n) → motive n { val := 0, isLt := isLt })
+  (h_2 : (as i : Nat) → (h : Nat.succ i < Nat.succ as) → motive (Nat.succ as) { val := Nat.succ i, isLt := h }) :
+  motive x✝ x✝¹
+testMatchProof.proof_1 (as i : Nat) (h : Nat.succ i < Nat.succ as) : Nat.succ i ≤ as
+testMatchProof.proof_2 (as i : Nat) (h : Nat.succ i < Nat.succ as) : Nat.succ i ≤ as
 -/
 #guard_msgs in
-#print prefix (config:={internals:=true}) testMatchProof
+#print prefix (config := {internals := true}) testMatchProof
 
 private inductive TestInd where
 | foo : TestInd
 | bar : TestInd
 
 /--
-info: _private.test.print_prefix.0.TestInd : Type
-_private.test.print_prefix.0.TestInd.bar : TestInd
-_private.test.print_prefix.0.TestInd.bar.sizeOf_spec : sizeOf TestInd.bar = 1
-_private.test.print_prefix.0.TestInd.casesOn : {motive : TestInd → Sort u} → (t : TestInd) → motive TestInd.foo → motive TestInd.bar → motive t
-_private.test.print_prefix.0.TestInd.foo : TestInd
-_private.test.print_prefix.0.TestInd.foo.sizeOf_spec : sizeOf TestInd.foo = 1
-_private.test.print_prefix.0.TestInd.noConfusion : {P : Sort v✝} → {x y : TestInd} → x = y → TestInd.noConfusionType P x y
-_private.test.print_prefix.0.TestInd.noConfusionType : Sort v✝ → TestInd → TestInd → Sort v✝
-_private.test.print_prefix.0.TestInd.rec : {motive : TestInd → Sort u} → motive TestInd.foo → motive TestInd.bar → (t : TestInd) → motive t
-_private.test.print_prefix.0.TestInd.recOn : {motive : TestInd → Sort u} → (t : TestInd) → motive TestInd.foo → motive TestInd.bar → motive t
-_private.test.print_prefix.0.TestInd.toCtorIdx : TestInd → Nat
+info: TestInd : Type
+TestInd.bar : TestInd
+TestInd.bar.sizeOf_spec : sizeOf TestInd.bar = 1
+TestInd.casesOn.{u} {motive : TestInd → Sort u} (t : TestInd) (foo : motive TestInd.foo) (bar : motive TestInd.bar) :
+  motive t
+TestInd.foo : TestInd
+TestInd.foo.sizeOf_spec : sizeOf TestInd.foo = 1
+TestInd.noConfusion.{v✝} {P : Sort v✝} {x y : TestInd} (h : x = y) : TestInd.noConfusionType P x y
+TestInd.noConfusionType.{v✝} (P : Sort v✝) (x y : TestInd) : Sort v✝
+TestInd.rec.{u} {motive : TestInd → Sort u} (foo : motive TestInd.foo) (bar : motive TestInd.bar) (t : TestInd) :
+  motive t
+TestInd.recOn.{u} {motive : TestInd → Sort u} (t : TestInd) (foo : motive TestInd.foo) (bar : motive TestInd.bar) :
+  motive t
+TestInd.toCtorIdx (x✝ : TestInd) : Nat
 -/
 #guard_msgs in
 #print prefix TestInd
