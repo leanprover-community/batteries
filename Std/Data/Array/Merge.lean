48643957cb394f0740a292b14954747c96bcd00e
Scott Morrison <scott.morrison@gmail.com>
2024-04-24 13:39:03 +1000

    fix

--- Std/Data/Array/Merge.lean
+++ Std/Data/Array/Merge.lean
@@ -3,6 +3,7 @@ Copyright (c) 2022 Jannis Limperg. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jannis Limperg
 -/
+import Std.Tactic.Alias
 
 namespace Array
 
