c5369389f1d4b0158a771b6865a53fb45ba04270
Kim Morrison <kim@tqft.net>
2024-05-10 09:59:54 +1000

    add lake build step

--- scripts/test.lean
+++ scripts/test.lean
@@ -15,10 +15,16 @@ If arguments are provided, assume they are names of files in the `test/` directo
 and just run those.
 -/
 def main (args : List String) : IO Unit := do
+  -- We first run `lake build`, to ensure oleans are available.
+  -- Alternatively, we could use `lake lean` below instead of `lake env lean`,
+  -- but currently with parallelism this results in build jobs interfering with each other.
+  _ ← (← IO.Process.spawn { cmd := "lake", args := #["build"] }).wait
+  -- Collect test targets, either from the command line or by walking the `test/` directory.
   let targets ← match args with
   | [] => System.FilePath.walkDir "./test"
   | _ => pure <| (args.map fun t => mkFilePath [".", "test", t] |>.withExtension "lean") |>.toArray
   let existing ← targets.filterM fun t => do pure <| (← t.pathExists) && !(← t.isDir)
+  -- Generate a `lake env lean` task for each test target.
   let tasks ← existing.mapM fun t => do
     IO.asTask do
       let out ← IO.Process.output
@@ -32,6 +38,7 @@ def main (args : List String) : IO Unit := do
         unless out.stdout.isEmpty do IO.eprintln out.stdout
         unless out.stderr.isEmpty do IO.eprintln out.stderr
       pure out.exitCode
+  -- Wait on all the jobs and exit with 1 if any failed.
   let mut exitCode : UInt8 := 0
   for t in tasks do
     let e ← IO.wait t
