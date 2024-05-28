332e74ded5dce9979b75ad0830c297009b04f1e4
F. G. Dorais <fgdorais@gmail.com>
2024-05-28 14:18:12 -0400

    feat: lemmas for `modify`

--- Batteries/Data/DArray.lean
+++ Batteries/Data/DArray.lean
@@ -149,7 +149,7 @@ protected def push (a : DArray n α) (v : β) :
     DArray (n+1) fun i => if h : i.val < n then α ⟨i.val, h⟩ else β :=
   mk fun i => if h : i.val < n then dif_pos h ▸ a.get ⟨i.val, h⟩ else dif_neg h ▸ v
 
-/-- Delete the last item of a `DArray`. `O(1)` if exclusive else `O(n)`. -/
+/-- Delete the last item of a `DArray`. `O(1)`. -/
 @[implemented_by popImpl]
 protected def pop (a : DArray (n+1) α) : DArray n fun i => α i.castSucc :=
   mk fun i => a.get i.castSucc
@@ -179,6 +179,28 @@ theorem set_set (a : DArray n α) (i : Fin n) (v w : α i) : (a.set i v).set i w
   else
     rw [get_set_ne _ _ h, get_set_ne _ _ h, get_set_ne _ _ h]
 
+theorem get_modifyF [Functor f] [LawfulFunctor f] (a : DArray n α) (i : Fin n) (t : α i → f (α i)) :
+    (DArray.get . i) <$> a.modifyF i t = t (a.get i) := by
+  simp [DArray.modifyF, ← comp_map]
+  conv => rhs; rw [← id_map (t (a.get i))]
+  congr; ext; simp
+
+@[simp]
+theorem get_modify (a : DArray n α) (i : Fin n) (t : α i → α i) :
+    (a.modify i t).get i = t (a.get i) := get_modifyF (f:=Id) a i t
+
+theorem get_modify_ne (a : DArray n α) (t : α i → α i) (h : i ≠ j) :
+    (a.modify i t).get j = a.get j := get_set_ne _ _ h
+
+@[simp]
+theorem set_modify (a : DArray n α) (i : Fin n) (t : α i → α i) (v : α i) :
+    (a.set i v).modify i t = a.set i (t v) := by
+  ext j
+  if h : i = j then
+    cases h; simp
+  else
+    simp [h, get_modify_ne, get_set_ne]
+
 @[simp]
 theorem uget_eq_get (a : DArray n α) (i : USize) (h : i.toNat < n) :
     a.uget i h = a.get ⟨i.toNat, h⟩ := rfl
@@ -186,6 +208,14 @@ theorem uget_eq_get (a : DArray n α) (i : USize) (h : i.toNat < n) :
 @[simp]
 theorem uset_eq_set (a : DArray n α) (i : USize) (h : i.toNat < n) (v : α ⟨i.toNat, h⟩) :
     a.uset i h v = a.set ⟨i.toNat, h⟩ v := rfl
+
+@[simp]
+theorem umodifyF_eq_modifyF [Functor f] (a : DArray n α) (i : USize) (h : i.toNat < n)
+    (t : α ⟨i.toNat, h⟩ → f (α ⟨i.toNat, h⟩)) : a.umodifyF i h t = a.modifyF ⟨i.toNat, h⟩ t := rfl
+
+@[simp]
+theorem umodify_eq_modify (a : DArray n α) (i : USize) (h : i.toNat < n)
+    (t : α ⟨i.toNat, h⟩ → α ⟨i.toNat, h⟩) : a.umodify i h t = a.modify ⟨i.toNat, h⟩ t := rfl
 
 @[simp]
 theorem copy_eq (a : DArray n α) : a.copy = a := rfl
