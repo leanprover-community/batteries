ee049a587bd75aa2cdbb1a9bff2d155076fe7024
F. G. Dorais <fgdorais@gmail.com>
2024-05-28 08:07:09 -0400

    chore: cleanup and docs

--- Batteries/Data/DArray.lean
+++ Batteries/Data/DArray.lean
@@ -6,20 +6,80 @@ Authors: François G. Dorais
 
 namespace Batteries
 
+/-!
+# Dependent Arrays
+
+`DArray` is a heterogenous array where the type of each item depends on the index. The model
+for this type is the dependent function type `(i : Fin n) → α i` where `α i` is the type assigned
+to items at index `i`.
+
+The implementation of `DArray` is based on Lean's persistent array type. This means that the array
+values are stored in a contiguous memory region and can be accessed in constant time. Lean's
+persistent arrays also support destructive updates when the array is exclusive (RC=1).
+
+### Implementation Details
+
+Lean's persistent array API does not directly support dependent arrays. Each `DArray n α` is
+internally stored as an `Array Unit` with length `n`. This is sound since Lean's persistent array
+implementation does not record nor use the type of the items stored in the array. So it is safe to
+use `UnsafeCast` to convert array items to the appropriate type when necessary.
+-/
+
 /-- `DArray` is a heterogenous array where the type of each item depends on the index. -/
 inductive DArray (n) (α : Fin n → Type _) where
-/-- `DArray` constructor. -/
-| mk (get : (i : Fin n) → α i)
+  /-- Makes a new `DArray` with given item values. -/
+  | mk (get : (i : Fin n) → α i)
 
 namespace DArray
 
-/-- Gets the `DArray` item at index `i`. -/
+section unsafe_implementation
+
+private unsafe abbrev data : DArray n α → Array Unit := unsafeCast
+
+private unsafe def mkImpl (get : (i : Fin n) → α i) : DArray n α :=
+  unsafeCast <| Array.ofFn fun i => (unsafeCast (get i) : Unit)
+
+private unsafe def getImpl (a : DArray n α) (i) : α i :=
+  unsafeCast <| a.data.get ⟨i.val, lcProof⟩
+
+private unsafe def setImpl (a : DArray n α) (i) (v : α i) : DArray n α :=
+  unsafeCast <| a.data.set ⟨i.val, lcProof⟩ <| unsafeCast v
+
+private unsafe def copyImpl (a : DArray n α) : DArray n α :=
+  unsafeCast <| a.data.extract 0 n
+
+end unsafe_implementation
+
+attribute [implemented_by mkImpl] DArray.mk
+
+/-- Gets the `DArray` item at index `i`. `O(1)`. -/
+@[implemented_by getImpl]
 protected def get : DArray n α → (i : Fin n) → α i
   | mk get => get
 
-/-- Sets the `DArray` item at index `i`. -/
+@[simp, inherit_doc DArray.get]
+protected abbrev getN (a : DArray n α) (i) (h : i < n := by get_elem_tactic) : α ⟨i, h⟩ :=
+  a.get ⟨i, h⟩
+
+private def recOnImpl.{u} {motive : DArray n α → Sort u} (a : DArray n α)
+  (h : (get : (i : Fin n) → α i) → motive (.mk get)) : motive a :=
+  h a.get
+
+attribute [implemented_by recOnImpl] DArray.recOn
+attribute [implemented_by recOnImpl] DArray.casesOn
+
+/-- Sets the `DArray` item at index `i`. `O(1)` if exclusive else `O(n)`. -/
+@[implemented_by setImpl]
 protected def set (a : DArray n α) (i : Fin n) (v : α i) : DArray n α :=
   mk fun j => if h : i = j then h ▸ v else a.get j
+
+@[simp, inherit_doc DArray.get]
+protected abbrev setN (a : DArray n α) (i) (h : i < n := by get_elem_tactic) (v : α ⟨i, h⟩) :=
+  a.set ⟨i, h⟩ v
+
+/-- Copies the `DArray` to an exclusive `DArray`. `O(1)` if exclusive else `O(n)`. -/
+@[implemented_by copyImpl]
+protected def copy (a : DArray n α) : DArray n α := mk a.get
 
 @[ext]
 protected theorem ext : {a b : DArray n α} → (∀ i, a.get i = b.get i) → a = b
@@ -39,40 +99,12 @@ theorem get_set_ne (a : DArray n α) (v : α i) (h : i ≠ j) : (a.set i v).get 
   simp only [DArray.get, DArray.set, dif_neg h]
 
 @[simp]
-theorem set_set {α : Fin n → Type _} (a : DArray n α) (i : Fin n)
-    (v w : α i) : (a.set i v).set i w = a.set i w := by
+theorem set_set (a : DArray n α) (i : Fin n) (v w : α i) : (a.set i v).set i w = a.set i w := by
   ext j
   if h : i = j then
     rw [← h, get_set, get_set]
   else
     rw [get_set_ne _ _ h, get_set_ne _ _ h, get_set_ne _ _ h]
 
-/-! # Experimental Unsafe Implementation
-
-For this implementation, `DArray n α` is secretly stored as an `Array Unit` with size `n`. This
-works because Lean never actually checks that objects have the appropriate type. So it's safe, in
-principle, to `unsafeCast` the fake `Unit` or `Array` objects to the appropriate type and similarly
-to `unsafeCast` any relevant object to a fake `Unit` or `Array` object.
--/
-
-private unsafe def mkUnsafe (get : (i : Fin n) → α i) : DArray n α :=
-  let data : Array Unit := .ofFn fun i => unsafeCast (get i)
-  unsafeCast data
-
-private unsafe def getUnsafe (a : DArray n α) (i) : α i :=
-  let data : Array Unit := unsafeCast a
-  unsafeCast <| data.get ⟨i.val, lcProof⟩
-
-private unsafe def setUnsafe (a : DArray n α) (i) (v : α i) : DArray n α :=
-  let data : Array Unit := unsafeCast a
-  unsafeCast <| data.set ⟨i.val, lcProof⟩ <| unsafeCast v
-
-private unsafe def recOnUnsafe.{u} {motive : DArray n α → Sort u} (a : DArray n α)
-  (h : (get : (i : Fin n) → α i) → motive (.mk get)) : motive a :=
-  h a.get
-
-attribute [implemented_by mkUnsafe] DArray.mk
-attribute [implemented_by getUnsafe] DArray.get
-attribute [implemented_by setUnsafe] DArray.set
-attribute [implemented_by recOnUnsafe] DArray.recOn
-attribute [implemented_by recOnUnsafe] DArray.casesOn
+@[simp]
+theorem copy_eq (a : DArray n α) : a.copy = a := rfl
