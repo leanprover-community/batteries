09358acfecb2689bcb370e52657e9b48a934bf54
Kim Morrison <kim@tqft.net>
2024-06-05 12:29:02 +1000

    fix copyright year

--- Batteries/Data/String/Matcher.lean
+++ Batteries/Data/String/Matcher.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2022 F. G. Dorais. All rights reserved.
+Copyright (c) 2023 F. G. Dorais. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: F. G. Dorais
 -/
