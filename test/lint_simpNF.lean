dd6080156858751c5b241420ba390804b0296e1d
Johan Commelin <johan@commelin.net>
2024-06-12 08:02:23 +0200

    add test demonstrating undesired behaviour on dsimp lemmas

--- test/lint_simpNF.lean
+++ test/lint_simpNF.lean
@@ -27,4 +27,30 @@ theorem sumCompl_apply_symm_of_pos (p : α → Prop) [DecidablePred p] (a : α) 
     (sumCompl p).symm a = Sum.inl ⟨a, h⟩ :=
   dif_pos h
 
+def foo (n : Nat) : Nat := if n = n then n else 0
+
+@[simp]
+theorem foo_eq_id : foo = id := by
+  funext n
+  simp [foo]
+
+-- The following `dsimp`-lemma is flagged by the linter
+@[simp]
+theorem foo_eq_ite (n : Nat) : foo n = if n = n then n else 0 := by
+  rfl
+
+end Equiv
+
+/--
+error: /- The `simpNF` linter reports:
+SOME SIMP LEMMAS ARE NOT IN SIMP-NORMAL FORM.
+see note [simp-normal form] for tips how to debug this.
+https://leanprover-community.github.io/mathlib_docs/notes.html#simp-normal%20form -/
+#check Equiv.foo_eq_ite /- simp can prove this:
+  by simp only [Equiv.foo_eq_id, @id_eq, @ite_true]
+One of the lemmas above could be a duplicate.
+If that's not the case try reordering lemmas or adding @[priority].
+ -/
+-/
+#guard_msgs in
 #lint- only simpNF
