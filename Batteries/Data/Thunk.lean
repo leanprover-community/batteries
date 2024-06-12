82899e43395e4f9f1265c42e997f7586d14fba40
Eric Wieser <wieser.eric@gmail.com>
2024-06-12 01:19:24 +0000

    chore: upstream most LazyList results from Mathlib

---
+++ Batteries/Data/Thunk.lean
@@ -0,0 +1,10 @@
+namespace Thunk
+
+@[ext]
+theorem ext {α : Type u} {a b : Thunk α} (eq : a.get = b.get) : a = b := by
+  have ⟨_⟩ := a
+  have ⟨_⟩ := b
+  congr
+  exact funext fun _ ↦ eq
+
+end Thunk
