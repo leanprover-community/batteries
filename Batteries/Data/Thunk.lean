1838cb25c0f366daa79f72948f1e767bbef9efb7
Eric Wieser <wieser.eric@gmail.com>
2024-06-12 01:25:54 +0000

    one more

--- Batteries/Data/Thunk.lean
+++ Batteries/Data/Thunk.lean
@@ -5,6 +5,6 @@ theorem ext {α : Type u} {a b : Thunk α} (eq : a.get = b.get) : a = b := by
   have ⟨_⟩ := a
   have ⟨_⟩ := b
   congr
-  exact funext fun _ ↦ eq
+  exact funext fun _ => eq
 
 end Thunk
