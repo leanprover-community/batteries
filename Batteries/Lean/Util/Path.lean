beaf8911723d98b5bdec391a1ac7963917eb23da
Kyle Miller <kmill31415@gmail.com>
2024-07-08 17:44:26 -0700

    unrelated

--- Batteries/Lean/Util/Path.lean
+++ Batteries/Lean/Util/Path.lean
@@ -11,24 +11,24 @@ import Lean.Elab.Term
 Use this as `searchPathRef.set compile_time_search_path%`.
 -/
 
-open Lean System
+-- open Lean System
 
--- Ideally this instance would be constructed simply by `deriving instance ToExpr for FilePath`
--- but for now we have decided not to upstream the `ToExpr` derive handler from `Mathlib`.
--- https://leanprover.zulipchat.com/#narrow/stream/348111-std4/topic/ToExpr.20derive.20handler/near/386476438
-instance : ToExpr FilePath where
-  toTypeExpr := mkConst ``FilePath
-  toExpr path := mkApp (mkConst ``FilePath.mk) (toExpr path.1)
+-- -- Ideally this instance would be constructed simply by `deriving instance ToExpr for FilePath`
+-- -- but for now we have decided not to upstream the `ToExpr` derive handler from `Mathlib`.
+-- -- https://leanprover.zulipchat.com/#narrow/stream/348111-std4/topic/ToExpr.20derive.20handler/near/386476438
+-- instance : ToExpr FilePath where
+--   toTypeExpr := mkConst ``FilePath
+--   toExpr path := mkApp (mkConst ``FilePath.mk) (toExpr path.1)
 
-/--
-Term elaborator that retrieves the current `SearchPath`.
+-- /--
+-- Term elaborator that retrieves the current `SearchPath`.
 
-Typical usage is `searchPathRef.set compile_time_search_path%`.
+-- Typical usage is `searchPathRef.set compile_time_search_path%`.
 
-This must not be used in files that are potentially compiled on another machine and then
-imported.
-(That is, if used in an imported file it will embed the search path from whichever machine
-compiled the `.olean`.)
--/
-elab "compile_time_search_path%" : term =>
-  return toExpr (← searchPathRef.get)
+-- This must not be used in files that are potentially compiled on another machine and then
+-- imported.
+-- (That is, if used in an imported file it will embed the search path from whichever machine
+-- compiled the `.olean`.)
+-- -/
+-- elab "compile_time_search_path%" : term =>
+--   return toExpr (← searchPathRef.get)
