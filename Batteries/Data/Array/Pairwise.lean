bb4d6ef7e57e0bdd9e551088d8c18c9b71371c42
F. G. Dorais <fgdorais@gmail.com>
2024-07-30 08:45:49 -0400

    fix: use list definition

--- Batteries/Data/Array/Pairwise.lean
+++ Batteries/Data/Array/Pairwise.lean
@@ -17,39 +17,39 @@ larger indices.
 For example `as.Pairwise (· ≠ ·)` asserts that `as` has no duplicates, `as.Pairwise (· < ·)` asserts
 that `as` is strictly sorted and `as.Pairwise (· ≤ ·)` asserts that `as` is weakly sorted.
 -/
-abbrev Pairwise (R : α → α → Prop) (as : Array α) : Prop :=
-  ∀ (i j : Fin as.size), i < j → R as[i] as[j]
+def Pairwise (R : α → α → Prop) (as : Array α) : Prop := as.data.Pairwise R
 
-theorem pairwise_data {as : Array α} : as.data.Pairwise R ↔ as.Pairwise R := by
-  simp [Pairwise, List.pairwise_iff_get, List.get_eq_getElem, data_length, getElem_eq_data_getElem]
+-- TODO: add efficient decidable instance
 
-theorem pairwise_iff_getElem {as : Array α} : as.Pairwise R ↔
+theorem pairwise_defFin {as : Array α} : as.Pairwise R ↔
+    ∀ (i j : Fin as.size) (_ : i < j), R as[i] as[j] := by
+  unfold Pairwise; simp [List.pairwise_iff_get, getElem_fin_eq_data_get]; rfl
+
+theorem pairwise_defNat {as : Array α} : as.Pairwise R ↔
     ∀ (i j : Nat) (_ : i < as.size) (_ : j < as.size) (_ : i < j), R as[i] as[j] := by
-  constructor
-  · intro h i j hi hj hij; exact h ⟨i, hi⟩ ⟨j, hj⟩ hij
-  · intro h ⟨i, hi⟩ ⟨j, hj⟩ hij; exact h i j hi hj hij
+  unfold Pairwise; simp [List.pairwise_iff_getElem, data_length]; rfl
 
 theorem pairwise_empty : #[].Pairwise R := by
-  rw [← pairwise_data]; exact List.Pairwise.nil
+  unfold Pairwise; exact List.Pairwise.nil
 
 theorem pairwise_singleton (R : α → α → Prop) (a) : #[a].Pairwise R := by
-  rw [← pairwise_data]; exact List.pairwise_singleton ..
+  unfold Pairwise; exact List.pairwise_singleton ..
 
 theorem pairwise_pair : #[a, b].Pairwise R ↔ R a b := by
-  rw [← pairwise_data]; exact List.pairwise_pair
+  unfold Pairwise; exact List.pairwise_pair
 
 theorem pairwise_append {as bs : Array α} :
     (as ++ bs).Pairwise R ↔ as.Pairwise R ∧ bs.Pairwise R ∧ (∀ x ∈ as, ∀ y ∈ bs, R x y) := by
-  simp [← pairwise_data, ← mem_data, append_data, ← List.pairwise_append]
+  unfold Pairwise; simp [← mem_data, append_data, ← List.pairwise_append]
 
 theorem pairwise_push {as : Array α} :
     (as.push a).Pairwise R ↔ as.Pairwise R ∧ (∀ x ∈ as, R x a) := by
-  simp [← pairwise_data, ← mem_data, push_data, List.pairwise_append, List.pairwise_singleton,
-    List.mem_singleton]
+  unfold Pairwise
+  simp [← mem_data, push_data, List.pairwise_append, List.pairwise_singleton, List.mem_singleton]
 
 theorem pairwise_extract {as : Array α} (h : as.Pairwise R) (start stop) :
     (as.extract start stop).Pairwise R := by
-  intro _ _ hlt
-  simp only [Fin.getElem_fin, get_extract]
-  rw [pairwise_iff_getElem] at h
-  apply h; exact Nat.add_lt_add_left hlt start
+  simp only [pairwise_defNat, get_extract, size_extract] at h ⊢
+  intro _ _ _ _ hlt
+  apply h
+  exact Nat.add_lt_add_left hlt start
