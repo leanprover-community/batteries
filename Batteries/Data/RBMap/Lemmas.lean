ae005f86a71b4625aa79d8bb200611cddcdd0c1b
Matthew Ballard <matt@mrb.email>
2024-08-13 11:35:09 -0400

    fix

--- Batteries/Data/RBMap/Lemmas.lean
+++ Batteries/Data/RBMap/Lemmas.lean
@@ -25,9 +25,9 @@ attribute [simp] fold foldl foldr Any forM foldlM Ordered
 @[simp] theorem max?_reverse (t : RBNode α) : t.reverse.max? = t.min? := by
   rw [← min?_reverse, reverse_reverse]
 
-@[simp] theorem mem_nil {x} : ¬x ∈ (.nil : RBNode α) := by simp [(·∈·), EMem]
+@[simp] theorem mem_nil {x} : ¬x ∈ (.nil : RBNode α) := by simp [Membership.mem, EMem]
 @[simp] theorem mem_node {y c a x b} :
-    y ∈ (.node c a x b : RBNode α) ↔ y = x ∨ y ∈ a ∨ y ∈ b := by simp [(·∈·), EMem]
+    y ∈ (.node c a x b : RBNode α) ↔ y = x ∨ y ∈ a ∨ y ∈ b := by simp [Membership.mem, EMem]
 
 theorem All_def {t : RBNode α} : t.All p ↔ ∀ x ∈ t, p x := by
   induction t <;> simp [or_imp, forall_and, *]
