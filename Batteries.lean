d3091570b4a34a4f77ef2cdc227838edf625dc85
damiano <adomani@gmail.com>
2024-08-17 01:37:12 +0200

    feat: add lemma code-action (#625)
    Co-authored-by: F. G. Dorais <fgdorais@gmail.com>

--- Batteries.lean
+++ Batteries.lean
@@ -82,6 +82,7 @@ import Batteries.Tactic.Congr
 import Batteries.Tactic.Exact
 import Batteries.Tactic.Init
 import Batteries.Tactic.Instances
+import Batteries.Tactic.Lemma
 import Batteries.Tactic.Lint
 import Batteries.Tactic.Lint.Basic
 import Batteries.Tactic.Lint.Frontend
