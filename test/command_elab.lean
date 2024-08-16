949aa94123420470b32a8a9e8ea4f3464d78df62
Mario Carneiro <di.gama@gmail.com>
2024-08-16 19:49:57 -0400

    try to fix test

--- test/command_elab.lean
+++ test/command_elab.lean
@@ -1,8 +1,6 @@
-import Std.Tactic.RunCmd
-import Std.Tactic.GuardMsgs
-import Std.Lean.Elab.Command
+import Batteries.Lean.Elab.Command
 import Lean.Meta.Tactic.Simp
-import Std.Tactic.Ext
+import Lean.Elab.Tactic.Ext
 
 open Lean Elab Command
 
@@ -14,7 +12,7 @@ Verify that `trySetOption`, `trySetOptions`, and `tryEraseAttrs`
 properly set options, erase attributes, and skip nonexistent ones
 -/
 
-run_cmd do
+run_cmd
   trySetOption `foo true
   trySetOption `pp.unicode.fun true
   trySetOptions #[
@@ -43,14 +41,14 @@ run_cmd do
 #guard_msgs(info) in run_cmd-- true
   logInfo <| repr <| (← getOptions).get `push_neg.use_distrib false
 
-/-- info: #[Lean.Meta.Origin.decl `ne_eq false] -/
+/-- info: #[Lean.Meta.Origin.decl `foo true false, Lean.Meta.Origin.decl `ne_eq true false] -/
 #guard_msgs(info) in run_cmd
   let thms := Lean.Meta.simpExtension.getState (← getEnv)
   logInfo <| repr <| thms.erased.fold Array.push Array.empty
 
 /-- info: #[`Prod.ext] -/
 #guard_msgs(info) in run_cmd-- #[`Prod.ext]
-  let thms := Std.Tactic.Ext.extExtension.getState (← getEnv)
+  let thms := Lean.Elab.Tactic.Ext.extExtension.getState (← getEnv)
   logInfo <| repr <| thms.erased.fold Array.push Array.empty
 
 /-- info: #[`Int.instDivInt, `Nat.instDivNat] -/
