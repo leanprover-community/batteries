e3adfbe8d4be02a4bfbe301dbed9a4e32698dbdc
Shrys <shreyasss94@gmail.com>
2024-08-17 22:40:45 +0000

    Basic file setup

---
+++ Batteries/Data/Vector/Monadic.lean
@@ -0,0 +1,23 @@
+/-
+Copyright (c) 2024 Shreyas Srinivas. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Shreyas Srinivas
+-/
+
+import Batteries.Data.Vector.Basic
+
+
+/-!
+## Monadic Definitions for Vectors
+In this file we add monadic definitions for statically sized vectors.
+-/
+
+namespace Batteries
+
+namespace Vector
+
+
+
+
+end Vector
+end Batteries
