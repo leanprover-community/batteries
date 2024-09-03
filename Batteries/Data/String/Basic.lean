7144419a33de8e6409d7625da95ee567160a1032
Kim Morrison <kim@tqft.net>
2024-09-03 16:30:12 +1000

    chore: upstream/undeprecate String.toAsciiByteArray

--- Batteries/Data/String/Basic.lean
+++ Batteries/Data/String/Basic.lean
@@ -104,3 +104,16 @@ def stripSuffix (s : String) (suff : Substring) : String :=
 /-- Count the occurrences of a character in a string. -/
 def count (s : String) (c : Char) : Nat :=
   s.foldl (fun n d => if d = c then n + 1 else n) 0
+
+/-- Convert a string of assumed-ASCII characters into a byte array.
+(If any characters are non-ASCII they will be reduced modulo 256.) -/
+def toAsciiByteArray (s : String) : ByteArray :=
+  let rec loop (p : Pos) (out : ByteArray) : ByteArray :=
+    if h : s.atEnd p then out else
+    let c := s.get p
+    have : utf8ByteSize s - (next s p).byteIdx < utf8ByteSize s - p.byteIdx :=
+      Nat.sub_lt_sub_left (Nat.lt_of_not_le <| mt decide_eq_true h)
+        (Nat.lt_add_of_pos_right (Char.utf8Size_pos _))
+    loop (s.next p) (out.push c.toUInt8)
+    termination_by utf8ByteSize s - p.byteIdx
+  loop 0 ByteArray.empty
