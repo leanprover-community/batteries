5a45e36015b410f12a45a15f7ce5cfbf86801578
Kim Morrison <kim@tqft.net>
2024-09-11 19:32:28 +1000

    .

--- Batteries/Data/String/Lemmas.lean
+++ Batteries/Data/String/Lemmas.lean
@@ -59,7 +59,8 @@ private theorem ne_self_add_add_utf8Size : i ≠ i + (n + Char.utf8Size c) :=
 
 theorem utf8Len_reverseAux (cs₁ cs₂) :
     utf8Len (cs₁.reverseAux cs₂) = utf8Len cs₁ + utf8Len cs₂ := by
-  induction cs₁ generalizing cs₂ <;> simp_all [← Nat.add_assoc, Nat.add_right_comm]
+  induction cs₁ generalizing cs₂ <;> simp_all [← Nat.add_assoc, Nat.add_right_comm, forall_const]
+
 
 @[simp] theorem utf8Len_reverse (cs) : utf8Len cs.reverse = utf8Len cs := utf8Len_reverseAux ..
 
