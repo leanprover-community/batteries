7863870ce378f85c139b550c6b3b99d91720e7c2
F. G. Dorais <fgdorais@gmail.com>
2024-09-24 17:42:11 -0400

    fix proofs for List.ofFn

--- Batteries/Data/List/OfFn.lean
+++ Batteries/Data/List/OfFn.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
 import Batteries.Data.List.Lemmas
+import Batteries.Data.Fin.Lemmas
 
 /-!
 # Theorems about `List.ofFn`
@@ -12,28 +13,28 @@ import Batteries.Data.List.Lemmas
 namespace List
 
 @[simp]
-theorem length_ofFn_go {n} (f : Fin n → α) (i j h) : length (ofFn.go f i j h) = i := by
-  induction i generalizing j <;> simp_all [ofFn.go]
-
-/-- The length of a list converted from a function is the size of the domain. -/
-@[simp]
-theorem length_ofFn {n} (f : Fin n → α) : length (ofFn f) = n := by
-  simp [ofFn, length_ofFn_go]
-
-theorem getElem_ofFn_go {n} (f : Fin n → α) (i j h) (k) (hk : k < (ofFn.go f i j h).length) :
-    (ofFn.go f i j h)[k] = f ⟨j + k, by simp at hk; omega⟩ := by
-  let i+1 := i
-  cases k <;> simp [ofFn.go, getElem_ofFn_go (i := i)]
-  congr 2; omega
+theorem length_ofFn (f : Fin n → α) : (ofFn f).length = n := by
+  simp only [ofFn]
+  induction n with
+  | zero => simp
+  | succ n ih => simp [Fin.foldr_succ, ih]
 
 @[simp]
-theorem getElem_ofFn {n} (f : Fin n → α) (i : Nat) (h : i < (ofFn f).length) :
+theorem getElem_ofFn (f : Fin n → α) (i : Nat) (h : i < (ofFn f).length) :
     (ofFn f)[i] = f ⟨i, by simp_all⟩ := by
-  simp [ofFn, getElem_ofFn_go]
+  simp only [ofFn]
+  induction n generalizing i with
+  | zero => simp at h
+  | succ n ih =>
+    match i with
+    | 0 => simp [Fin.foldr_succ]
+    | i+1 =>
+      simp only [Fin.foldr_succ]
+      apply ih
+      simp_all
 
-/-- The `n`th element of a list -/
 @[simp]
-theorem getElem?_ofFn {n} (f : Fin n → α) (i) : (ofFn f)[i]? = ofFnNthVal f i :=
+theorem getElem?_ofFn (f : Fin n → α) (i) : (ofFn f)[i]? = ofFnNthVal f i :=
   if h : i < (ofFn f).length
   then by
     rw [getElem?_eq_getElem h, getElem_ofFn]
@@ -42,7 +43,7 @@ theorem getElem?_ofFn {n} (f : Fin n → α) (i) : (ofFn f)[i]? = ofFnNthVal f i
     rw [ofFnNthVal, dif_neg] <;>
     simpa using h
 
-@[simp] theorem ofFn_toArray (f : Fin n → α) : (ofFn f).toArray = Array.ofFn f := by
+@[simp] theorem toArray_ofFn (f : Fin n → α) : (ofFn f).toArray = Array.ofFn f := by
   ext <;> simp
 
 end List
