a3429916e676f12be46a8fb8b8c8165b38f7ee0c
F. G. Dorais <fgdorais@gmail.com>
2024-11-07 16:58:19 -0500

    feat: generate docs from subdirectory

---
+++ BatteriesDocs.lean
@@ -0,0 +1,19 @@
+/-
+Copyright (c) 2024 François G. Dorais. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: François G. Dorais
+-/
+
+import Batteries
+
+/-! # Batteries Documentation
+
+To generate documentation for Batteries:
+
+1. Change to the `BatteriesDocs` subdirectory
+2. Run `lake build BatteriesDocs:docs`
+
+The [`doc-gen4`](https://github.com/leanprover/doc-gen4) documentation will be found in the
+`.lake/buid/doc` and `.lake/build/doc-data` subdirectories.
+
+-/
