1a3fe2e8e46f0ceb8ea084a5596a1d5f6fb90883
F. G. Dorais <fgdorais@gmail.com>
2024-11-11 00:55:10 -0500

    refactor: reorganize docs template

---
+++ docs/docs.lean
@@ -0,0 +1,15 @@
+/- Import project roots here -/
+import Batteries
+
+/-! # Documentation
+
+To generate documentation for this project:
+
+```sh
+cd docs
+lake build
+```
+
+The [`doc-gen4`](https://github.com/leanprover/doc-gen4) documentation will be found in the
+`docs/doc` and `docs/doc-data` subdirectories.
+-/
