041547c89bd271a3f291b1e701def8894f8266ef
Kim Morrison <kim@tqft.net>
2024-11-11 13:34:26 +1100

    somewhat satisfying

--- BatteriesTest/satisfying.lean
+++ BatteriesTest/satisfying.lean
@@ -1,5 +1,6 @@
 import Batteries.Lean.SatisfiesM
 import Batteries.Data.Array.Monadic
+import Batteries.Data.List.Monadic
 
 open Lean Meta Array Elab Term Tactic Command
 
@@ -21,3 +22,21 @@ instance : LawfulMonad CommandElabM :=
 example (xs : Array Expr) : MetaM { ts : Array Expr // ts.size = xs.size } := do
   let r ← satisfying (xs.size_mapM inferType)
   return r
+
+def addRandomEvens (n : Nat) (k : Nat) : IO Nat := do
+  let mut r := k
+  for _ in List.range n do
+    r := r + 2 * (← IO.rand 0 37)
+  pure r
+
+theorem addRandomEvens_spec (n k) : SatisfiesM (fun r => r % 2 = k % 2) (addRandomEvens n k) := by
+  rw [addRandomEvens]
+  simp [List.forIn_yield_eq_foldlM]
+  apply List.SatisfiesM_foldlM
+  · rfl
+  · intros b w a m
+    apply SatisfiesM.bind_pre
+    simp_all [SatisfiesM_EStateM_eq, EStateM.run]
+
+def addRandomEvens' (n : Nat) (k : Nat) : IO { r : Nat // r % 2 = k % 2 } := do
+  satisfying (addRandomEvens_spec n k)
