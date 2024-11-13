31d6d6b66c206354f75a9aba75bf78f594d05412
Kim Morrison <kim@tqft.net>
2024-11-13 16:31:02 +1100

    chore: remove unnecessary import in Batteries.Lean.LawfulMonad

--- Batteries/Lean/LawfulMonad.lean
+++ Batteries/Lean/LawfulMonad.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
-import Batteries.Classes.SatisfiesM
 import Lean.Elab.Command
 
 /-!
