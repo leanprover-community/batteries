44dd1bc58f9c3e23c6f3934f325e9b40b1fb42c3
Kim Morrison <kim@tqft.net>
2024-12-11 09:52:33 +1100

    .

--- BatteriesTest/lint_lean.lean
+++ BatteriesTest/lint_lean.lean
@@ -19,29 +19,33 @@ but it is useful to run locally to see what the linters would catch.
 
 /-! Failing lints that need work. -/
 
--- Many fixed in https://github.com/leanprover/lean4/pull/4620 and subsequent PRs
--- and should be checked again.
 -- #lint only simpNF in all -- Found 22 errors
+-- attribute [nolint simpNF] List.nil_append_fun
 
 /-! Lints that fail, but that we're not intending to do anything about. -/
 
--- Mostly fixed in https://github.com/leanprover/lean4/pull/4621
 -- There are many false positives here.
 -- To run this properly we would need to ignore all declarations with `@[extern]`.
 -- #lint only unusedArguments in all -- Found 89 errors
 
--- After https://github.com/leanprover/lean4/pull/4616, these are all intentional and have
--- `nolint` attributes above.
+-- These are all intentional and have `nolint` attributes above.
 -- #lint only dupNamespace in all -- Found 6 errors
 
--- After https://github.com/leanprover/lean4/pull/4619 all of these should be caused by `abbrev`.
+-- All of these should be caused by `abbrev`.
 -- Unless we decide to upstream something like `alias`, we're not planning on fixing these.
 -- #lint only defLemma in all -- Found 31 errors
 
 /-! Lints that have succeeded in the past, and hopefully still do! -/
 
--- #lint only explicitVarsOfIff in all -- Found 1 errors, `Iff.refl`, which could be nolinted.
+-- -- attribute [nolint explicitVarsOfIff] Iff.refl
+-- -- These next three are awkward to use if we follow the `explicitVarsOfIff` rule.
+-- -- attribute [nolint explicitVarsOfIff] Std.DHashMap.Internal.Raw₀.isEmpty_eq_false_iff_exists_contains_eq_true
+-- -- attribute [nolint explicitVarsOfIff] Std.DHashMap.Internal.Raw₀.isEmpty_iff_forall_contains
+-- -- attribute [nolint explicitVarsOfIff] Std.DHashMap.Internal.Raw₀.mem_keys
+-- #lint only explicitVarsOfIff in all -- Found 4 errors.
 -- #lint only impossibleInstance  in all -- Found 0 errors
 -- #lint only simpVarHead in all -- Found 0 error
--- #lint only unusedHavesSuffices in all  -- Found 0 errors
+-- -- This seems to be a bug in the linter.
+-- -- attribute [nolint unusedHavesSuffices] Std.DHashMap.Internal.AssocList.length_eq
+-- #lint only unusedHavesSuffices in all  -- Found 1 errors
 -- #lint only checkUnivs in all -- Found 0 errors
