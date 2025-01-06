9ac228f4e6e6aadde0fd185104009f82156bc1f3
F. G. Dorais <fgdorais@gmail.com>
2025-01-05 21:04:24 -0500

    fix: use sign extension

--- Batteries/Data/Int/OfBits.lean
+++ Batteries/Data/Int/OfBits.lean
@@ -20,14 +20,27 @@ Construct an integer from bit values (little endian).
 If `fill` is false, the result will be a nonnegative integer with all higher unspecified bits zero.
 If `fill` is true, the result will be a negative integer with all higher unspecified bits one.
 -/
-@[inline] def ofBits (f : Fin n → Bool) : (fill : Bool := false) → Int
+@[inline] def ofBitsFill (f : Fin n → Bool) : (fill : Bool := false) → Int
   | false => Int.ofNat (Nat.ofBits f)
   | true => Int.negSucc (Nat.ofBits fun i => ! f i)
 
-@[simp] theorem testBit_ofBits_lt (f : Fin n → Bool) (fill : Bool) (i) (h : i < n) :
-    (ofBits f fill).testBit i = f ⟨i, h⟩ := by
-  cases fill <;> simp [ofBits, testBit, h]
+@[simp] theorem testBit_ofBitsFill_lt (f : Fin n → Bool) (fill : Bool) (i) (h : i < n) :
+    (ofBitsFill f fill).testBit i = f ⟨i, h⟩ := by
+  cases fill <;> simp [ofBitsFill, testBit, h]
 
-@[simp] theorem testBit_ofBits_ge (f : Fin n → Bool) (fill : Bool) (i) (h : n ≤ i) :
-    (ofBits f fill).testBit i = fill := by
-  cases fill <;> simp [ofBits, testBit, h]
+@[simp] theorem testBit_ofBitsFill_ge (f : Fin n → Bool) (fill : Bool) (i) (h : n ≤ i) :
+    (ofBitsFill f fill).testBit i = fill := by
+  cases fill <;> simp [ofBitsFill, testBit, h]
+
+/--
+Construct an integer from bit values (little endian). The most significant specified bit is used to
+determine the sign:
+* If the most significant specified bit is unset then the result will be nonnegative and all
+  higher order bits will read as unset.
+* If the most significant specified bit is set then the result will be negative and all higher
+  order bits will read as set.
+-/
+@[inline] def ofBits (f : Fin n → Bool) : Int :=
+  match n with
+  | 0 => 0
+  | n+1 => ofBitsFill f (f ⟨n, Nat.lt_succ_self n⟩)
