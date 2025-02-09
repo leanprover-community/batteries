25deaa9b851b602fc9788629b6c49746291e8e97
Kim Morrison <kim@tqft.net>
2025-02-09 17:38:17 +1100

    long lines

--- Batteries/Control/Monad.lean
+++ Batteries/Control/Monad.lean
@@ -5,5 +5,7 @@ Authors: Kim Morrison
 -/
 import Batteries.Tactic.Alias
 
-@[deprecated (since := "2025-02-09")] alias LawfulFunctor.map_inj_right_of_nonempty := map_inj_right_of_nonempty
-@[deprecated (since := "2025-02-09")] alias LawfulMonad.map_inj_right := map_inj_right
+@[deprecated (since := "2025-02-09")] alias LawfulFunctor.map_inj_right_of_nonempty :=
+map_inj_right_of_nonempty
+@[deprecated (since := "2025-02-09")] alias LawfulMonad.map_inj_right :=
+map_inj_right
