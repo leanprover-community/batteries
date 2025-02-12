25fbdde640ff0206fe5cdcf6e9aa1b625741c117
Kim Morrison <kim@tqft.net>
2025-02-13 09:37:20 +1100

    chore: adaptations for nightly-2025-02-12 (#1132)
    Co-authored-by: leanprover-community-mathlib4-bot <leanprover-community-mathlib4-bot@users.noreply.github.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>
    Co-authored-by: Matthew Ballard <matt@mrb.email>

--- Batteries/Data/BinaryHeap.lean
+++ Batteries/Data/BinaryHeap.lean
@@ -40,8 +40,8 @@ def heapifyDown (lt : α → α → Bool) (a : Vector α sz) (i : Fin sz) :
       simp only [maxChild] at h
       split at h
       · split at h
-        · split at h <;> (cases h; simp_arith)
-        · cases h; simp_arith
+        · split at h <;> (cases h; simp +arith)
+        · cases h; simp +arith
       · contradiction
     if lt a[i] a[j] then
       heapifyDown lt (a.swap i j) j
@@ -117,8 +117,8 @@ def popMax (self : BinaryHeap α lt) : BinaryHeap α lt :=
     self.popMax.size = self.size - 1 := by
   simp only [popMax, size]
   split
-  · simp_arith [*]
-  · split <;> simp_arith [*]
+  · simp +arith [*]
+  · split <;> simp +arith
 
 /-- `O(log n)`. Return and remove the maximum element from a `BinaryHeap`. -/
 def extractMax (self : BinaryHeap α lt) : Option α × BinaryHeap α lt :=
