ebb91a69075a8a70c6ca71919daba6820aa8fcc5
Eric Wieser <efw@google.com>
2025-02-14 11:00:53 +0000

    Apply suggestions from code review
    Co-authored-by: François G. Dorais <fgdorais@gmail.com>

--- Batteries/Lean/EStateM.lean
+++ Batteries/Lean/EStateM.lean
@@ -20,11 +20,11 @@ def map {ε σ α β} (f : α → β) (x : Result ε σ α) : Result ε σ β :=
 @[simp] theorem map_error {ε σ α β} (f : α → β) (e : ε) (s : σ) :
     (Result.error e s : Result ε σ α).map f = .error e s := rfl
 
-@[simp] theorem map_eq_ok {ε σ α β} (f : α → β) (x : Result ε σ α) (b : β) (s : σ) :
+@[simp] theorem map_eq_ok {ε σ α β} {f : α → β} {x : Result ε σ α} {b : β} {s : σ} :
     x.map f = .ok b s ↔ ∃ a, x = .ok a s ∧ b = f a := by
   cases x <;> simp [and_assoc, and_comm, eq_comm]
 
-@[simp] theorem map_eq_error {ε σ α β} (f : α → β) (x : Result ε σ α) (e : ε) (s : σ) :
+@[simp] theorem map_eq_error {ε σ α β} (f : α → β) {x : Result ε σ α} {e : ε} {s : σ} :
     x.map f = .error e s ↔ x = .error e s := by
   cases x <;> simp [eq_comm]
 
@@ -33,7 +33,7 @@ end Result
 @[simp] theorem run_map (f : α → β) (x : EStateM ε σ α) :
     (f <$> x).run s = (x.run s).map f := rfl
 
-@[ext] theorem ext {ε σ α} (x y : EStateM ε σ α) (h : ∀ s, x.run s = y.run s) : x = y := by
+@[ext] theorem ext {ε σ α} {x y : EStateM ε σ α} (h : ∀ s, x.run s = y.run s) : x = y := by
   funext s
   exact h s
 
