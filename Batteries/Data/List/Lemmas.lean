5a56dfa58c18a1c4c485e5e630f9035e56c0952b
Kim Morrison <kim@tqft.net>
2025-04-02 19:15:01 +1100

    chore: bump toolchain to v4.19.0-rc1 (#1186)
    Co-authored-by: leanprover-community-mathlib4-bot <leanprover-community-mathlib4-bot@users.noreply.github.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>
    Co-authored-by: Matthew Ballard <matt@mrb.email>
    Co-authored-by: Markus Himmel <markus@lean-fro.org>
    Co-authored-by: Kyle Miller <kmill31415@gmail.com>
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>
    Co-authored-by: Sebastian Ullrich <sebasti@nullri.ch>
    Co-authored-by: Joachim Breitner <mail@joachim-breitner.de>
    Co-authored-by: Mac Malone <mac@lean-fro.org>

--- Batteries/Data/List/Lemmas.lean
+++ Batteries/Data/List/Lemmas.lean
@@ -38,7 +38,7 @@ theorem dropLast_eq_eraseIdx {xs : List α} {i : Nat} (last_idx : i + 1 = xs.len
 
 /-! ### set -/
 
-theorem set_eq_modify (a : α) : ∀ n (l : List α), set l n a = modify (fun _ => a) n l
+theorem set_eq_modify (a : α) : ∀ n (l : List α), l.set n a = l.modify n (fun _ => a)
   | 0, l => by cases l <;> rfl
   | _+1, [] => rfl
   | _+1, _ :: _ => congrArg (cons _) (set_eq_modify _ _ _)
@@ -48,7 +48,7 @@ theorem set_eq_take_cons_drop (a : α) {n l} (h : n < length l) :
   rw [set_eq_modify, modify_eq_take_cons_drop h]
 
 theorem modify_eq_set_getElem? (f : α → α) :
-    ∀ n (l : List α), l.modify f n = ((fun a => l.set n (f a)) <$> l[n]?).getD l
+    ∀ n (l : List α), l.modify n f = ((fun a => l.set n (f a)) <$> l[n]?).getD l
   | 0, l => by cases l <;> simp
   | _+1, [] => rfl
   | n+1, b :: l =>
@@ -57,7 +57,7 @@ theorem modify_eq_set_getElem? (f : α → α) :
 @[deprecated (since := "2025-02-15")] alias modify_eq_set_get? := modify_eq_set_getElem?
 
 theorem modify_eq_set_get (f : α → α) {n} {l : List α} (h) :
-    l.modify f n = l.set n (f (l.get ⟨n, h⟩)) := by
+    l.modify n f = l.set n (f (l.get ⟨n, h⟩)) := by
   rw [modify_eq_set_getElem?, getElem?_eq_getElem h]; rfl
 
 theorem getElem?_set_eq_of_lt (a : α) {n} {l : List α} (h : n < length l) :
@@ -184,7 +184,7 @@ theorem disjoint_of_disjoint_cons_left {l₁ l₂} : Disjoint (a :: l₁) l₂ �
 theorem disjoint_of_disjoint_cons_right {l₁ l₂} : Disjoint l₁ (a :: l₂) → Disjoint l₁ l₂ :=
   disjoint_of_subset_right (subset_cons_self _ _)
 
-@[simp] theorem disjoint_nil_left (l : List α) : Disjoint [] l := fun a => (not_mem_nil a).elim
+@[simp] theorem disjoint_nil_left (l : List α) : Disjoint [] l := fun _ => not_mem_nil.elim
 
 @[simp] theorem disjoint_nil_right (l : List α) : Disjoint l [] := by
   rw [disjoint_comm]; exact disjoint_nil_left _
@@ -328,7 +328,7 @@ theorem Sublist.diff_right : ∀ {l₁ l₂ l₃ : List α}, l₁ <+ l₂ → l�
 
 theorem Sublist.erase_diff_erase_sublist {a : α} :
     ∀ {l₁ l₂ : List α}, l₁ <+ l₂ → (l₂.erase a).diff (l₁.erase a) <+ l₂.diff l₁
-  | [], _, _ => erase_sublist _ _
+  | [], _, _ => erase_sublist
   | b :: l₁, l₂, h => by
     if heq : b = a then
       simp [heq]
--- Batteries/Tactic/Alias.lean
+++ Batteries/Tactic/Alias.lean
@@ -112,6 +112,7 @@ elab (name := alias) mods:declModifiers "alias " alias:ident " := " name:ident :
     addDeclarationRangesFromSyntax declName (← getRef) alias
     Term.addTermInfo' alias (← mkConstWithLevelParams declName) (isBinder := true)
     addDocString' declName declMods.docString?
+    enableRealizationsForConst declName
     Term.applyAttributes declName declMods.attrs
     let info := (← getAliasInfo name).getD <| AliasInfo.plain name
     setAliasInfo info declName
@@ -123,7 +124,7 @@ elab (name := alias) mods:declModifiers "alias " alias:ident " := " name:ident :
       let mut doc := info.toString
       if let some origDoc ← findDocString? (← getEnv) name then
         doc := s!"{doc}\n\n---\n\n{origDoc}"
-      addDocString declName doc
+      addDocStringCore declName doc
 
 /--
 Given a possibly forall-quantified iff expression `prf`, produce a value for one
@@ -158,7 +159,7 @@ private def addSide (mp : Bool) (declName : Name) (declMods : Modifiers) (thm : 
     let mut doc := info.toString
     if let some origDoc ← findDocString? (← getEnv) thm.name then
       doc := s!"{doc}\n\n---\n\n{origDoc}"
-    addDocString declName doc
+    addDocStringCore declName doc
 
 @[inherit_doc «alias»]
 elab (name := aliasLR) mods:declModifiers "alias "
