32307dfddbe804f98e65e42bcfcf817ece53dc83
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-04-10 14:01:57 +0200

    Specify imports

--- Shake/Main.lean
+++ Shake/Main.lean
@@ -3,8 +3,10 @@ Copyright (c) 2023 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
+import Lean.Environment
+import Lean.Parser.Module
 import Lean.Util.FoldConsts
-import Lean
+import Lean.Util.Paths
 
 /-! # `lake exe shake` command
 
