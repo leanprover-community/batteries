886739b05f144f4870aa658403cbae6c08618fd9
Wrenna Robson <wren.robson@gmail.com>
2025-05-22 14:15:57 +0100

    Apply suggestions from code review
    Co-authored-by: François G. Dorais <fgdorais@gmail.com>

--- Batteries/Data/Vector/Lemmas.lean
+++ Batteries/Data/Vector/Lemmas.lean
@@ -43,20 +43,22 @@ theorem toArray_injective : ∀ {v w : Vector α n}, v.toArray = w.toArray → v
 
 /-! ### tail lemmas -/
 
-theorem tail_zero {v : Vector α 0} :
-    v.tail = #v[] := Vector.eq_empty
+theorem tail_zero {v : Vector α 0} : v.tail = #v[] := Vector.eq_empty
+
 theorem tail_ne_zero [NeZero n] {v : Vector α n} :
     v.tail = v.eraseIdx 0 n.pos_of_neZero := dif_pos _
 
 @[simp] theorem toList_tail {v : Vector α n} :
-    v.tail.toList = v.toList.tail := match n with
+    v.tail.toList = v.toList.tail :=
+  match n with
   | 0 => by simp only [tail_zero, Vector.eq_empty, List.tail_nil]
   | _ + 1 => by simp only [tail_ne_zero, Vector.toList_eraseIdx, List.eraseIdx_zero]
 
 /-! ### getElem lemmas -/
 
 theorem getElem_tail {v : Vector α n} {i : Nat} (hi : i < n - 1) :
-    (v.tail)[i] = v[i + 1] := match n with
+    v.tail[i] = v[i + 1] :=
+  match n with
   | _ + 1 => getElem_congr_coll tail_ne_zero |>.trans <| getElem_eraseIdx (Nat.zero_lt_succ _) hi
 
 /-! ### get lemmas -/
