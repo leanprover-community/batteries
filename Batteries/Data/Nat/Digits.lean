f962db7fb47197d66ac96a454870f605897ef7e6
Marcus Rossel <marcusrossel@icloud.com>
2025-06-08 15:33:53 +0200

    Change `isDigit_digitChar_iff_lt` to `isDigit_digitChar`

--- Batteries/Data/Nat/Digits.lean
+++ Batteries/Data/Nat/Digits.lean
@@ -6,13 +6,13 @@ Authors: Marcus Rossel
 
 namespace Nat
 
-theorem isDigit_digitChar_iff_lt : n.digitChar.isDigit ↔ (n < 10) :=
+@[simp]
+theorem isDigit_digitChar : n.digitChar.isDigit = decide (n < 10) :=
   match n with
-  | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 => ⟨by decide, by simp [*, digitChar]⟩
+  | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 => by simp [digitChar]
   | _ + 10 => by
-    refine ⟨fun h => ?_, fun _ => by contradiction⟩
-    simp only [digitChar, Nat.reduceEqDiff] at h
-    (repeat' split at h) <;> contradiction
+    simp only [digitChar, ↓reduceIte, Nat.reduceEqDiff]
+    (repeat' split) <;> simp
 
 private theorem isDigit_of_mem_toDigitsCore_10
     (hf : n < fuel) (hc : c ∈ cs → c.isDigit) (h : c ∈ toDigitsCore 10 fuel n cs) :
@@ -24,7 +24,7 @@ private theorem isDigit_of_mem_toDigitsCore_10
     case' isFalse => apply ih (by omega) (fun h => ?_) h
     all_goals
       cases h
-      next => have := isDigit_digitChar_iff_lt.mpr (mod_lt n <| by decide); simp_all
+      next => simp [mod_lt]
       next hm => exact hc hm
 
 theorem isDigit_of_mem_toDigits_10 (h : c ∈ toDigits 10 n) : c.isDigit :=
