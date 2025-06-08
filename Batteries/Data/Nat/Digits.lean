a44eb80aa3cb19688ccf4760e51281f0a80419b6
Marcus Rossel <marcusrossel@icloud.com>
2025-06-08 17:36:35 +0200

    Add two additional lemmas

--- Batteries/Data/Nat/Digits.lean
+++ Batteries/Data/Nat/Digits.lean
@@ -29,3 +29,12 @@ private theorem isDigit_of_mem_toDigitsCore_10
 
 theorem isDigit_of_mem_toDigits_10 (h : c ∈ toDigits 10 n) : c.isDigit :=
   isDigit_of_mem_toDigitsCore_10 (Nat.lt_succ_self _) (fun _ => by contradiction) h
+
+theorem toDigits_10_of_lt_10 (h : n < 10) : toDigits 10 n = [n.digitChar] :=
+  match n with
+  | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 => by decide
+  | _ + 10                                => by contradiction
+
+@[simp]
+theorem Nat.toDigits_10_natAbs_ofNat (n : Nat) : toDigits 10 (n : Int).natAbs = toDigits 10 n := by
+  simp
