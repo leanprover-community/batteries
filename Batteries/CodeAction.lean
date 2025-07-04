619e5040aa466432e44ceec6cc0367fd8d4e443d
Moritz Roos <mo.roos@gmx.de>
2025-07-04 20:40:12 +0200

    delete probably unneccesary examples, that just produce a bunch of warnings for sorrys

--- Batteries/CodeAction.lean
+++ Batteries/CodeAction.lean
@@ -1,37 +1,3 @@
 import Batteries.CodeAction.Attr
 import Batteries.CodeAction.Basic
 import Batteries.CodeAction.Misc
-
-
-example (x : Nat) : x = x := by
-  induction x with
-  | zero => sorry
-  | succ n _ => sorry
-
-/-direct pattern completion here will be worked on, when the explicit match is deemed good--/
-def myfun : Nat → Nat → Nat
-  | .zero => sorry
-  | .succ n => sorry
-
---These will be generated with underscores instead of sorry's
-def myfun2 (n:Nat) : Nat :=
-  match n with
-  | .zero => sorry
-  | .succ n => sorry
-
-
-
-def myfun3 (e: Lean.Expr) : Bool :=
-  match e with
-  | .bvar deBruijnIndex => sorry
-  | .fvar fvarId => sorry
-  | .mvar mvarId => sorry
-  | .sort u => sorry
-  | .const declName us => sorry
-  | .app fn arg => sorry
-  | .lam binderName binderType body binderInfo => sorry
-  | .forallE binderName binderType body binderInfo => sorry
-  | .letE declName type value body nonDep => sorry
-  | .lit _ => sorry
-  | .mdata data expr => sorry
-  | .proj typeName idx struct => sorry
