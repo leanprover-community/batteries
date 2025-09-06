eb28e8fa7fa7b8cd999899dcc35833666988631e
Eric Wieser <wieser.eric@gmail.com>
2025-09-06 22:19:36 +0100

    fix lint

--- Batteries/Control/Lemmas.lean
+++ Batteries/Control/Lemmas.lean
@@ -20,10 +20,12 @@ attribute [ext] ReaderT.ext
 @[simp] theorem run_throwThe [MonadExceptOf ε m] (e : ε) (ctx : ρ) :
     (throwThe ε e : ReaderT ρ m α).run ctx = throwThe ε e := rfl
 
-@[simp] theorem run_tryCatch [MonadExceptOf ε m] (body : ReaderT ρ m α) (handler : ε → ReaderT ρ m α) (ctx : ρ) :
+@[simp] theorem run_tryCatch [MonadExceptOf ε m]
+    (body : ReaderT ρ m α) (handler : ε → ReaderT ρ m α) (ctx : ρ) :
     (tryCatch body handler).run ctx = tryCatch (body.run ctx) (handler · |>.run ctx) := rfl
-  
-@[simp] theorem run_tryCatchThe [MonadExceptOf ε m] (body : ReaderT ρ m α) (handler : ε → ReaderT ρ m α) (ctx : ρ) :
+
+@[simp] theorem run_tryCatchThe [MonadExceptOf ε m]
+    (body : ReaderT ρ m α) (handler : ε → ReaderT ρ m α) (ctx : ρ) :
     (tryCatchThe ε body handler).run ctx = tryCatchThe ε (body.run ctx) (handler · |>.run ctx) := rfl
 
 end ReaderT
@@ -44,10 +46,12 @@ attribute [ext] StateT.ext
 @[simp] theorem run_throwThe [Monad m] [MonadExceptOf ε m] (e : ε) (s : σ) :
     (throwThe ε e : StateT σ m α).run s = (do let a ← throwThe ε e; pure (a, s)) := rfl
 
-@[simp] theorem run_tryCatch [Monad m] [MonadExceptOf ε m] (body : StateT σ m α) (handler : ε → StateT σ m α) (s : σ) :
+@[simp] theorem run_tryCatch [Monad m] [MonadExceptOf ε m]
+    (body : StateT σ m α) (handler : ε → StateT σ m α) (s : σ) :
     (tryCatch body handler).run s = tryCatch (body.run s) (handler · |>.run s) := rfl
-  
-@[simp] theorem run_tryCatchThe [Monad m] [MonadExceptOf ε m] (body : StateT σ m α) (handler : ε → StateT σ m α) (s : σ) :
+
+@[simp] theorem run_tryCatchThe [Monad m] [MonadExceptOf ε m]
+    (body : StateT σ m α) (handler : ε → StateT σ m α) (s : σ) :
     (tryCatchThe ε body handler).run s = tryCatchThe ε (body.run s) (handler · |>.run s) := rfl
 
 end StateT
