73f14714683d6c1c5c3304f04d369ec6451d8e53
Sebastian Ullrich <sebasti@nullri.ch>
2025-10-24 11:56:59 +0200

    Reapply "remove defunct instances"
    This reverts commit 979dcd044802cb9f198e99e8a0d55866adf9f8b4.

--- Batteries/Lean/LawfulMonad.lean
+++ Batteries/Lean/LawfulMonad.lean
@@ -6,7 +6,6 @@ Authors: Kim Morrison
 module
 
 public import Lean.Elab.Command
-import all Init.System.ST
 
 @[expose] public section
 
@@ -16,27 +15,19 @@ import all Init.System.ST
 
 open Lean Elab Term Tactic Command
 
-instance : LawfulMonad (ST σ) := .mk' _
-  (id_map := fun x => rfl)
-  (pure_bind := fun x f => rfl)
-  (bind_assoc := fun f g x => rfl)
+-- instance : LawfulMonad (EIO ε) := inferInstanceAs <| LawfulMonad (EStateM _ _)
+-- instance : LawfulMonad BaseIO := inferInstanceAs <| LawfulMonad (EIO _)
+-- instance : LawfulMonad IO := inferInstanceAs <| LawfulMonad (EIO _)
 
-instance  : LawfulMonad (EST ε σ) := .mk' _
-  (id_map := fun x => funext fun v => by dsimp [Functor.map, EST.bind]; cases x v <;> rfl)
-  (pure_bind := fun x f => rfl)
-  (bind_assoc := fun f g x => funext fun v => by dsimp [Bind.bind, EST.bind]; cases f v <;> rfl)
+-- instance : LawfulMonad (EST ε σ) := inferInstanceAs <| LawfulMonad (EStateM _ _)
 
-instance : LawfulMonad (EIO ε) := inferInstanceAs <| LawfulMonad (EST _ _)
-instance : LawfulMonad BaseIO := inferInstanceAs <| LawfulMonad (ST _)
-instance : LawfulMonad IO := inferInstanceAs <| LawfulMonad (EIO _)
-
-instance : LawfulMonad CoreM :=
-  inferInstanceAs <| LawfulMonad (ReaderT _ <| StateRefT' _ _ (EIO Exception))
-instance : LawfulMonad MetaM :=
-  inferInstanceAs <| LawfulMonad (ReaderT _ <| StateRefT' _ _ CoreM)
-instance : LawfulMonad TermElabM :=
-  inferInstanceAs <| LawfulMonad (ReaderT _ <| StateRefT' _ _ MetaM)
-instance : LawfulMonad TacticM :=
-  inferInstanceAs <| LawfulMonad (ReaderT _ $ StateRefT' _ _ $ TermElabM)
-instance : LawfulMonad CommandElabM :=
-  inferInstanceAs <| LawfulMonad (ReaderT _ $ StateRefT' _ _ $ EIO _)
+-- instance : LawfulMonad CoreM :=
+--   inferInstanceAs <| LawfulMonad (ReaderT _ <| StateRefT' _ _ (EIO Exception))
+-- instance : LawfulMonad MetaM :=
+--   inferInstanceAs <| LawfulMonad (ReaderT _ <| StateRefT' _ _ CoreM)
+-- instance : LawfulMonad TermElabM :=
+--   inferInstanceAs <| LawfulMonad (ReaderT _ <| StateRefT' _ _ MetaM)
+-- instance : LawfulMonad TacticM :=
+--   inferInstanceAs <| LawfulMonad (ReaderT _ $ StateRefT' _ _ $ TermElabM)
+-- instance : LawfulMonad CommandElabM :=
+--   inferInstanceAs <| LawfulMonad (ReaderT _ $ StateRefT' _ _ $ EIO _)
