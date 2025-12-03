7aee4eeaa7e5109d2ab6187c8c0a45b53fac17f5
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-12-03 12:59:07 +0100

    feat: upstream the `library_note` command from Mathlib
    This PR moves Mathlib's `library_note2` command to Batteries, replacing
    `library_note`. The main difference, apart from a slightly changed
    syntax, is `library_note` now adds a declaration to the `LibraryNote`
    namespace.
    
    As a consequence, it is no longer possible to have two library notes
    with the same name. (Was this feature ever used?)

--- Batteries/Util/LibraryNote.lean
+++ Batteries/Util/LibraryNote.lean
@@ -17,8 +17,16 @@ namespace Batteries.Util.LibraryNote
 
 open Lean
 
-/-- A library note consists of a (short) tag and a (long) note. -/
-@[expose] def LibraryNoteEntry := String × String
+/-- A library note is identified by the name of its declaration, and its content should be contained
+in its doc-string. -/
+@[expose] def LibraryNote := Unit
+deriving Inhabited
+
+/-- Entry for library notes in the environment extension.
+
+We only store the name, and look up the constant's docstring to find its contents.
+-/
+@[expose] def LibraryNoteEntry := Name
 deriving Inhabited
 
 /-- Environment extension supporting `library_note`. -/
@@ -28,20 +36,48 @@ initialize libraryNoteExt : SimplePersistentEnvExtension LibraryNoteEntry (Array
     addImportedFn := Array.flatMap id
   }
 
-open Lean Parser Command in
-/--
-```
-library_note "some tag" /--
-... some explanation ...
--/
-```
-creates a new "library note", which can then be cross-referenced using
+open Elab Command in
+/-- `library_note «my note» /-- documentation -/` creates a library note named `my note`
+in the `LibraryNote` namespace, whose content is `/-- documentation -/`.
+This can then be cross-referenced using
 ```
 -- See note [some tag]
 ```
 in doc-comments.
+You can access the contents using, for example, `#print LibraryNote.«my note»`.
 Use `#help note "some tag"` to display all notes with the tag `"some tag"` in the infoview.
 This command can be imported from Batteries.Tactic.HelpCmd .
 -/
-elab "library_note " title:strLit ppSpace text:docComment : command => do
-  modifyEnv (libraryNoteExt.addEntry · (title.getString, text.getDocString))
+elab "library_note " name:ident ppSpace dc:docComment : command => do
+  modifyEnv (libraryNoteExt.addEntry · name.getId)
+  let stx ← `(
+    $dc:docComment
+    def $(mkIdent (`_root_.LibraryNote ++ name.getId)) : LibraryNote :=
+      default)
+  elabCommandTopLevel stx
+
+open Elab Command in
+/-- Support the old `library_note "foo"` syntax, with a deprecation warning. -/
+elab "library_note " name:str ppSpace dc:docComment : command => do
+  logWarningAt name <|
+    "deprecation warning: library_note now takes an identifier instead of a string.\n" ++
+    "Hint: replace the double quotes with «french quotes»."
+  let name := Name.mkSimple name.getString
+  let stx ← `(library_note $(mkIdent name):ident $dc:docComment)
+  elabCommandTopLevel stx
+
+open Elab Command in
+/-- Support the old `library_note2 «foo»` syntax, with a deprecation warning. -/
+elab "library_note2 " name:ident ppSpace dc:docComment : command => do
+  logWarningAt name <|
+    "deprecation warning: library_note2 has been replaced with library_note."
+  let stx ← `(library_note $name:ident $dc:docComment)
+  elabCommandTopLevel stx
+
+open Elab Command in
+/-- Support the old `library_note2 "foo"` syntax, with a deprecation warning. -/
+elab "library_note2 " name:str ppSpace dc:docComment : command => do
+  logWarningAt name <|
+    "deprecation warning: library_note2 has been replaced with library_note."
+  let stx ← `(library_note name $dc:docComment)
+  elabCommandTopLevel stx
