231035c7a2ab2a16890447404ef2d32adfd5212d
F. G. Dorais <fgdorais@gmail.com>
2026-01-06 18:18:09 -0500

    chore: style

--- Batteries/Control/Alternative.lean
+++ Batteries/Control/Alternative.lean
@@ -1,28 +1,31 @@
 /-
-Copyright (c) 2025 Owen Shepherd. All rights reserved.
+Copyright (c) 2026 Owen Shepherd. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Owen Shepherd
 -/
+module
+
+namespace Alternative
 
 /--
 Zero or more Alternatives.
 For example, given a Parser, which implements Alternative, you can use the `many` combinator
 to parse zero or more items.
 -/
-partial def Alternative.many {a : Type} [Alternative f] [Inhabited (f (List a))]
-    (p : f a) : f (List a) :=
-  List.cons <$> p <*> Alternative.many p <|> pure []
+partial def many [Alternative f] [Inhabited (f (List α))]
+    (p : f α) : f (List α) :=
+  List.cons <$> p <*> many p <|> pure []
 
 /--
 One or more Alternatives.
 For example, given a Parser, which implements Alternative, you can use the `many1` combinator
 to parse one or more items.
 -/
-def Alternative.many1 [Alternative f] [Inhabited (f (List a))]
-    (p : f a) : f (Σ n, Vector a (1 + n)) :=
+def many1 [Alternative f] [Inhabited (f (List α))]
+    (p : f α) : f (Σ n, Vector α (1 + n)) :=
   let g x xs := ⟨xs.1, Vector.singleton x ++ xs.2⟩
-  let toVec (l : List a) : Σ n, Vector a n :=
+  let toVec (l : List α) : Σ n, Vector α n :=
     let arr := List.toArray l
     ⟨arr.size, ⟨arr, rfl⟩⟩
-  let manyVec : f (Σ n, (Vector a n)) := toVec <$> Alternative.many p
+  let manyVec : f (Σ n, (Vector α n)) := toVec <$> Alternative.many p
   g <$> p <*> manyVec <|> Alternative.failure
