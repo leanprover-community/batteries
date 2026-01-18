3fb4241c15f2609cbae5908724d020aa2b06dbb9
Owen Shepherd <owen@owen.cafe>
2026-01-18 16:44:28 +0000

    Revert "Remove `many1`, to be separated out into its own PR"
    This reverts commit d5d7e846e27a50ca506fa98570e7b0a8bfd2af4b.

--- Batteries/Control/Alternative.lean
+++ Batteries/Control/Alternative.lean
@@ -19,3 +19,17 @@ to parse zero or more items.
 partial def many [Alternative f] (p : f α) : f (List α) :=
   letI : Nonempty (f (List α)) := ⟨pure []⟩
   List.cons <$> p <*> many.aux p <|> pure []
+
+/--
+One or more Alternatives.
+For example, given a Parser, which implements Alternative, you can use the `many1` combinator
+to parse one or more items.
+-/
+def many1 [Alternative f]
+    (p : f α) : f (Σ n, Vector α (1 + n)) :=
+  let g x xs := ⟨xs.1, Vector.singleton x ++ xs.2⟩
+  let toVec (l : List α) : Σ n, Vector α n :=
+    let arr := List.toArray l
+    ⟨arr.size, ⟨arr, rfl⟩⟩
+  let manyVec : f (Σ n, (Vector α n)) := toVec <$> Alternative.many p
+  g <$> p <*> manyVec <|> Alternative.failure
