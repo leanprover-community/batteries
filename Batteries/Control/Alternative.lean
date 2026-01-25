efedef4302dc1b2465a4805f061e1f84b6917934
Owen Shepherd <owen@owen.cafe>
2026-01-25 09:11:02 +0000

    Remove many.aux

--- Batteries/Control/Alternative.lean
+++ Batteries/Control/Alternative.lean
@@ -7,18 +7,14 @@ module
 
 namespace Alternative
 
-private partial def many.aux [Alternative f] [Nonempty (f (List α))]
-    (p : f α) : f (List α) :=
-  List.cons <$> p <*> many.aux p <|> pure []
-
 /--
 Zero or more Alternatives.
 For example, given a Parser, which implements Alternative, you can use the `many` combinator
 to parse zero or more items.
 -/
-partial def many [Alternative f] (p : f α) : f (List α) :=
-  letI : Nonempty (f (List α)) := ⟨pure []⟩
-  List.cons <$> p <*> many.aux p <|> pure []
+private partial def many [Alternative f]
+    (p : f α) : f (List α) :=
+  List.cons <$> p <*> many p <|> pure []
 
 /--
 One or more Alternatives.
