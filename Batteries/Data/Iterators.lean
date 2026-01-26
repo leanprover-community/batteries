cbded182f171c908958d5f947f064f11c29825de
Chad Sharp <cmlsharp@umich.edu>
2026-01-25 21:44:08 -0500

    satisfy linter

--- Batteries/Data/Iterators.lean
+++ Batteries/Data/Iterators.lean
@@ -1,3 +1,5 @@
 module
-public import Batteries.Data.Iterators.Scan
+
 public import Batteries.Data.Iterators.Lemmas
+public import Batteries.Data.Iterators.Lemmas.Scan
+public import Batteries.Data.Iterators.Scan
