bafc5fb5c98e9f61d012920f7650962d366fe5c4
Chad Sharp <cmlsharp@umich.edu>
2026-01-26 13:02:24 -0500

    move public section

--- Batteries/Data/Iterators/Lemmas/Scan.lean
+++ Batteries/Data/Iterators/Lemmas/Scan.lean
@@ -9,14 +9,12 @@ public import Batteries.Data.Iterators.Scan
 public import Batteries.Data.List.Scan
 public import Batteries.Data.Array.Scan
 
-public section
-
 namespace Std.Iterators
 
-private theorem toIterM_eq_mk {state : ScanM α m n β γ f} :
+theorem toIterM_eq_mk {state : ScanM α m n β γ f} :
     (toIterM state n γ : IterM n γ) = { internalState := state } := rfl
 
-private theorem toList_scanM_emittedTrue [Iterator α Id β] [Finite α Id] [Monad m] [LawfulMonad m]
+theorem toList_scanM_emittedTrue [Iterator α Id β] [Finite α Id] [Monad m] [LawfulMonad m]
   [IteratorCollect α Id Id (β := β)] [LawfulIteratorCollect α Id Id (β := β)]
   {f : γ → β → m γ} {acc : γ} {it : IterM (α := α) Id β} :
     (toIterM (m := m) (β := γ) (⟨it.internalState, acc, true⟩ : ScanM α Id m β γ f)).toList
@@ -41,6 +39,8 @@ private theorem toList_scanM_emittedTrue [Iterator α Id β] [Finite α Id] [Mon
     simp_all [liftM, monadLift, Id.run, toIterM]
   | ⟨.done, hp⟩ =>
     simp_all
+
+public section
 
 @[simp]
 theorem IterM.toList_scanM [Iterator α Id β] [Finite α Id] [Monad m] [LawfulMonad m]
@@ -107,4 +107,5 @@ theorem Iter.toArray_scan [Iterator α Id β] [Finite α Id]
   unfold scan
   apply IterM.toArray_scan
 
+end
 end Std.Iterators
