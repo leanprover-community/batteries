73b0c26afd7516af968d07222b471cf3eabed934
Chad Sharp <cmlsharp@umich.edu>
2026-01-27 22:48:24 -0500

    update docstrings

--- Batteries/Data/Bool.lean
+++ Batteries/Data/Bool.lean
@@ -7,14 +7,24 @@ module
 
 @[expose] public section
 
-/-- Boolean '<' is well founded -/
+/--
+Boolean '<' is well founded
+These are not instances, despite WellFoundedRelation being a class, as we provide versions for both
+< and >. If you need instances, you may use the WellFoundedLT and WellFoundedGT classes available in
+Mathlib.
+-/
 def Bool.lt_wfRel : WellFoundedRelation Bool where
   rel := (· < ·)
   wf := ⟨fun
     | false => ⟨false, nofun⟩
     | true => ⟨true, fun | false, _ => ⟨false, nofun⟩⟩⟩
 
-/-- Boolean '>' is well founded -/
+/--
+Boolean '>' is well founded
+These are not instances, despite WellFoundedRelation being a class, as we provide versions for both
+< and >. If you need instances, you may use the WellFoundedLT and WellFoundedGT classes available in
+Mathlib.
+-/
 def Bool.gt_wfRel : WellFoundedRelation Bool where
   rel := (· > ·)
   wf := ⟨fun
