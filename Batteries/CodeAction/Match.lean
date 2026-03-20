3cee882a310529c3599eeea6ca9990beda929813
Moritz Roos macbook <mo.roos@gmx.de>
2026-03-20 02:14:23 +0100

    add License and author for Match.lean

--- Batteries/CodeAction/Match.lean
+++ Batteries/CodeAction/Match.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Moritz Roos. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Moritz Roos
+-/
 module
 
 public meta import Batteries.CodeAction.Misc
