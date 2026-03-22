3e7f7fce5684752d7a204b87a9c36e94713b6303
Yuyang Zhao 赵雨扬 <zhao.yu-yang@foxmail.com>
2026-03-22 08:55:48 +0800

    Squashed commit

--- Batteries/Data/Nat.lean
+++ Batteries/Data/Nat.lean
@@ -1,6 +1,7 @@
 module
 
 public import Batteries.Data.Nat.Basic
+public import Batteries.Data.Nat.BinaryRec
 public import Batteries.Data.Nat.Bisect
 public import Batteries.Data.Nat.Bitwise
 public import Batteries.Data.Nat.Gcd
