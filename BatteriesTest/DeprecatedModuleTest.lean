876526987a9617bb545797b23a2bbb9454c8f69d
F. G. Dorais <fgdorais@gmail.com>
2026-05-13 04:16:38 -0400

    fix: tests

--- BatteriesTest/DeprecatedModuleTest.lean
+++ BatteriesTest/DeprecatedModuleTest.lean
@@ -1,20 +1,20 @@
-import MathlibTest.DeprecatedModule
+import BatteriesTest.DeprecatedModule
 
 /--
 warning: We can also give more details about the deprecation
-'MathlibTest.DeprecatedModule' has been deprecated: please replace this import by
+'BatteriesTest.DeprecatedModule' has been deprecated: please replace this import by
 
-import Mathlib.Tactic.Linter.DocPrime
-import Mathlib.Tactic.Linter.DocString
+import Batteries.Tactic.Alias
+import Batteries.Tactic.Basic
 
 
 Note: This linter can be disabled with `set_option linter.deprecated.module false`
 ---
 warning:
-'MathlibTest.DeprecatedModule' has been deprecated: please replace this import by
+'BatteriesTest.DeprecatedModule' has been deprecated: please replace this import by
 
-import Mathlib.Tactic.Linter.DocPrime
-import Mathlib.Tactic.Linter.DocString
+import Batteries.Tactic.Alias
+import Batteries.Tactic.Basic
 
 
 Note: This linter can be disabled with `set_option linter.deprecated.module false`
