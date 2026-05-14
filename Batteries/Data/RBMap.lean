297fdf4db40fcfe01eeb94e37c2f18fa3ca48745
F. G. Dorais <fgdorais@gmail.com>
2026-05-14 01:16:20 -0400

    chore: deprecate modules

--- Batteries/Data/RBMap.lean
+++ Batteries/Data/RBMap.lean
@@ -1,7 +1,10 @@
 module
 
-public import Batteries.Data.RBMap.Alter
+public import Batteries.Linter.DeprecatedModule
 public import Batteries.Data.RBMap.Basic
 public import Batteries.Data.RBMap.Depth
 public import Batteries.Data.RBMap.Lemmas
+public import Batteries.Data.RBMap.Alter
 public import Batteries.Data.RBMap.WF
+
+deprecated_module "it is recommended to use Std.TreeMap instead" (since := "2026-05-14")
