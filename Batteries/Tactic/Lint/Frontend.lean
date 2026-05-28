1c6ab0cd893be22146e8760e4a55507561baf25e
yurekami <yurekami@users.noreply.github.com>
2026-05-28 17:20:07 +0800

    feat: add disable hint to environment linter output

--- Batteries/Tactic/Lint/Frontend.lean
+++ Batteries/Tactic/Lint/Frontend.lean
@@ -231,7 +231,8 @@ def formatLinterResults
           groupedByFilename results (useErrorFormat := useErrorFormat)
         else
           printWarnings results
-      pure $ some m!"/- The `{linter.name}` linter reports:\n{linter.errorsFound} -/\n{warnings}\n"
+      pure $ some m!"/- The `{linter.name}` linter reports:\n{linter.errorsFound
+        }\nUse `@[nolint {linter.name}]` to ignore a declaration. -/\n{warnings}\n"
     else if verbose = LintVerbosity.high then
       pure $ some m!"/- OK: {linter.noErrorsFound} -/"
     else
