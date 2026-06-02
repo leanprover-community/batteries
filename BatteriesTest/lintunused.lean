61fb6b07f7fbfedc60f35ddf96ba9241fcf90ea6
thorimur <68410468+thorimur@users.noreply.github.com>
2026-06-02 11:12:58 -0400

    chore: make tests defs

--- BatteriesTest/lintunused.lean
+++ BatteriesTest/lintunused.lean
@@ -3,7 +3,9 @@ import Batteries.Tactic.Lint
 -- should be ignored as the proof contains sorry
 /-- warning: declaration uses `sorry` -/
 #guard_msgs in
-theorem Foo (h : 1 = 1) : True :=
-  sorry
+def foo (h : 1 = 1) : Bool := sorry
+
+-- should be ignored since it uses `_h`
+def foo' (_h : 1 = 1) : Bool := true
 
 #lint- only unusedArguments
