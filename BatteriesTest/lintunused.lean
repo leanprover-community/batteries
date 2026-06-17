125807f43a86b5d58892b7ea6972eec0d6c164d2
Thomas R. Murrills <68410468+thorimur@users.noreply.github.com>
2026-06-17 17:46:44 -0400

    feat: allow arguments starting with `_` to be unused in `unusedArguments` linter (#1834)
    Co-authored-by: François G. Dorais <fgdorais@gmail.com>

--- BatteriesTest/lintunused.lean
+++ BatteriesTest/lintunused.lean
@@ -3,7 +3,20 @@ import Batteries.Tactic.Lint
 -- should be ignored as the proof contains sorry
 /-- warning: declaration uses `sorry` -/
 #guard_msgs in
-theorem Foo (h : 1 = 1) : True :=
-  sorry
+def foo (h : 1 = 1) : Bool := sorry
 
+-- should be ignored since it uses `_h`
+def foo' (_h : 1 = 1) : Bool := true
+
+-- should not be ignored
+set_option linter.unusedVariables false in
+def fooBad (h : 1 = 1) : Bool := true
+
+/--
+error: /- The `unusedArguments` linter reports:
+UNUSED ARGUMENTS.
+This linter can be disabled with `@[nolint unusedArguments]`. -/
+#check fooBad /- argument 1: h : 1 = 1 -/
+-/
+#guard_msgs in
 #lint- only unusedArguments
