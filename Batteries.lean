c35ff55997866abfddeb272c3a293f941fb37f65
F. G. Dorais <fgdorais@gmail.com>
2026-06-24 12:32:45 -0400

    fix: import all modules in Batteries

--- Batteries.lean
+++ Batteries.lean
@@ -108,7 +108,12 @@ public import Batteries.Tactic.HelpCmd
 public import Batteries.Tactic.Init
 public import Batteries.Tactic.Instances
 public import Batteries.Tactic.Lemma
-public import Batteries.Linter
+public import Batteries.Tactic.Lint
+public import Batteries.Tactic.Lint.Basic
+public import Batteries.Tactic.Lint.Frontend
+public import Batteries.Tactic.Lint.Misc
+public import Batteries.Tactic.Lint.Simp
+public import Batteries.Tactic.Lint.TypeClass
 public import Batteries.Tactic.NoMatch
 public import Batteries.Tactic.OpenPrivate
 public import Batteries.Tactic.PermuteGoals
