ed847021e3a0aa8514f10f626b7767933e952b8f
thorimur <68410468+thorimur@users.noreply.github.com>
2026-07-07 17:56:33 -0400

    test: add tests when tests exist

--- BatteriesTest/lint_dupNamespace.lean
+++ BatteriesTest/lint_dupNamespace.lean
@@ -3,4 +3,7 @@ import Batteries.Tactic.Lint
 -- internal names should be ignored
 theorem Foo.Foo._bar : True := trivial
 
+-- deprecated names are ignored
+@[deprecated Foo.Foo._bar (since := "today")] theorem Foo.Foo.bar : True := trivial
+
 #lint- only dupNamespace
