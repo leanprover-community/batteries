0e13d383b23748b554b2c23c042a34684aad1237
F. G. Dorais <fgdorais@gmail.com>
2026-08-20 15:56:58 -0400

    chore: delete old deprecations

--- Batteries/Data/Array/Lemmas.lean
+++ Batteries/Data/Array/Lemmas.lean
@@ -12,13 +12,6 @@ public import Batteries.Data.List.Lemmas
 
 namespace Array
 
-@[deprecated forIn_toList (since := "2025-07-01")]
-theorem forIn_eq_forIn_toList [Monad m]
-    (as : Array α) (b : β) (f : α → β → m (ForInStep β)) :
-    forIn as b f = forIn as.toList b f := by
-  cases as
-  simp
-
 /-! ### idxOf? -/
 
 @[grind =]
@@ -28,9 +21,6 @@ theorem idxOf?_toList [BEq α] {a : α} {l : Array α} :
   simp
 
 /-! ### erase -/
-
-@[deprecated (since := "2025-02-06")] alias eraseP_toArray := List.eraseP_toArray
-@[deprecated (since := "2025-02-06")] alias erase_toArray := List.erase_toArray
 
 @[simp, grind =] theorem toList_erase [BEq α] (l : Array α) (a : α) :
     (l.erase a).toList = l.toList.erase a := by
@@ -52,10 +42,6 @@ theorem toList_drop (as: Array α) (n : Nat) :
 /-! ### mem -/
 
 /-! ### insertAt -/
-
-@[deprecated (since := "2025-02-06")] alias getElem_insertIdx_lt := getElem_insertIdx_of_lt
-@[deprecated (since := "2025-02-06")] alias getElem_insertIdx_eq := getElem_insertIdx_self
-@[deprecated (since := "2025-02-06")] alias getElem_insertIdx_gt := getElem_insertIdx_of_gt
 
 /-! ### extract -/
 
