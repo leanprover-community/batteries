2129ce482e1aeb0c9b07a093967924e6b94263ba
F. G. Dorais <fgdorais@gmail.com>
2026-08-29 17:56:22 -0400

    chore: fix imports

--- Batteries/Data/RBMap.lean
+++ Batteries/Data/RBMap.lean
@@ -1,4 +1,11 @@
-module
+module -- deprecated_module: ignore
+
+public import Batteries.Data.RBMap.Alter
+public import Batteries.Data.RBMap.Basic
+public import Batteries.Data.RBMap.Depth
+public import Batteries.Data.RBMap.Lemmas
+public import Batteries.Data.RBMap.WF
+
 deprecated_module
   "This module is obsolete and has been moved to Batteries Cell `batteries-recycling`"
   (since := "2026-05-10")
