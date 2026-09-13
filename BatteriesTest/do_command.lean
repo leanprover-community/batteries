6ab5357cddc8a9d45528095b9fef7e349ce65c0d
Rob23oba <robin.arnez@web.de>
2026-09-14 01:49:20 +0200

    add test

---
+++ BatteriesTest/do_command.lean
@@ -0,0 +1,88 @@
+module
+public import Batteries
+
+/-!
+Tests for the `#do` command
+-/
+
+/-!
+Basic test
+-/
+
+#do let x := 3
+
+/-- info: 3 -/
+#guard_msgs in #do IO.println x
+
+/-!
+`let` declarations actually stay `let` declarations
+-/
+
+/-- info: 4 -/
+#guard_msgs in #do
+  have : x = 3 := rfl
+  IO.println #[1, 2, 3, 4][x]
+
+/-!
+You can use any do elements, most importantly, monadic computations
+-/
+
+open Lean Meta
+
+#do
+  let cinfo ← getConstInfo `id
+  let value := cinfo.value!
+
+/-- info: fun {α} a => a -/
+#guard_msgs in #do logInfo m!"{value}"
+
+/-!
+Mutable variables also work
+-/
+
+#do let mut x := x
+#do x := x + 2
+
+/-- info: 5 -/
+#guard_msgs in #do IO.println x
+
+/-- error: mutable variable `x` cannot be shadowed -/
+#guard_msgs in #do let x := 5
+
+/-!
+`#do_meta` lets you run computation in `MetaM` instead
+-/
+
+/-- info: 2 -/
+#guard_msgs in #do_meta lambdaTelescope value fun vars _ => logInfo m!"{vars.size}"
+
+/-!
+If you use `return`, variables will not be persisted
+-/
+
+set_option linter.unusedVariables false in
+#do let uncaptured := 5; return
+
+/-- error: Unknown identifier `uncaptured` -/
+#guard_msgs in #do IO.println uncaptured
+
+/-!
+... but only if it's in a branch that's also executed
+-/
+
+set_option linter.unusedVariables false in
+#do
+  if false then return
+  let y := 5
+
+/-- info: 5 -/
+#guard_msgs in #do IO.println y
+
+/-!
+After `#clear_do`, all previously existing variables are removed.
+-/
+
+#clear_do
+
+/-- error: Unknown identifier `x` -/
+#guard_msgs in #do IO.println x
